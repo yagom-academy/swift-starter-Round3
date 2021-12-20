b2caa0990b271b1ec95814852166ca3d6c228ec6
Mino777 <jomino7777@gmail.com>
2021-12-20 22:22:26 +0900

    feat: add Person

---
+++ CodeStarterCamp_Week3/CoffeeShop/Person.swift
@@ -0,0 +1,18 @@
+//
+//  Person.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 조민호 on 2021/12/20.
+//
+
+import Foundation
+
+struct Person {
+    var age: Int
+    var name: String
+    var money: Int
+    
+    mutating func buy(coffee: Coffee) {
+        
+    }
+}
