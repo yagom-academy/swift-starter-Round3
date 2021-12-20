4fb3aa310a1bfd5edc52932adb70c5b729681db9
Mino777 <jomino7777@gmail.com>
2021-12-20 22:23:43 +0900

    feat: add CoffeeShop

---
+++ CodeStarterCamp_Week3/CoffeeShop/CoffeeShop.swift
@@ -0,0 +1,23 @@
+//
+//  CoffeeShop.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 조민호 on 2021/12/20.
+//
+
+import Foundation
+
+struct CoffeeShop {
+    var sales: Int
+    var barista: Person?
+    var menu: [Coffee:Int]
+    var pickUpTable: [Coffee]
+    
+    mutating func order(coffee: Coffee) {
+        
+    }
+    
+    mutating func brewCoffee(coffee: Coffee) {
+    
+    }
+}
