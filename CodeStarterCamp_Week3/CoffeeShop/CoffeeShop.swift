bb35cbd62ebf9c79a37e5ec121c8bc9cacd70abd
Mino777 <jomino7777@gmail.com>
2021-12-21 14:38:21 +0900

    feat: add order(coffee: Coffee) method in CoffeeShop

--- CodeStarterCamp_Week3/CoffeeShop/CoffeeShop.swift
+++ CodeStarterCamp_Week3/CoffeeShop/CoffeeShop.swift
@@ -9,12 +9,24 @@ import Foundation
 
 struct CoffeeShop {
     private(set) var sales: Int
-    var barista: Person?
     private(set) var menu: [Coffee:Int]
     private(set) var pickUpTable: [Coffee]
+    var barista: Person?
+    var customer: Person?
     
     mutating func order(coffee: Coffee) {
+        guard let money = customer?.money else {
+            return
+        }
         
+        guard money >= coffee.price else {
+            print("잔액이 \(coffee.price - money)원만큼 부족합니다.")
+            return
+        }
+        
+        customer?.buy(coffee: coffee)
+        self.sales += coffee.price
+        brewCoffee(coffee: coffee)
     }
     
     mutating func brewCoffee(coffee: Coffee) {
