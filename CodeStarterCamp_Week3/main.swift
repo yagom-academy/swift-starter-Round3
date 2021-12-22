cadd82a56122d5699f26b71ad7940d83e6bfb301
Taeangel <wngks2050@gmail.com>
2021-12-23 00:10:29 +0900

    step1

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,39 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Person {
+  var money: Int
+  init(money: Int) {
+    self.money = money
+  }
+    func buy(){
+      print("물건을 구매합니다.")
+  }
+}
 
+
+struct CoffeeShop {
+  var totalSales = 0
+  var barista: Person?
+  let menu: [String: Int] = ["americano": 100, "cappuchino": 120]
+  var pickUpTable = "pickUpTable"
+  func takeOrder(){
+    print("주문을 받습니다.")
+  }
+  func makeCoffee(){
+    print("커피를 만듭니다.")
+  }
+}
+
+
+enum Coffee {
+  case milk
+  case iceamricano
+  case cappuccino
+}
+
+
+var yagombucks: CoffeeShop = CoffeeShop()
+let misterKim: Person = Person(money: 0)
+
+yagombucks.barista = misterKim
