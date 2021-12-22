1fccd0f594fbc1212c14c5a451730a64808a076f
Quokkaaa <rktrkdml@gmail.com>
2021-12-22 14:49:28 +0900

    feat: class Person 구현
    * name, birthYear, purse 프로퍼티 생성
    * buy, move, have 메서드 생성

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -1,12 +1,29 @@
-//
 //  main.swift
 //  CodeStarterCamp_Week3
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
+//  Created by yagom
+//  Copyright © yagom academy. All rights reserved
 
 import Foundation
 
-print("Hello, World!")
-
+class Person {
+    let name: String
+    let birthYear: Int
+    var purse: Int?
+    
+    init(name: String, birthYear: Int) {
+        self.name = name
+        self.birthYear = birthYear
+    }
+    
+    func buy(something: Any) {
+        
+    }
+    
+    func move(to: Any) {
+        print("\(name)님이 \(to)로 이동합니다.")
+    }
+    
+    func have(something: Any) {
+        
+    }
+}
