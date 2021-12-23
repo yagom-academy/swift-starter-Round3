34f78a9e59df4cc2f8f8f3e278e322eabd753670
Mino777 <jomino7777@gmail.com>
2021-12-23 19:48:05 +0900

    refactor: 테스트용 코드 제거

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -19,24 +19,5 @@ var yagombucks: CoffeeShop = CoffeeShop(sales: 0,
 
 yagombucks.customer = missMinjin
 yagombucks.barista = misterMino
-
-print("계산하기 전 -손님- 보유 금액: \(missMinjin.money)")
-print("계산하기 전 /매장/ 보유 금액: \(yagombucks.sales)\n")
 yagombucks.order(coffee: Coffee.vanillaLatte)
-print("계산 후 -손님- 보유 금액: \(missMinjin.money)")
-print("계산 후 /매장/ 보유 금액: \(yagombucks.sales)\n")
 
-yagombucks.order(coffee: Coffee.vanillaLatte)
-print("잔액이 부족할 때 -손님- 보유 금액: \(missMinjin.money)")
-print("잔액이 부족할 때 /매장/ 보유 금액: \(yagombucks.sales)\n")
-
-yagombucks.customer = misterMino
-print("계산하기 전 -손님- 보유 금액: \(misterMino.money)")
-print("계산하기 전 /매장/ 보유 금액: \(yagombucks.sales)\n")
-yagombucks.order(coffee: Coffee.vanillaLatte)
-print("계산 후 -손님- 보유 금액: \(misterMino.money)")
-print("계산 후 /매장/ 보유 금액: \(yagombucks.sales)\n")
-
-yagombucks.order(coffee: Coffee.vanillaLatte)
-print("잔액이 부족할 때 -손님- 보유 금액: \(misterMino.money)")
-print("잔액이 부족할 때 /매장/ 보유 금액: \(yagombucks.sales)")
