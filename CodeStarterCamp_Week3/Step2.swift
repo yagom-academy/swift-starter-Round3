339f491e56f6e98ebdb4adaab0d9b61617700034
JoSH0318 <jsh9870@naver.com>
2021-12-28 18:43:44 +0900

    Refactor: 1.Call takeOrder method in buyCoffee method of Person class 2.change structure to class

--- CodeStarterCamp_Week3/Step2.swift
+++ CodeStarterCamp_Week3/Step2.swift
@@ -17,12 +17,13 @@ class Person {
         self.money = money
     }
     
-    func buyCoffee(someCoffee: Coffee) {
-        print("\(someCoffee)을(를) 구입하려 합니다.")
+    func buyCoffee(coffee: Coffee, coffeeShop: CoffeeShop) {
+        print("\(coffee)을(를) 구입하려 합니다.")
+        coffeeShop.takeOrder(coffee: coffee, coffeeShop: coffeeShop)
     }
 }
 
-struct CoffeeShop {
+class CoffeeShop {
     var barista: Person?
     var customer: Person?
     var salesRevenue: Int = 0
@@ -38,30 +39,30 @@ struct CoffeeShop {
         self.menuBoard = menuBoard
     }
     
-    mutating func order(coffee: Coffee) {
+    func takeOrder(coffee: Coffee, coffeeShop: CoffeeShop) {
         if menuBoard.keys.contains(coffee) {
             print("\(coffee)을(를) 주문하셨습니다.")
-            calculateMoney(coffee: coffee)
+            guard let coffeePrice = menuBoard[coffee] else { return }
+            guard let customerMoney = customer?.money else { return }
+            if coffeePrice > customerMoney {
+                print("잔액이 \(coffeePrice - customerMoney)원만큼 부족합니다. ")
+            } else {
+                print("결제되셨습니다. \(coffee)이(가) 나오면 불러드리겠습니다.")
+                calculateMoney(coffee: coffee)
+            }
         } else {
             print("\(coffee)은(는) 판매하지 않습니다.")
         }
     }
     
-    mutating func calculateMoney(coffee: Coffee) {
-        guard let someCoffeePrice = menuBoard[coffee] else { return }
-        guard let customerOfCoffeeShop = customer else { return }
-
-        if someCoffeePrice > customerOfCoffeeShop.money {
-            print("잔액이 \(someCoffeePrice - customerOfCoffeeShop.money)원만큼 부족합니다. ")
-        } else {
-            print("결제되셨습니다. \(coffee)이(가) 나오면 불러드리겠습니다.")
-            salesRevenue += someCoffeePrice
-            customerOfCoffeeShop.money -= someCoffeePrice
-            serveOnPickUpTable(coffee: coffee)
-        }
+    func calculateMoney(coffee: Coffee) {
+        guard let coffeePrice = menuBoard[coffee] else { return }
+        salesRevenue += coffeePrice
+        customer?.money -= coffeePrice
+        serveOnPickUpTable(coffee: coffee)
     }
     
-    mutating func serveOnPickUpTable(coffee: Coffee) {
+    func serveOnPickUpTable(coffee: Coffee) {
         pickUpTable = coffee
     }
 }
@@ -70,25 +71,23 @@ enum Coffee {
     case americano, cafeLatte, espresso, cappuccino, cafeMocha
 }
 
-let misterLee = Person(name: "misterLee", money: 100_000)
-let missKim = Person(name: "missKim", money: 6_000)
+var misterLee = Person(name: "misterLee", money: 100_000)
+var missKim = Person(name: "missKim", money: 6_000)
 var yagombucks = CoffeeShop(menuBoard: [.americano : 4000,
                                         .cafeLatte : 4200,
                                         .espresso : 3000,
                                         .cappuccino : 4300,
                                         .cafeMocha : 4500])
+
 //yagombucks.barista = misterLee
 //yagombucks.customer = missKim
-
-//missKim.buyCoffee(someCoffee: .americano)
-//yagombucks.order(coffee: .americano)
+//
+//missKim.buyCoffee(coffee: .americano, coffeeShop: yagombucks)
 //print(missKim.money)
 //print(yagombucks.salesRevenue)
-
-//missKim.buyCoffee(someCoffee: .cappuccino)
-//yagombucks.order(coffee: .cappuccino, customer: missKim)
+//
+//missKim.buyCoffee(coffee: .cappuccino, coffeeShop: yagombucks)
 //print(missKim.money)
 //print(yagombucks.salesRevenue)
-
 
 
