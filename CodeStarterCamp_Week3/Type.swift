bc9e9fd04ce07be7b426d6fd6a6c5d3abcbc8432
Jungmo Yu <zzoo789@naver.com>
2021-12-29 01:29:05 +0900

    refactor: Step2내용 수정

--- CodeStarterCamp_Week3/Type.swift
+++ CodeStarterCamp_Week3/Type.swift
@@ -17,51 +17,47 @@ class Person {
     }
     
     func buyCoffee(at coffeeShop: CoffeeShop, coffee: Coffee) {
-        if self.money < coffee.price {
+        var resultOfOrder: Bool = false
+        guard (self.money > coffee.price) else {
             print("잔액이 \(coffee.price - self.money)원만큼 부족합니다.")
-        } else {
-            let isOrderCompleted = coffeeShop.order(coffee: coffee, nameOfCustomer: self.name)
-            self.money = isOrderCompleted ? (self.money - coffee.price) : self.money
+            return
         }
+        resultOfOrder = coffeeShop.order(coffee: coffee, nameOfCustomer: self.name)
+        if resultOfOrder { self.money -= coffee.price }
     }
 }
 
 
 class Barista: Person {
-    func makeCoffee(coffee: Coffee) -> Bool {
-        var finishedExtractCoffee = false
-        finishedExtractCoffee = true
-        
-        return finishedExtractCoffee
-    }
+    func makeCoffee(coffee: Coffee) -> Bool { return true}
 }
 
 
 class CoffeeShop {
-    var sales: UInt = 0
+    private var sales: UInt = 0
+    private var menu: [Coffee]
     var barista: Barista?
-    var menu: [Coffee]
     var pickUpTable: Coffee?
     
     init(menu: [Coffee]) {
         self.menu = menu
     }
     
-    func order(coffee: Coffee, nameOfCustomer: String) -> Bool {
-        var isSucceed: Bool = false {
+    func order(coffee: Coffee, nameOfCustomer: String) -> Bool{
+        var isSucceed : Bool = false {
             didSet {
-                print("\(nameOfCustomer) 님의 커피가 준비되었습니다. 픽업대에서 가져가주세요.")
                 pickUpTable = coffee
+                self.sales += coffee.price
+                print("매출이 \(coffee.price)원 만큼 증가하여 \(self.sales)원이 되었습니다.")
+                print("\(nameOfCustomer) 님의 커피가 준비되었습니다. 픽업대에서 가져가주세요.")
             }
         }
-        
-        if let barista = self.barista {
-            isSucceed = barista.makeCoffee(coffee: coffee)
-        } else {
+        guard let barista = self.barista else {
             print("현재 카페에 바리스타가 없어서 커피를 만들 수 없습니다!")
+            return isSucceed
         }
         
-        self.sales += coffee.price
+        isSucceed = barista.makeCoffee(coffee: coffee)
         return isSucceed
     }
 }
@@ -74,13 +70,11 @@ enum Coffee {
     case hotChocolate
     
     var price: UInt {
-        get {
-            switch self {
-            case .americano: return 4500
-            case .cafeLatte: return 5500
-            case .cappuccino: return 6000
-            case .hotChocolate: return 5000
-            }
+        switch self {
+        case .americano: return 4500
+        case .cafeLatte: return 5500
+        case .cappuccino: return 6000
+        case .hotChocolate: return 5000
         }
     }
 }
