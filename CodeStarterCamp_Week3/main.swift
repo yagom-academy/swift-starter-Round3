d979c2b16e09889275890e29cb29541e4c46f9d3
Minseong-yagom <msb0987@naver.com>
2021-12-29 13:13:59 +0900

    Feat 주문과 동시에 발생하는 이벤트 추가
    주문과 동시에 고객의 잔고가 줄어들고 커피가게의 매출액은 증가
    커피 완성시 픽업테이블 배치

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -5,6 +5,7 @@ enum Coffee {
 }
 
 struct Person {
+    var name: String = "free"
     var money: Int = 0
 
     func buy(product: String) {
@@ -15,16 +16,32 @@ struct Person {
 struct CoffeeShop {
     var cafeSales: Int = 0
     var menu: [Coffee : Int] = [.americano : 2500, .cafeLatte : 3000, .espresso : 4000]
-    var pickUpTable: String?
+    var pickUpTable: Coffee?
     var barista: Person = Person()
 
-    func takeOrderMake(coffee: Coffee) {
+    mutating func takeOrderMake(coffee: Coffee, customer: Person) {
+        var customerMoney = customer.money
+
         print("\(coffee)를 주문 받았습니다.")
-        print("주문하신 \(coffee)나왔습니다.")
+
+        if let cost = menu[coffee] {
+            if customerMoney < cost {
+                print("잔액이 \(cost - customerMoney)원 부족합니다.")
+            } else {
+                customerMoney -= cost
+                cafeSales += cost
+
+                pickUpTable = coffee
+            }
+        }
+        if let _ = pickUpTable {
+            print("\(customer.name) 님의 커피가 준비되었습니다. 픽업대에서 가져가주세요.")
+        }
     }
 }
 
-var misterLee: Person = Person(money: 10000)
-var missKim: Person = Person(money: 30000)
+var misterLee: Person = Person(name: "misterLee", money: 10000)
+var missKim: Person = Person(name: "missKim", money: 10000)
 var yagombucks: CoffeeShop = CoffeeShop()
 yagombucks.barista = misterLee
+yagombucks.takeOrderMake(coffee: .americano, customer: missKim)
