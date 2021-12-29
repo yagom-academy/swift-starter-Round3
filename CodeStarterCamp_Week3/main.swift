10ad8797338bd6116ab43a6a1996844e8d8b21f5
서원지 <75601594+suhwj@users.noreply.github.com>
2021-12-29 11:50:24 +0900

    week3 step1 해서 올려 드립니다

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,6 +8,65 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Person {
+    let name: String
+    var money: Int
+    func buy(items: String) {
+        print("\(items)을 삽니다 ")
+    }
+}
+
+enum Coffee {
+    case espresso
+    case americano
+    case cafeLatte
+    case vanillalatte
+    case caramelMacchiato
+    case cafemocha
+    case affogato
+    
+    var cost: Int {
+        switch self {
+        case .espresso:
+            return 3000
+        case .americano:
+            return 4500
+        case .cafeLatte:
+            return 5000
+        case .vanillalatte:
+            return 5500
+        case .caramelMacchiato:
+            return 5500
+        case .cafemocha:
+            return 5700
+        case .affogato:
+            return 6000
+        }
+    }
+}
+
+struct CoffeeShop {
+    var brista: Person
+    var salesRevenue = 0
+    var pickUpTable: String? = nil
+    
+    init(brista: Person){
+        self.brista = brista
+    }
+    
+    mutating func takeOrderMenu(coffee: Coffee) {
+        let costOfcoffee = coffee.cost
+        salesRevenue += costOfcoffee
+        print("\(coffee)는 \(costOfcoffee)원 입니다")
+        print(salesRevenue)
+    }
+    func makeCoffeeMenu(coffee: String) {
+        print("\(coffee)를 제조중입니다!!!")
+    }
+}
+
+var misterLee = Person(name: "Lee", money: 20000)
+var missKim = Person(name: "Kim", money: 15000)
+var yagombucks = CoffeeShop(brista: misterLee)
 
   
