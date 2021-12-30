32f4a2ff2773ee0e3b3ec593d26bb21a4ea3f14b
nayeon <vennie20@naver.com>
2021-12-30 17:20:30 +0900

    feat: main remove price[]

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -1,3 +1,3 @@
 var misterLee = Person(name: "misterLee", gender: .male)
 var missKim = Person(name:"missKim", gender: .female, money: 50000)
-var yagombucks = CoffeeShop(prices: [1000, 2000, 3000, 4000, 5000], barista: misterLee)
+var yagombucks = CoffeeShop(barista: misterLee)
