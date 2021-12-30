757ccc7856bc2319c3c0f0aa6b174e8cfb79f858
Minseong-yagom <msb0987@naver.com>
2021-12-31 03:17:20 +0900

    Refactor 2차 피드백 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -19,26 +19,26 @@ struct CoffeeShop {
     var customers: Person?
     var menu: [Coffee : Int] = [.americano : 2500, .cafeLatte : 3000, .espresso : 4000]
     var pickUpTable: Coffee? {
-        didSet {
-            if let customersName = customers?.name {
-                print("\(customersName) 님의 커피가 준비되었습니다. 픽업대에서 가져가주세요.")
+        willSet(finishedCoffee) {
+            if let customerCoffee = finishedCoffee {
+                if let customersName = customers?.name {
+                    print("\(customersName) 님의 \(customerCoffee)가 준비되었습니다. 픽업대에서 가져가주세요.")
+                }
             }
         }
     }
     
-    mutating func takeOrderMake(coffee: Coffee) {
+    mutating func takeOrderMake(coffee: Coffee, customer: inout Person) {
         print("\(coffee)를 주문 받았습니다.")
         
         if let cost = menu[coffee] {
-            if let customerMoney = customers?.money {
-                if cost > customerMoney {
-                    print("잔액이 \(cost - customerMoney)원 부족합니다.")
-                }else {
-                    customers?.money -= cost
-                    cafeSales += cost
-                    
-                    pickUpTable = coffee
-                }
+            if cost > customer.money {
+                print("잔액이 \(cost - customer.money)원 부족합니다.")
+            }else {
+                customer.money -= cost
+                cafeSales += cost
+                
+                pickUpTable = coffee
             }
         }
     }
@@ -49,4 +49,5 @@ var missKim: Person = Person(name: "missKim", money: 10000)
 var yagombucks: CoffeeShop = CoffeeShop()
 yagombucks.barista = misterLee.name
 yagombucks.customers = missKim
-yagombucks.takeOrderMake(coffee: .americano)
+yagombucks.takeOrderMake(coffee: .americano, customer: &missKim)
+
