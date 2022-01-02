acd360b4000ffeb21bb8d968ae207ca3521ba9b1
marisol <marisol.develop@gmail.com>
2022-01-02 16:58:04 +0900

    refactor: 열거형 내부에 프로퍼티 생성, 함수명과 구조 변경 bugfix : customer의 asset과 coffeeShop의 turnover 업데이트 되도록 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,64 +8,86 @@
 
 import Foundation
 
-struct Person {
+class Person {
     var asset: Int
     var name: String
-    var job: String
-    mutating func purchaseSomeThing(price: Int) -> Int {
-        asset -= price
-        return asset
+    func orderCoffee(coffeeShop: CoffeeShop, coffee: Coffee) {
+        print("\(name) ordered \(coffee)")
+        coffeeShop.takeOrder(coffee: coffee)
+    }
+    
+    init(name: String, asset: Int) {
+        self.name = name
+        self.asset = asset
     }
 }
 
-struct CoffeeShop {
-    var turnover: Int
+class CoffeeShop {
+    var turnover: Int = 0
     var pickUpTable: Dictionary<String, Coffee>? = [String: Coffee]()
-    var baristar: Person
+    var baristar: Person = Person(name: "misterLee", asset: 30000)
+    var customer: Person?
     var menu: Dictionary<Coffee, Int> = [Coffee.americano : 4500,
                                          Coffee.cafeLatte : 5000,
-                                         Coffee.cafeMocha : 5500,
+                                         Coffee.cafeMocha : 5000,
                                          Coffee.vanillaLatte : 6000]
     
-    mutating func order(customer: Person, coffee: Coffee) {
-        var assetOfCustomer = customer.asset
-        let priceOfCoffee = coffee.rawValue
-        var turnoverOfCoffeeShop = turnover
-        
-        if assetOfCustomer < priceOfCoffee {
-            let lackOfMoney: Int = priceOfCoffee - assetOfCustomer
+    func takeOrder(coffee: Coffee) {
+        let priceOfCoffee = coffee.priceOfCoffee
+        guard let customerAsset = customer?.asset else {return}
+        guard let customerName = customer?.name else {return}
+
+        if customerAsset < priceOfCoffee {
+            let lackOfMoney: Int = priceOfCoffee - customerAsset
             print("잔액이 \(lackOfMoney)원만큼 부족합니다.\n")
         } else {
-            assetOfCustomer -= priceOfCoffee
-            turnoverOfCoffeeShop += priceOfCoffee
-            brewCoffee(customer: customer.name, coffee: coffee)
-            print("고객 잔액 : \(assetOfCustomer), 커피 가격 : \(coffee.rawValue), 카페 이익 : \(turnoverOfCoffeeShop)\n")
+            updateAsset(coffee: coffee)
+            brewCoffee(customer: customerName, coffee: coffee)
+            print("고객 잔액 : \(customer?.asset ?? 0), 커피 가격 : \(menu[coffee] ?? 0), 카페 이익 : \(turnover)\n")
         }
     }
     
-    mutating func brewCoffee(customer: String, coffee: Coffee) {
-        print("baristar \(baristar.name) is now brewing \(customer)'s \(coffee)...")
+    func updateAsset(coffee: Coffee) {
+        customer?.asset -= coffee.priceOfCoffee
+        turnover += coffee.priceOfCoffee
+    }
+    
+    func brewCoffee(customer: String, coffee: Coffee) {
         guard ((pickUpTable?[customer] = coffee) != nil) else {
             return print("픽업대가 비었습니다.")
         }
+        print("baristar \(baristar.name) is now brewing \(customer)'s \(coffee)...")
         print("\(customer)님의 \(coffee)가 준비되었습니다. 픽업대에서 가져가주세요.")
-        
-        
     }
 }
 
 enum Coffee: Int {
-    case americano = 4500
-    case cafeLatte = 5000
-    case cafeMocha = 5500
-    case vanillaLatte = 6000
+    case americano
+    case cafeLatte
+    case cafeMocha
+    case vanillaLatte
+    
+    var priceOfCoffee: Int {
+        switch self {
+        case .americano:
+            return 4500
+        case .cafeLatte:
+            return 5000
+        case .cafeMocha:
+            return 5000
+        case .vanillaLatte:
+            return 6000
+        }
+    }
 }
 
-var misterLee: Person = Person(asset: 20000, name: "misterLee", job: "baristar")
-var missKim: Person = Person(asset: 3000, name: "missKim", job: "iOSDeveloper")
-var marisol: Person = Person(asset: 50000, name: "marisol", job: "student")
-var yagombucks: CoffeeShop = CoffeeShop(turnover: 0, baristar: misterLee)
+var missKim: Person = Person(name: "missKim", asset: 3000)
+var marisol: Person = Person(name: "marisol", asset: 50000)
+var yagombucks: CoffeeShop = CoffeeShop()
 
-yagombucks.order(customer: missKim, coffee: Coffee.americano)
-yagombucks.order(customer: marisol, coffee: Coffee.cafeLatte)
-yagombucks.order(customer: marisol, coffee: Coffee.americano)
+yagombucks.customer = marisol
+marisol.orderCoffee(coffeeShop: yagombucks, coffee: .americano)
+marisol.orderCoffee(coffeeShop: yagombucks, coffee: .cafeMocha)
+marisol.orderCoffee(coffeeShop: yagombucks, coffee: .cafeLatte)
+yagombucks.customer = missKim
+missKim.orderCoffee(coffeeShop: yagombucks, coffee: .vanillaLatte)
