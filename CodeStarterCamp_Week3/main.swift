4679080e6bcc244af1cd56e1af354077eaad9a32
nayeon <vennie20@naver.com>
2022-01-03 14:52:11 +0900

    feat: make step1 comment, missKim order in main

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -1,3 +1,5 @@
 var misterLee = Person(name: "misterLee", gender: .male)
 var missKim = Person(name:"missKim", gender: .female, money: 50000)
 var yagombucks = CoffeeShop(barista: misterLee)
+
+missKim.orderCoffeeFromShop(coffee: .chocolateLatte, from: yagombucks)
---
+++ week3_step2.swift
@@ -0,0 +1,8 @@
+//
+//  week3_step2.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 김나연 on 2022/01/03.
+//
+
+import Foundation
