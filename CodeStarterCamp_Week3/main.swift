7f43838191cb8a451c212ce327ea0799db3c8841
nayeon <vennie20@naver.com>
2022-01-04 16:43:40 +0900

    feat: order from shop not from person

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -2,4 +2,4 @@ var misterLee = Person(name: "misterLee", gender: .male)
 var missKim = Person(name:"missKim", gender: .female, money: 50000)
 var yagombucks = CoffeeShop(barista: misterLee)
 
-missKim.orderCoffeeFromShop(coffee: .chocolateLatte, from: yagombucks)
+yagombucks.order(coffee: .chocolateLatte, forWho: missKim)
