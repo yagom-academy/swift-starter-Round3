1889f8bec3a9b054517014724a60613a55ef6790
iilim <42.4.ilim@gmail.com>
2022-01-05 12:56:38 +0000

    Feat: Coffee 열거형 타입으로 선언 완료, 나머지 타입 미완성

---
+++ CodeStarterCamp_Week3/orderCoffee.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+class Person {
+    var money: Int = 0
+	
+	func buy() {
+		
+	}
+}
+
+class CoffeeShop {
+	var revenue: Int = 0
+	
+}
+
+enum Coffee {
+    case americano, caffeLatte, cappuccino
+}
