c512438d221dc3c0b688e11d6d4707cf52504ea1
iilim <42.4.ilim@gmail.com>
2022-01-06 20:06:07 +0900

    Feat: buy 함수 완성 및 모든 코드 main파일로 이동

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Person {
+    var money: Int = 0
+	
+	func buyThings(things: String) {
+		print("\(things)를 구매합니다.")
+	}
+}
+
+struct CoffeeShop {
+	var revenue: Int = 0
+	var menu = [String : Int]()
+	var pickUpTable: String = ""
+	var barista: String = ""
+
+	mutating func order(coffee: String) {
+		makeCoffee(coffee: coffee)
+	}
+
+	mutating func makeCoffee(coffee: String) {
+		pickUpTable = coffee
+	}
+}
+
+enum Coffee: String {
+    case americano
+	case caffeLatte
+	case cappuccino
+}
+
+var misterLee: Person = Person()
+var missKim: Person = Person()
+var yagombucks: CoffeeShop = CoffeeShop()
+
+yagombucks.barista = "misterLee"
 
