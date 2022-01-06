222dae9346c0cffdfae94d51312ea9a192a6cd43
iilim <42.4.ilim@gmail.com>
2022-01-06 19:44:48 +0900

    Feat: buy 함수를 제외한 나머지 기능 완성

--- CodeStarterCamp_Week3/orderCoffee.swift
+++ CodeStarterCamp_Week3/orderCoffee.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-class Person {
+struct Person {
     var money: Int = 0
 	
 	func buy() {
@@ -8,11 +8,31 @@ class Person {
 	}
 }
 
-class CoffeeShop {
+struct CoffeeShop {
 	var revenue: Int = 0
-	
+	var menu = [String : Int]()
+	var pickUpTable: String = ""
+	var barista: String = ""
+
+	mutating func order(coffee: String) {
+		makeCoffee(coffee: coffee)
+	}
+
+	mutating func makeCoffee(coffee: String) {
+		pickUpTable = coffee
+	}
 }
 
-enum Coffee {
-    case americano, caffeLatte, cappuccino
+enum Coffee: String {
+    case americano
+	case caffeLatte
+	case cappuccino
 }
+
+var misterLee: Person = Person()
+var missKim: Person = Person()
+var yagombucks: CoffeeShop = CoffeeShop()
+
+yagombucks.barista = "misterLee"
+
+
