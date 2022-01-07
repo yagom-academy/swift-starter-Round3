1fb8fdd714654b5a2f065ff94f1174138fe3de93
Kioding <65153742+Kioding@users.noreply.github.com>
2022-01-07 17:52:10 +0900

    Revert "2 dnwhd0112 week3 step2"

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,12 +8,5 @@
 
 import Foundation
 
-var misterLee: Person = Person(name: "misterLee", money: 3000)
-var missKim: Person = Person(name: "missKim", money: 3000)
-var yagombucks = CoffeeShop(barista: misterLee)
+print("Hello, World!")
 
-
-yagombucks.menu = [.arrabica:1000,.yogu:2000,.mix:4000]
-yagombucks.getOrder(person: missKim, orderedCoffee: .mix)
-yagombucks.getOrder(person: missKim, orderedCoffee: .yogu)
-missKim.getProduct(product: yagombucks.pickUpTable as Any)
