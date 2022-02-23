830d9f6a55f60170edee68e3063501364a855577
ohkyeongsik <unchain123@kakao.com>
2022-02-23 23:42:30 +0900

    feat: add CoffeShop class

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -17,3 +17,23 @@ class Person {
         print(money-price)
     }
 }
+
+class CoffeeShop {
+    var sales: Int
+    var menu: (KindOfCoffee: String, price: Int)
+    var pickUpTable: Array<String> = Array<String>()
+    
+    init(sales: Int, menu: (String,Int), pcikUpTable: Array<String>){
+        self.menu = menu
+        self.sales = sales
+        self.pickUpTable = pcikUpTable
+    }
+    
+    func orderMenu(coffee: Coffee) {
+        print("\(coffee)한잔 주문 받았습니다.")
+    }
+    
+    func makeCoffe(coffee: Coffee) {
+        print("\(coffee)를 만들었습니다.")
+    }
+}
