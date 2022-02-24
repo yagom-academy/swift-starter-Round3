bea12a7c3e1be264742ef0fa4e8f6c5de07303e2
yeeton37 <lyeeun37@naver.com>
2022-02-24 15:47:32 +0900

    Person 타입 정의

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -7,3 +7,15 @@ enum Coffee {
     case coldBrew
 }
 
+class Person {
+    var money: Int
+    var feeling: String
+    
+    func buyThings(coffee: Coffee) {
+        print("안녕하세요. \(coffee) 한 잔 주시겠어요?")
+    }
+    init(money: Int, feeling: String) {
+        self.money = money
+        self.feeling = feeling
+    }
+}
