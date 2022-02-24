3042ec1c19db21e584fb0957155ab301a006b79b
lia388 <lia388@naver.com>
2022-02-24 17:48:16 +0900

    [Week3-step1] : Person 타입 및 인스턴스 생성

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -15,3 +15,18 @@ enum Coffee {
     case fruitSmoothie
 }
 
+struct Person {
+    var name: String
+    var money: Int
+    
+    mutating func buySomething(somethingPrice: Int) {
+        if self.money >= somethingPrice {
+            let minus = self.money - somethingPrice
+            print("\(self.name)이 구매를 완료했습니다. 잔액은 \(minus)원 입니다")
+        } else {
+            print("잔액이 부족합니다.")
+        }
+    }
+}
+var misterLee = Person(name: "misterLee", money: 100000)
+var missKim = Person(name: "missKim", money: 100000)
