501c30aa39aaa48405e36a19f0be02831a43ee13
lia388 <lia388@naver.com>
2022-02-24 17:49:57 +0900

    [Week3-step1] : CoffeeShop 타입 및 인스턴스 생성, 바리스타 할당

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -30,3 +30,29 @@ struct Person {
 }
 var misterLee = Person(name: "misterLee", money: 100000)
 var missKim = Person(name: "missKim", money: 100000)
+
+struct CoffeShop {
+    var sales: Int
+    var menu: [Coffee: Int]
+    var pickUpTable: String
+    var barista = Person(name: "", money: 0)
+
+    mutating func orderCoffee(coffee: Coffee) {
+        if let coffeePrice = menu[coffee] {
+            sales += coffeePrice
+        } else {
+            print("다시 주문해주세요")
+        }
+    }
+    mutating func makeCoffee(person: String, coffee: Coffee) {
+        let finishCoffee = pickUpTable == "on"
+        if finishCoffee {
+            print("\(person)님 주문하신 \(coffee) 나왔습니다")
+        } else {
+            print("잠시만 기다려주세요")
+        }
+    }
+}
+
+var yagombucks = CoffeShop(sales: 200000, menu: [Coffee.americano: 4100, Coffee.latte: 4500], pickUpTable: "on")
+yagombucks.barista = misterLee
