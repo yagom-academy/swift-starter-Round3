b49055f441a85416c23430bb9edd106fcd41e58f
lia388 <lia388@naver.com>
2022-02-25 15:42:10 +0900

    [Week3-step1] : 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -12,17 +12,17 @@ enum Coffee {
     case americano
     case latte
     case espresso
-    case fruitSmoothie
 }
 
 struct Person {
-    var name: String
+    let name: String
     var money: Int
     
-    mutating func buySomething(somethingPrice: Int) {
-        if self.money >= somethingPrice {
-            let minus = self.money - somethingPrice
-            print("\(self.name)이 구매를 완료했습니다. 잔액은 \(minus)원 입니다")
+    mutating func buySomething(for price: Int) {
+        let hasEnoughMoney = self.money >= price
+        if hasEnoughMoney {
+            self.money -= price
+            print("\(self.name)이 구매를 완료했습니다. 잔액은 \(self.money)원 입니다")
         } else {
             print("잔액이 부족합니다.")
         }
@@ -33,26 +33,26 @@ var missKim = Person(name: "missKim", money: 100000)
 
 struct CoffeShop {
     var sales: Int
-    var menu: [Coffee: Int]
+    let menu: [Coffee: Int]
     var pickUpTable: String
-    var barista = Person(name: "", money: 0)
+    var barista: Person?
 
-    mutating func orderCoffee(coffee: Coffee) {
+    mutating func takeOrder(coffee: Coffee) {
         if let coffeePrice = menu[coffee] {
             sales += coffeePrice
         } else {
             print("다시 주문해주세요")
         }
     }
-    mutating func makeCoffee(person: String, coffee: Coffee) {
-        let finishCoffee = pickUpTable == "on"
-        if finishCoffee {
-            print("\(person)님 주문하신 \(coffee) 나왔습니다")
+    
+    func makeCoffee(coffee: Coffee, to person: Person) {
+        let isCompleted = pickUpTable == "on"
+        if isCompleted {
+            print("\(person.name)님 주문하신 \(coffee) 나왔습니다")
         } else {
             print("잠시만 기다려주세요")
         }
     }
 }
 
-var yagombucks = CoffeShop(sales: 200000, menu: [Coffee.americano: 4100, Coffee.latte: 4500], pickUpTable: "on")
-yagombucks.barista = misterLee
+var yagombucks = CoffeShop(sales: 200000, menu: [Coffee.espresso: 3800, Coffee.americano: 4100, Coffee.latte: 4500], pickUpTable: "on", barista: misterLee)
