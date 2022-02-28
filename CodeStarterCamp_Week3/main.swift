80a397291396f5c8ab73e8f1230e6fa564a4b0c0
Makseong2 <wshstjddl@gmail.com>
2022-02-28 22:20:33 +0900

    [Week3-step1] : 총 매출 확인

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -11,3 +11,8 @@ import Foundation
 var misterLee = Person(moneyYouHave : nil)
 var missKim = Person(moneyYouHave: 10000)
 var yagombucks = CoffeeShop(totalSale: 0, menu: [[.americano:4500],[.tea: 2500],[.juice: 3000]], pickUpTable: 5)
+
+yagombucks.takeOrder(drinkType: .juice)
+yagombucks.takeOrder(drinkType: .juice)
+yagombucks.takeOrder(drinkType: .juice)
+print(yagombucks.totalSale)
