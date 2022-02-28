0e7e8b9010a6bee6e34c50c82ff49bcffa4c8d57
yeeton37 <lyeeun37@naver.com>
2022-02-28 16:59:29 +0900

    feat: [step1] 개행 및  띄어쓰기 통일 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -11,9 +11,11 @@ class Person {
     var money: Int
     var feeling: String = ""
     let name: String
+    
     func buyThings(coffee: Coffee) {
         print("안녕하세요. \(coffee) 한 잔 주시겠어요?")
     }
+    
     init(name: String, money: Int) {
         self.name = name
         self.money = money
@@ -23,7 +25,7 @@ class Person {
 class CoffeeShop {
     var sales: Int = 0
     var pickUpTable: String?
-    var menu : [Coffee :Int] = [.americano : 1500, .latte : 3000, .cappuccino : 3500, .coldBrew : 4000]
+    var menu : [Coffee: Int] = [.americano: 1500, .latte: 3000, .cappuccino: 3500, .coldBrew: 4000]
     var barista: Person
     var customer: Person?
     
@@ -32,12 +34,13 @@ class CoffeeShop {
         print("주문하신 \(coffee) 나왔습니다.")
     }
     
-    init(sales: Int,barista: Person, pickUpTable: String) {
+    init(sales: Int, barista: Person, pickUpTable: String) {
         self.sales = sales
         self.barista = barista
         self.pickUpTable = pickUpTable
     }
-    init(sales: Int,barista: Person) {
+    
+    init(sales: Int, barista: Person) {
         self.sales = sales
         self.barista = barista
     }
