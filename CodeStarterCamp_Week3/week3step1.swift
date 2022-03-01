0d14cbc099dd6153c624c7e55ea7739c8e30bff2
mskaang <mskaang@gmail.com>
2022-03-01 18:33:42 +0900

    [UPDATE] Person 타입 정의 완료

--- CodeStarterCamp_Week3/week3step1.swift
+++ CodeStarterCamp_Week3/week3step1.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+
+class Person {
+    var name: String
+    var money: Int
+    
+    init(name: String, money: Int) {
+        self.name = name
+        self.money = money
+    }
+    
+    func buy(something: String) {
+        var inventory: Array<String> = []
+        inventory.append(something)
+        print("\(self.name)의 인벤토리에 \(something)이/가 추가되었습니다.")
+    }
+}
