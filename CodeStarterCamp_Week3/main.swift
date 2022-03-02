58c39823b058d5bac00a857b34ca86f5b2606d8a
your_name <sondaehyeon@sondaehyeon-ui-MacBookAir.local>
2022-03-02 21:53:05 +0900

    Week3 [STEP2] 작성해봤습니다.

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -1,30 +1,58 @@
 import Foundation
 
-struct Person {
-    var money: Int
-    func buyCoffee() {
-        print("커피를 구매합니다")
-    }
-}
 enum Coffee: Int {
     case iceAmericano = 2500
-    case cafeLatte = 3300
-    case cappuccino = 3500
+    case cafeLatte = 4300
+    case cappuccino = 4800
 }
-struct CoffeeShop {
-    //당장 필요하지 않은 값들은 옵셔널로 표시 했습니다
-    var sales: Int?
-    var baristar: Person
-    var coffeeMenu: Coffee?
-    var pickUpTable: String?
-    func takeOrder(coffee: Coffee) {
-        print("\(coffee)하나 부타드립니다.")
+class CoffeeShop {
+    var sales: Int
+    var coffeeMenu : Coffee?
+    var pickUpTable: String = ""
+    init(sales: Int) {
+        self.sales = sales
+    }
+    func takeOrder(coffee: Coffee, person: Person) {
+        print("\(coffee) 주문받았습니다")
     }
     func makeCoffee(making: Coffee) {
-        print("\(making)하나 제조중입니다")
+        print("\(making) 제조중입니다")
     }
 }
-var misterLee: Person = Person(money: 15000)
-var missKim: Person = Person(money: 15000)
-var yagomBucks: CoffeeShop = CoffeeShop(baristar: misterLee)
-print(yagomBucks.baristar)
+class Person: CoffeeShop {
+    
+    var money: Int
+    var name: String
+    init(money: Int, name: String) {
+        self.money = money
+        self.name = name
+        super.init(sales: 0)
+    }
+    func buyCoffee(coffee: Coffee) {
+        print("\(coffee) 부탁드립니다")
+    }
+    override func takeOrder(coffee: Coffee, person: Person) {
+        buyCoffee(coffee: coffee)
+        super.takeOrder(coffee: coffee, person: person)
+        // 값 들을 편하게 연산하기 위해 상수로 정의 해 봤습니다.
+        let missMoney: Int = person.money
+        let yagomMoney: Int = yagomBucks.sales
+        let coffeePrice: Int = coffee.rawValue
+        print("\(name) 잔액 : \(missMoney-coffeePrice)원, yagomBucks매출액 : \(yagomMoney + coffeePrice)원")
+        missKim.pickUpTable = "\(name)님의 커피가 준비되었습니다. 픽업대에서 가져가주세요."
+        missLee.pickUpTable = "\(name)님의 커피가 준비되었습니다. 픽업대에서 가져가주세요."
+        if missMoney < coffeePrice {
+            print("잔액이 \(coffeePrice-missMoney)원 만큼 부족합니다")
+        } else if missMoney >= coffeePrice {
+            super.makeCoffee(making: coffee)
+            print("\(pickUpTable)")
+        }
+    }
+}
+var missKim: Person = Person(money: 3000, name: "missKim")
+var missLee: Person = Person(money: 3000, name: "missLee")
+var yagomBucks: CoffeeShop = CoffeeShop(sales: 2000)
+//두 결과를 확인하기 위해 두 값을 출력 해 봤습니다.
+missKim.takeOrder(coffee: .cappuccino, person: missKim)
+print("ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ")
+missLee.takeOrder(coffee: .iceAmericano, person: missLee)
