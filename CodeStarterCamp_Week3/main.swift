6ac4d08a2911f252a8d3d8c5eeb668b0adca4274
lia388 <lia388@naver.com>
2022-03-04 23:55:07 +0900

    [Week3-step2] : buy 메소드, coffeeShop 클래스 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -35,15 +35,19 @@ class Person {
     }
     
     func buy(coffee: Coffee, from coffeeShop: CoffeeShop) {
-        let hasEnoughMoney = self.money >= coffee.price
-        guard hasEnoughMoney else {
+        if Coffee.allCases.contains(coffee) {
+            let hasEnoughMoney = self.money >= coffee.price
+            guard hasEnoughMoney else {
+                print("잔액이 \(coffee.price-self.money)원만큼 부족합니다.")
+                return
+            }
             self.money -= coffee.price
-            print("잔액이 \(-self.money)원만큼 부족합니다.")
-            return
+            print("결제가 완료되었습니다. 잔액은 \(self.money)원입니다.")
+            coffeeShop.takeOrder(coffee: coffee, from: self)
         }
-        self.money -= coffee.price
-        print("결제가 완료되었습니다. 잔액은 \(self.money)원입니다.")
-        coffeeShop.takeOrder(coffee: coffee, from: self)
+        else {
+            print("죄송합니다. 주문하신 \(coffee)는 판매하지 않습니다.")
+        }
     }
 }
 
@@ -51,34 +55,32 @@ var misterLee = Person(name: "misterLee", money: 10000)
 var missKim = Person(name: "missKim", money: 10000)
 
 class CoffeeShop {
-    var sales: Int
-    let menu: [Coffee: Int]
-    var pickUpTable: Bool
+    var sales: Int = 0
+    let menu: [Coffee] = Coffee.allCases
+    var customer: String = ""
+    var pickUpTable: [Coffee] = [] {
+        didSet {
+            print("\(customer)님의 커피가 준비되었습니다. 픽업대에서 가져가주세요.")
+        }
+    }
     let barista: Person
     
-    init(sales: Int, menu: [Coffee: Int], pickUpTable: Bool, barista: Person) {
-        self.sales = sales
-        self.menu = menu
-        self.pickUpTable = pickUpTable
+    init(barista: Person) {
         self.barista = barista
     }
-
+    
     func takeOrder(coffee: Coffee, from person: Person) {
         sales += coffee.price
-        pickUpTable = true
-        makeCoffee(coffee: coffee, to: person, on: pickUpTable)
+        makeCoffee(coffee: coffee, to: person)
+        
     }
     
-    func makeCoffee(coffee: Coffee, to person: Person, on pickUpTable: Bool) {
-        let isCompleted = pickUpTable == true
-        if isCompleted {
-            print("\(person.name)님의 커피가 준비되었습니다. 픽업대에서 가져가주세요.")
-        } else {
-            print("잠시만 기다려주세요")
-        }
+    func makeCoffee(coffee: Coffee, to person: Person) {
+        customer = person.name
+        pickUpTable.append(coffee)
     }
 }
 
-var yagombucks = CoffeeShop(sales: 0, menu: [Coffee.espresso: Coffee.espresso.price, Coffee.americano: Coffee.americano.price, Coffee.latte: Coffee.latte.price], pickUpTable: false, barista: misterLee)
+var yagombucks = CoffeeShop(barista: misterLee)
 
 missKim.buy(coffee: Coffee.americano, from: yagombucks)
