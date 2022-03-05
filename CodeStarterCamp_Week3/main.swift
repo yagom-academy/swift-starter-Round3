244e553ffbef40b6a06e7708be7ba10212d1d54d
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2022-03-06 03:53:27 +0900

    Feat : CoffeeShop 클래스 생성
    - 매출액, 메뉴판, 바리스타, 픽업테이블을 프로퍼티로 갖는 CoffeeShop 클래스 생성
    - takeOrder 인스턴스 메서드로 주문을 받고 처리하는 동작 구현
    - 열거형의 Coffee타입을 String 타입의 원시값을 사용할 수 있게 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-enum Coffee {
+enum Coffee: String {
     case americano
     case cafeLatte
     case vanilaLatte
@@ -28,3 +28,50 @@ struct Person {
         }
     }
 }
+
+class CoffeeShop {
+    var sales: Int
+    var barista: Person!
+    let menuBoard: [Coffee: Int]
+    var pickUpTable: Bool {
+        willSet {
+            if newValue {
+                print("주문하신 커피가 완성되어 픽업대에 올려졌습니다!")
+            }
+        }
+    }
+    
+    init(amerianoPrice: Int, cafeLattePrice: Int, vanilaLattePrice: Int, cafeMocaPrice: Int,
+         chocoLattePrice: Int) {
+        sales = 0
+        menuBoard = [
+            .americano: amerianoPrice,
+            .cafeLatte: cafeLattePrice,
+            .vanilaLatte: vanilaLattePrice,
+            .cafeMoca: cafeMocaPrice,
+            .chocoLatte: chocoLattePrice
+        ]
+        pickUpTable = false
+    }
+    
+    func takeOrder(coffee: Coffee) {
+        let price = menuBoard[coffee] ?? 0
+        
+        if price != 0 {
+            print("\(coffee.rawValue)을 주문 받았습니다!")
+            sales += price
+            pickUpTable = true
+        } else {
+            print("메뉴에 없는 커피를 주문하셨네요...메뉴판을 잘 봐주세요!")
+        }
+    }
+}
+
+var misterLee: Person = Person(money: 10000)
+var missKim: Person = Person(money: 20000)
+
+var yagomBucks: CoffeeShop = CoffeeShop(amerianoPrice: 2000, cafeLattePrice: 2500, vanilaLattePrice: 3000, cafeMocaPrice: 3500, chocoLattePrice: 4000)
+
+yagomBucks.barista = misterLee
+yagomBucks.takeOrder(coffee: .americano)
+
