ab209f820c23bd0d1f3b5122a9eecfcc68ab30ca
sozohoy <sozohoy@gmail.com>
2022-03-06 18:37:40 +0900

    [IMPROVE] STEP2 프로퍼티 옵저버 적용, 미사용 프로퍼티 사용

--- step2/main.swift
+++ step2/main.swift
@@ -63,11 +63,15 @@ struct CoffeeShop{
     
     var cafeName: String
     var cafeProfit: Int
-    var pickUpTable: String
-    var barista: String
+    var pickUpTable: String = "주방" {
+        didSet {
+            print("커피는 \(oldValue)에서 만든 후, \(pickUpTable)에 올려 놓을게요!")
+        }
+    }
+    var barista: Person
     
-    let coffeeType = Coffee.allCases
-    let cafeMenu: [Coffee] = Coffee.allCases
+    let cafeMenu = Coffee.allCases.map({ "\($0)" }) .joined(separator: ", ")
+    
     
     mutating func order(coffee: Coffee, customer: inout Person){ // 주문시 missKim 가격 다운 & 야곰벅스 매출액 증가
 
@@ -81,14 +85,15 @@ struct CoffeeShop{
             
             cafeProfit += coffeePrice
             customer.money -= coffeePrice // missKim
-            makeCoffee(coffee: coffee, barista: barista, customer: customer)
+            makeCoffee(coffee: coffee, customer: customer)
             
         }
         
     }
     
-    mutating func makeCoffee(coffee: Coffee, barista: String, customer: Person){
-        print("\(coffee)는 바리스타 \(barista)님이 만들고 있습니다!")
+    mutating func makeCoffee(coffee: Coffee, customer: Person){
+
+        print("\(coffee)는 바리스타 \(barista.name)님이 만들고 있습니다!")
         print("\(customer.name) 님의 커피가 준비되었습니다! \(pickUpTable)에서 가져가주세요!")
         print("카페 이익 : \(cafeProfit), \(customer.name)님의 남은 돈 : \(customer.money)")
     }
@@ -99,7 +104,11 @@ var misterLee = Person(name: "misterLee", money: 15000)
 var missKim = Person(name: "missKim", money: 5000)
 var seok = Person(name: "seok", money: 3000)
 
-var yagombukcs = CoffeeShop(cafeName: "yagombucks", cafeProfit: 100000, pickUpTable: "카운터", barista: misterLee.name)
+var yagombucks = CoffeeShop(cafeName: "yagombucks", cafeProfit: 100000, barista: misterLee)
 
-yagombukcs.order(coffee: .americano, customer: &missKim)
-yagombukcs.order(coffee: .vanillaLatte, customer: &seok)
+print("**카페메뉴** \n[\(yagombucks.cafeMenu)]")
+
+yagombucks.pickUpTable = "픽업 테이블"
+
+yagombucks.order(coffee: .americano, customer: &missKim)
+yagombucks.order(coffee: .vanillaLatte, customer: &seok)
