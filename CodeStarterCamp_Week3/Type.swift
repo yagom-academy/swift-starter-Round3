ba3025e8caff99eaa4b586bb0c88e11dfa513adc
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2022-03-07 16:02:19 +0900

    Fix: CoffeeShop의 takeOrder 메서드와 Person의 buyStuff 메서드의 전반적인 변화
    - 각각의 money와 sales 프로퍼티들의 활용도를 높히기 위해 수정했습니다.
    - Person을 값 타입인 struct가 아닌 참조 타입인 class로 바꿈으로써 buyStuff에 의한 money 프로퍼티의 변화가 외부 scope에서도 적용될 수 있게 만들었습니다.

--- CodeStarterCamp_Week3/Type.swift
+++ CodeStarterCamp_Week3/Type.swift
@@ -8,36 +8,45 @@
 import Foundation
 
 enum Coffee: String {
-    case americano, cafeLatte, vanilaLatte, cafeMoca, chocoLatte
+    case americano, cafeLatte, vanilaLatte, cafeMoca, chocoLatte, greenTeaLatte
 }
 
-struct Person {
+class Person {
     var money: Int
+    let name: String
     
-    mutating func buyStuff(price: Int) {
+    init(money: Int, name: String) {
+        self.money = money
+        self.name = name
+    }
+    func buyStuff(price: Int) -> Bool {
         if money >= price {
-            print("\(price)를 내고 구매하였습니다!")
             money -= price
+            print("\(name): \(price)원 내겠습니다! (\(money)원 남았네...)")
+            return true
         } else {
-            print("열심히 일해서 돈을 더 법시다!")
+            print("\(name): 어라,, 돈이 없어서 못사네요...")
         }
+        return false
     }
 }
 
 class CoffeeShop {
     var sales: Int
-    var barista: Person!
+    var barista: Person?
     let menuBoard: [Coffee: Int]
+    let name: String
+    
     var pickUpTable: Bool {
         didSet {
             if pickUpTable {
-                print("주문하신 커피가 완성되어 픽업대에 올려졌습니다!")
+                print("\(name): 주문하신 커피가 완성되어 픽업대에 올려졌습니다!")
             }
         }
     }
     
     init(amerianoPrice: Int, cafeLattePrice: Int, vanilaLattePrice: Int, cafeMocaPrice: Int,
-         chocoLattePrice: Int) {
+         chocoLattePrice: Int, name: String) {
         sales = 0
         menuBoard = [
             .americano: amerianoPrice,
@@ -47,20 +56,30 @@ class CoffeeShop {
             .chocoLatte: chocoLattePrice
         ]
         pickUpTable = false
+        self.name = name
     }
     
-    func takeOrder(coffee: Coffee) {
+    func takeOrder(coffee: Coffee, customer: Person) {
         let price = menuBoard[coffee] ?? 0
         
+        print("""
+        \(name): 무엇을 주문하시겠습니까?
+        \(customer.name): \(coffee) 하나요!
+        """)
         if price != 0 {
-            print("\(coffee.rawValue)을 주문 받았습니다!")
-            sales += price
-            pickUpTable = true
+            if customer.buyStuff(price: price) {
+                print("\(name): \(coffee.rawValue) 주문 받았습니다! (만드는 중...)")
+                sales += price
+                pickUpTable = true
+            } else {
+                print("\(name): 다른 커피를 주문해주세요!")
+            }
         } else {
-            print("메뉴에 없는 커피를 주문하셨네요...메뉴판을 잘 봐주세요!")
+            print("\(name): 메뉴에 없는 커피를 주문하셨네요...메뉴판을 잘 봐주세요!")
         }
         if pickUpTable {
             pickUpTable = false
         }
+        print("")
     }
 }
