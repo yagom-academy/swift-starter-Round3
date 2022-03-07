6c7df8b77639e4edbd7539ca4a4735fa6586a8d5
Jinyoung Yoo <jinyoo@Jinyoungui-MacBookPro.local>
2022-03-07 14:13:04 +0900

    Fix : 파일 분리
    각각의 타입들을 Type.swift라는 파일에 따로 저장해서 main.swift에서는 코드 실행부만 존재하게 만들었습니다!

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,65 +8,6 @@
 
 import Foundation
 
-enum Coffee: String {
-    case americano
-    case cafeLatte
-    case vanilaLatte
-    case cafeMoca
-    case chocoLatte
-}
-
-struct Person {
-    var money: Int
-    
-    mutating func buyStuff(price: Int) {
-        if money >= price {
-            print("\(price)를 내고 구매하였습니다!")
-            money -= price
-        } else {
-            print("열심히 일해서 돈을 더 법시다!")
-        }
-    }
-}
-
-class CoffeeShop {
-    var sales: Int
-    var barista: Person!
-    let menuBoard: [Coffee: Int]
-    var pickUpTable: Bool {
-        willSet {
-            if newValue {
-                print("주문하신 커피가 완성되어 픽업대에 올려졌습니다!")
-            }
-        }
-    }
-    
-    init(amerianoPrice: Int, cafeLattePrice: Int, vanilaLattePrice: Int, cafeMocaPrice: Int,
-         chocoLattePrice: Int) {
-        sales = 0
-        menuBoard = [
-            .americano: amerianoPrice,
-            .cafeLatte: cafeLattePrice,
-            .vanilaLatte: vanilaLattePrice,
-            .cafeMoca: cafeMocaPrice,
-            .chocoLatte: chocoLattePrice
-        ]
-        pickUpTable = false
-    }
-    
-    func takeOrder(coffee: Coffee) {
-        let price = menuBoard[coffee] ?? 0
-        
-        if price != 0 {
-            print("\(coffee.rawValue)을 주문 받았습니다!")
-            sales += price
-            pickUpTable = true
-        } else {
-            print("메뉴에 없는 커피를 주문하셨네요...메뉴판을 잘 봐주세요!")
-        }
-    }
-}
-
 var misterLee: Person = Person(money: 10000)
 var missKim: Person = Person(money: 20000)
 
@@ -74,4 +15,3 @@ var yagomBucks: CoffeeShop = CoffeeShop(amerianoPrice: 2000, cafeLattePrice: 250
 
 yagomBucks.barista = misterLee
 yagomBucks.takeOrder(coffee: .americano)
-
