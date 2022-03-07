96993a11820ef45b7e10e25f23ae40bd7e0588ba
yeeton37 <lyeeun37@naver.com>
2022-03-07 19:04:41 +0900

    refactor: 함수 구현 순서 변경

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -3,7 +3,8 @@ import Foundation
 missKim.buyCoffee(.americano)
 
 if hasAsset(of: missKim, for: .americano) == true {
-    yagombucks.orderCoffee(.americano)
-    yagombucks.makeCoffee(.americano)
-    yagombucks.pickUp(by: missKim, coffee: .latte)
+     yagombucks.orderCoffee(.americano)
+     yagombucks.makeCoffee(.americano)
+     yagombucks.pickUp(by: missKim, coffee: .latte)
 }
+ 
