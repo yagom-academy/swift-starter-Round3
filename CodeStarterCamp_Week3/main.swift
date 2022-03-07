038240bb4c0d0aa129b4ebf0882e6e41f9f75a08
Juho Jung <jooho293@gmail.com>
2022-03-07 17:01:06 +0100

    Fix: fix code for instancce 인스턴스 코드 수정
    -

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -11,4 +11,5 @@ import Foundation
 
 var misterLee = Person(name: "misterLee", age: 25, money: 10000)
 var missKim = Person(name: "missKim", age: 27, money: 20000)
-var yagomBucks = CoffeeShop(totalRevenue: 0, menu: [Coffee.americano: 5000, Coffee.latte: 5500, Coffee.cappuccino: 5500], pickUpTable: "주문하신 \(Coffee.Type.self) 나왔습니다.", barista: misterLee)
+var yagomBucks = CoffeeShop(totalRevenue: 0, barista: misterLee)
+
