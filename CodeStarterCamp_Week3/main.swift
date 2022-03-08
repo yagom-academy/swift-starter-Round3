b191565acb412d9a15049f36ac6aaaafcd518c1e
uni <miju@Uui-MacBookAir.local>
2022-03-08 18:07:33 +0900

    coffeeShop 타입 구현, Coffee 종류 열거형으로 구현

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -22,3 +22,28 @@ class Person {
     }
 }
 
+class coffeeShop  {
+    var sales: Int = 0
+    var coffee: coffee
+    var price: Int
+    var barista: Person
+    
+    init(coffee: coffee, price: Int, barista: Person) {
+        self.coffee = coffee
+        self.price = price
+        self.barista = barista
+    }
+    
+    func orderCoffee() {
+        print("\(coffee) 주문 받았습니다.")
+    }
+    
+    func pickupTable(coffee: coffee) {
+        print("주문하신 \(coffee) 나왔습니다.")
+    }
+}
+
+enum coffee: String {
+    case latte = "라떼"
+    case americano = "아메리카노"
+}
