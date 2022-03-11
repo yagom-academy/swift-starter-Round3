73936dc72326a4fca3ac68cb4e8e6c28cfeaab72
SEO-YJ <tjdbwns19@naver.com>
2022-03-11 17:42:23 +0900

    feat: week3-step1 수정본 - 코드 스타터캠프 마지막 commit ㅠㅠ...

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -9,14 +9,14 @@ protocol AnimalAct {
 protocol AnimalBody {
     var height: Double { get set }
     var weight: Double { get set }
-    var eyes: Int { get }
-    var nose: Int { get }
-    var mouse: Int { get }
-    var ears: Int { get }
     var arms: Int { get }
+    var ears: Int { get }
+    var eyes: Int { get }
+    var feet: Int { get }
     var hands: Int { get }
     var legs: Int { get }
-    var feet: Int { get }
+    var mouse: Int { get }
+    var nose: Int { get }
     
     mutating func setBreakBody()
 }
@@ -44,21 +44,20 @@ struct PersonAct: AnimalAct {
 }
 
 struct PersonBody: AnimalBody {
-    
     var height: Double
     var weight: Double
-    var breakEyes: Int = 0
-    var breakEars: Int = 0
     var breakArms: Int = 0
+    var breakEars: Int = 0
+    var breakEyes: Int = 0
+    var breakFeet: Int = 0
     var breakHands: Int = 0
     var breakLegs: Int = 0
-    var breakFeet: Int = 0
-    var breakNose: Int = 0
     var breakMouse: Int = 0
+    var breakNose: Int = 0
     
-    var eyes: Int {
+    var arms: Int {
         get {
-            return 2 - breakEyes
+            return 2 - breakArms
         }
     }
     var ears: Int {
@@ -66,9 +65,14 @@ struct PersonBody: AnimalBody {
             return 2 - breakEars
         }
     }
-    var arms: Int {
+    var eyes: Int {
         get {
-            return 2 - breakArms
+            return 2 - breakEyes
+        }
+    }
+    var feet: Int {
+        get {
+            return 2 - breakFeet
         }
     }
     var hands: Int {
@@ -81,19 +85,14 @@ struct PersonBody: AnimalBody {
             return 2 - breakLegs
         }
     }
-    var feet: Int {
+    var mouse: Int {
         get {
-            return 2 - breakFeet
+            return 1 - breakMouse
         }
     }
     var nose: Int {
         get {
             return 1 - breakNose
-        }
-    }
-    var mouse: Int {
-        get {
-            return 1 - breakMouse
         }
     }
     
@@ -163,49 +162,16 @@ struct PersonInformation {
 class Person {
     var personInformation: PersonInformation
     var personBody: PersonBody
-    // personInformation, personBody 두 구조체를 외부에서 주입하였습니다.
-    // 변수로 선언한 이유는 구조체는 '값 타입'이므로 'Stack'에 저장되므로, let으로 선언할 경우 내부 프로퍼티의 값을 변경할 수 없기에
-    // var로 선언하였습니다!
-    // personBody구조체를 상위 프로토콜 타입을 만들어 사용한 이유는 '의존성 주입'때문입니다.
-    // 이를 통해, 구조체의 프로퍼티, 메소드가 변경되거나 제거될 경우,
-    // Person 클래스의 인스턴스에서 에러가 발생하는 것이 아닌, 프로토콜 타입의 구조체 인스턴스에서 에러가 발생하니,
-    // 어느 부분에서 에러가 발생한지 명확하게 파악할 수 있습니다.
-    
-    // personInformation의 프로토콜을 만들지 않은 이유는 프로토콜로 상위 타입을 만들 수 없다고 생각했기 때문입니다.
-    // 동물 -> 사람의 관계를 '상위' '하위 관계로 생각해보았을때.
-    // 동물은 정보를 가질 수야 있지만, 사람처럼 '정보'를 꼭 가지고 있지는 않다고 생각하여
-    // personInformation은 구조체만 선언하였습니다.
-    
-    private let personAct: PersonAct
-    // personAct 구조체 역시 외부에서 주입하였습니다.
-    // personAct구조체를 상위 프로토콜 타입을 만들어 사용한 이유는 '의존성 주입'때문입니다.
-    // 이를 통해, 구조체의 프로퍼티, 메소드가 변경되거나 제거될 경우,
-    // Person 클래스의 인스턴스에서 에러가 발생하는 것이 아닌, 프로토콜 타입의 구조체 인스턴스에서 에러가 발생하니,
-    // 어느 부분에서 에러가 발생한지 명확하게 파악할 수 있습니다.
-    
-    
+    let personAct: PersonAct
     
     init(personInformation: PersonInformation, personBody: PersonBody, personAct: PersonAct) {
         self.personInformation = personInformation
         self.personBody = personBody
         self.personAct = personAct
     }
-    // 생성자는 모두 외부에서 구조체의 인스턴스를 주입하는 방식으로 짜보았습니다.
-    // 이를 통해, 1. 매개변수의 갯수를 줄일 수 있다. 2. 분기문을 제거하였다. 3. 코드의 간결성이 높아진다. 를 생각하여 작성해 보았습니다.
-    
     
     deinit {
         print("\(self.personInformation.name)이 사망하셨습니다.")
-            }
-    
-    func see() {
-        if self.personBody.eyes > 0 {
-            print(self.personInformation.name, terminator: "")
-            self.personAct.see()
-        } else {
-            print("\(self.personInformation.name)이 보지 못합니다.")
-        }
-        
     }
     
     func breath() {
@@ -217,11 +183,19 @@ class Person {
         }
     }
     
+    func eat() {
+        if self.personBody.mouse > 0 {
+            print(self.personInformation.name, terminator: "")
+            self.personAct.eat()
+        } else {
+            print("\(self.personInformation.name)이 먹지 못합니다.")
+        }
+    }
+    
     func hear() {
         if self.personBody.ears > 0 {
             print(self.personInformation.name, terminator: "")
             self.personAct.hear()
-
         } else {
             print("\(self.personInformation.name)이 듣지 못합니다.")
         }
@@ -236,12 +210,12 @@ class Person {
         }
     }
     
-    func eat() {
-        if self.personBody.mouse > 0 {
+    func see() {
+        if self.personBody.eyes > 0 {
             print(self.personInformation.name, terminator: "")
-            self.personAct.eat()
+            self.personAct.see()
         } else {
-            print("\(self.personInformation.name)이 먹지 못합니다.")
+            print("\(self.personInformation.name)이 보지 못합니다.")
         }
     }
     
@@ -254,7 +228,7 @@ class Person {
     }
 }
 
-class CoffeeCustomer: Person {
+class Customer: Person {
     func buy(to things: [String: Int], _ thingsName: String) {
         guard let things = things[thingsName] else {
             print("물건이 없습니다.")
@@ -262,7 +236,7 @@ class CoffeeCustomer: Person {
         }
         print("\(self.personInformation.name)이 \(thingsName)을(를) 구매하였습니다!")
         self.personInformation.money -= things
-        print("현재 \(self.personInformation.name)이 보유한 돈은 \(self.personInformation.money))입니다.")
+        print("현재 \(self.personInformation.name)이 보유한 돈은 \(self.personInformation.money)원 입니다.")
     }
 }
 
@@ -276,25 +250,14 @@ class CoffeeShop {
         case espresso
         case hazlnutLatte
         case vienna
-        
-        /*
-        static let coffeeList = [americano, caffeeLatte, caffeMocha, cappuccino, caramelMacchiato, espresso, hazlnutLatte, vienna]
-         */
     }
     
-    var cafeName: String
+    let cafeName: String
     var barista: Person
-    
-    
-    
     var counterPosMachineMoney = 0
     var salesMoney = 100000
     var menu: [String: Int] = [:]
     var pickUpTable: [String: Int] = [:]
-
-    // 카페의 물건들은 setCafeObject 메소드를 이용해 초기화를 하는 방법으로 작성해보았습니다.
-    // lazy(지연 저장 프로퍼티)를 통해 카페의 물건을 세팅할 시점에 저장 프로퍼티 값을 초기화하도록 하였습니다.
-    // 중요: lazy 키워드는 생성자에서 초기화 하지 않기 때문에 선언 시점에 기본값을 저장해주어야 합니다.
     lazy var coffeeMachine: String = ""
     lazy var table: Int = 0
     lazy var chair: Int = 0
@@ -315,58 +278,118 @@ class CoffeeShop {
             self.coffeeMachine = coffeeMachineSelect
         }
         
-        print("책상 갯수:", terminator: " ")
-        let tableSelect = readLine()
-        if let tableSelect = tableSelect {
-            let tableInteger = Int(tableSelect)
-            if let tableInteger = tableInteger {
-                self.table = tableInteger
+        while true {
+            var loopIsNumberCount = 0
+            print("책상 갯수:", terminator: " ")
+            let tableSelect = readLine()
+            if let tableSelect = tableSelect {
+                for tableSelectChar in tableSelect {
+                    if !tableSelectChar.isNumber {
+                        print("다시 입력 바랍니다!")
+                        print()
+                        break
+                    } else {
+                        loopIsNumberCount += 1
+                    }
+                }
+                if loopIsNumberCount == tableSelect.count {
+                    self.table = Int(tableSelect)!
+                    print()
+                    break
+                }
             }
         }
         
-        print("의자 갯수:", terminator: " ")
-        let chairSelect = readLine()
-        if let chairSelect = chairSelect {
-            let chairInteger = Int(chairSelect)
-            if let chairInteger = chairInteger {
-                self.chair = chairInteger
+        while true {
+            var loopIsNumberCount = 0
+            print("의자 갯수:", terminator: " ")
+            let chariSelect = readLine()
+            if let chariSelect = chariSelect {
+                for chairSelectChar in chariSelect {
+                    if !chairSelectChar.isNumber {
+                        print("다시 입력 바랍니다!")
+                        print()
+                        break
+                    } else {
+                        loopIsNumberCount += 1
+                    }
+                }
+                if loopIsNumberCount == chariSelect.count {
+                    self.chair = Int(chariSelect)!
+                    print()
+                    break
+                }
             }
         }
         
-        print("컵 갯수:", terminator: " ")
-        let cupSelect = readLine()
-        if let cupSelect = cupSelect {
-            let cupInteger = Int(cupSelect)
-            if let cupInteger = cupInteger {
-                self.cup = cupInteger
+        while true {
+            var loopIsNumberCount = 0
+            print("컵 갯수:", terminator: " ")
+            let cupSelect = readLine()
+            if let cupSelect = cupSelect {
+                for cupSelectChar in cupSelect {
+                    if !cupSelectChar.isNumber {
+                        print("다시 입력 바랍니다!")
+                        print()
+                        break
+                    } else {
+                        loopIsNumberCount += 1
+                    }
+                }
+                if loopIsNumberCount == cupSelect.count {
+                    self.cup = Int(cupSelect)!
+                    print()
+                    break
+                }
             }
         }
         
-        print("빨대 갯수:", terminator: " ")
-        let strawSelect = readLine()
-        if let strawSelect = strawSelect {
-            let strawInteger = Int(strawSelect)
-            if let strawInteger = strawInteger {
-                self.straw = strawInteger
+        while true {
+            var loopIsNumberCount = 0
+            print("빨대 갯수:", terminator: " ")
+            let strawSelect = readLine()
+            if let strawSelect = strawSelect {
+                for strawSelectChar in strawSelect {
+                    if !strawSelectChar.isNumber {
+                        print("다시 입력 바랍니다!")
+                        print()
+                        break
+                    } else {
+                        loopIsNumberCount += 1
+                    }
+                }
+                if loopIsNumberCount == strawSelect.count {
+                    self.straw = Int(strawSelect)!
+                    print()
+                    break
+                }
             }
         }
+        
+        print("""
+              !*** 카페에 물건이 구비되었습니다 ***!
+              커피머신: \(self.coffeeMachine)
+              책: \(self.table)개
+              의자: \(self.chair)개
+              컵: \(self.cup)개
+              빨대: \(self.straw)개
+              """)
     }
     
-    
-    // isNumber 메소드를 사용하여, Character 타입의 값이 '숫자'가 아닌 다른 값일 경우에 대한 조건을 작성해보았다.
     func decideCoffeePrice() {
         var loopCount = 0
         let coffeeList = Coffee.allCases.map({ "\($0)" })
         
         while loopCount < coffeeList.count {
+            var loopIsNumberCount = 0
+            
             print("\'\(coffeeList[loopCount])\'의 가격을 숫자로 입력하여 결정하세요!")
             let coffeePrice = readLine()
+            
             guard let coffeePrice = coffeePrice else {
                 continue
             }
-                
-            var loopIsNumberCount = 0
-                
+            
             for coffeePriceChar in coffeePrice {
                 if !coffeePriceChar.isNumber {
                     print("다시 입력 바랍니다!")
@@ -409,6 +432,9 @@ class CoffeeShop {
     
     func decideCoffeeMenu(to customer: Person) {
         while true {
+            var coffeeCountInteger = 0
+            var loopIsNumberCount = 0
+            
             print("!!** 메뉴에서 \'X\'를 누르시면 결제가 진행됩니다 **!!")
             print("메뉴: ", terminator: " ")
             let coffeeName = readLine()
@@ -422,12 +448,11 @@ class CoffeeShop {
                 
             print("잔: ", terminator: " ")
             let coffeeCount = readLine()
-            var coffeeCountInteger = 0
-            var loopIsNumberCount = 0
             
             guard let coffeeName = coffeeName, let coffeeCount = coffeeCount else {
                 continue
             }
+            
             for coffeeCountChar in coffeeCount {
                 if !coffeeCountChar.isNumber {
                     print("다시 잔 수를 '숫자'로 입력 바랍니다!")
@@ -444,7 +469,7 @@ class CoffeeShop {
             
             menu.forEach { (menuName, price) in
                 if coffeeName == menuName {
-                    print("\(customer.personInformation.name)님이 \(coffeeName) 커피: \(coffeeCountInteger)잔을 골랐습니다!")
+                    print("\(customer.personInformation.name)님이 \(coffeeName) 커피 \(coffeeCountInteger)잔을 골랐습니다!")
                     calculateNowPrice(coffeeName: coffeeName, coffeeCount: coffeeCountInteger)
                     pickUpTable[coffeeName] = Int(coffeeCount)
                 }
@@ -492,29 +517,24 @@ class CoffeeShop {
         }
     }
     
-    func playCoffeeShopGame(_ customerPlayer: Person, _ coffeeShop: CoffeeShop) {
-        
+    func playCoffeeShopGame(customerPlayer: Person, coffeeShop: CoffeeShop) {
         decideCoffeePrice()
         order(to: barista, for: customerPlayer)
         calculateAllPricePay(to: coffeeShop, for: customerPlayer)
         makeCoffee(for: customerPlayer)
-        
     }
 }
 
+var personBodyMisterLee = PersonBody(height: 175, weight: 60)
+var personActMisterLee = PersonAct()
+var personInformationMisterLee = PersonInformation(name: "Lee", age: 24, socialSecurityNumber: "999999-2222222", residence: "서울", money: 100000)
 
-var personBodyLee = PersonBody(height: 175, weight: 60)
-var personActLee = PersonAct()
-var personInformationLee = PersonInformation(name: "Lee", age: 24, socialSecurityNumber: "999999-2222222", residence: "서울", money: 100000)
+var personBodyMissKim = PersonBody(height: 160, weight: 50)
+var personActMissKim = PersonAct()
+var personInformationMissKim = PersonInformation(name: "Kim", age: 30, socialSecurityNumber: "999999-1111111", residence: "경기도", money: 200000)
 
-var personBodyKim = PersonBody(height: 160, weight: 50)
-var personActKim = PersonAct()
-var personInformationKim = PersonInformation(name: "Kim", age: 30, socialSecurityNumber: "999999-1111111", residence: "경기도", money: 200000)
+let misterLee = Person(personInformation: personInformationMisterLee, personBody: personBodyMisterLee, personAct: personActMisterLee)
+let missKim = Person(personInformation: personInformationMissKim, personBody: personBodyMissKim, personAct: personActMissKim)
+let yagombucks = CoffeeShop(cafeName: "yagombucks", barista: misterLee)
 
-var misterLee = Person(personInformation: personInformationLee, personBody: personBodyLee, personAct: personActLee)
-var missKim = Person(personInformation: personInformationKim, personBody: personBodyKim, personAct: personActKim)
-
-var yagombucks = CoffeeShop(cafeName: "yagombucks", barista: misterLee)
-
-yagombucks.playCoffeeShopGame(missKim, yagombucks)
-
+yagombucks.playCoffeeShopGame(customerPlayer: missKim, coffeeShop: yagombucks)
