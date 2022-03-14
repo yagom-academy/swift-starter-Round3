06b3a85b4f6bf6b8298dcf88f3832dbb05c22764
Juho Jung <jooho293@gmail.com>
2022-03-14 10:03:39 +0100

    Style: fix order funtion and class change to struct
    -

--- CodeStarterCamp_Week3/coffeeShop.swift
+++ CodeStarterCamp_Week3/coffeeShop.swift
@@ -7,45 +7,7 @@
 
 import Foundation
 
-
-class CoffeeShop {
-    var totalRevenue: Int
-    var menu: [Coffee: Int] = [.americano: 5000, .latte: 5500, .cappuccino: 5500]
-    var barista: Person
-    var pickUpTable: String? {
-        didSet {
-            if let pickUpTable = pickUpTable {
-                print("\(pickUpTable)가 준비되었습니다. 픽업대에서 가져가주세요.")
-            } else {
-                return
-            }
-        }
-    }
-    
-    init(totalRevenue: Int, menu: [Coffee: Int] = [.americano: 5000, .latte: 5500, .cappuccino: 5500], barista: Person) {
-        self.totalRevenue = totalRevenue
-        self.menu = menu
-        self.barista = barista
-    }
-    
-    func takeOrder(coffee: Coffee, person: Person) {
-        person.order(coffee: coffee)
-        if person.money < coffee.price {
-            print("잔액이 \(coffee.price - person.money)만큼 부족합니다.")
-        } else {
-            print("네 \(coffee)주문 받았습니다.")
-            person.money -= coffee.price
-            totalRevenue += coffee.price
-            takeOutCoffee(coffee: coffee)
-        }
-    }
-    
-    func takeOutCoffee(coffee: Coffee) {
-        pickUpTable = String("\(coffee)")
-    }
-    
-}
-
+// 열거형은 파일의 최상단에 있으면 가독성이 높습니다 :)
 enum Coffee {
     case americano
     case latte
@@ -61,5 +23,39 @@ enum Coffee {
             return 5500
         }
         
+    }
+}
+
+struct CoffeeShop {
+    var totalRevenue: Int
+    var menu: [Coffee: Int] = [.americano: 5000, .latte: 5500, .cappuccino: 5500]
+    var barista: Person
+    var pickUpTable: String? {
+        didSet {
+            if let pickUpTable = pickUpTable {
+                print("\(pickUpTable)가 준비되었습니다. 픽업대에서 가져가주세요.")
+            }
+        }
+    }
+
+    mutating func takeOrder(coffee: Coffee, person: Person) {
+        // 커피 주문 후 결제가 가능한지 확인하기 때문에, 주문한 커피를 말하고자 한다면 여기가 적당해보여요!
+        print("\(coffee) 주세요")
+        
+        if person.money < coffee.price {
+            print("잔액이 \(coffee.price - person.money)만큼 부족합니다.")
+        } else {
+            // 이 곳에서 가격만 넘겨줍니다.
+            person.order(price: coffee.price)
+            totalRevenue += coffee.price
+            
+            // 단순한 print더라도 언제 coffee를 주문 받았는지도 순서를 생각해보면 좋아요! 더 나아가면 함수 호출의 순서로 언제든 생각해볼 수 있습니다 :)
+            print("네 \(coffee) 주문 받았습니다.")
+            takeOutCoffee(coffee: coffee)
+        }
+    }
+    
+    mutating func takeOutCoffee(coffee: Coffee) {
+        pickUpTable = String("\(coffee)")
     }
 }
--- CodeStarterCamp_Week3/person.swift
+++ CodeStarterCamp_Week3/person.swift
@@ -17,10 +17,17 @@ class Person {
         self.age = age
         self.money = money
     }
+    // juho님 기존 코드
+//    func order(coffee: Coffee) {
+//        print("\(coffee) 주세요")
+//    }
     
-    func order(coffee: Coffee) {
-        print("\(coffee) 주세요")
+    // 매개변수를 현재는 Coffee 타입을 받고 있는데요.
+    // 재사용 가능한 함수를 위해, 가격만 받으려면 Int 로 바꿔주면 좋을 것 같아요.
+    func order(price: Int) {
+        self.money -= price
     }
 }
+
 
 
