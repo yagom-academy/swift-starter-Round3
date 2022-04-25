26c40e8b2081697d61ffda39c460d3abc21ef886
wolframhwang <hzw94@naver.com>
2022-04-26 08:00:51 +0900

    [feat] Mission3: Step1: CoffeeShop 생성
    커피샵의 클래스를 생성했습니다.
    1. 기본적으로 커피샵은 바리스타를 보유하고있는 형태이기 때문에 프로퍼티로 바리스타를 가집니다.
    2. 커피메뉴도 주문에 대한 금액이나, 그리고 매칭을 해야할 책임이 있어서, 클래스로 정의할 예정입니다.
    3. pickUpTable은 주문 받은 커피가 완성 될 경우, 커피받아야할 책임이 있기 때문에 클래스로 정의했고, 가게 마다 픽업 테이블은 처음부터 초기화 될 것 이라고 생각해서 미리 초기화를 해뒀습니다.

---
+++ CodeStarterCamp_Week3/CoffeeShop.swift
@@ -0,0 +1,33 @@
+//
+//  CoffeeShop.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 황지웅 on 2022/04/26.
+//
+
+import Foundation
+
+class CoffeeShop {
+    var barista: Person? = nil
+    var coffeeMenu: CoffeeMenu
+    var todaysSalesMoney: Int
+    var pickUpTable: PickUpTable = PickUpTable()
+    
+    init(coffeeMenu: CoffeeMenu) {
+        todaysSalesMoney = 0
+        self.coffeeMenu = coffeeMenu
+    }
+    
+    func hireBarista(barista: Person) {
+        self.barista = barista
+    }
+    
+    func depositMoney(coffeePrice: Int) {
+    }
+    
+    func insertOrderToPickUpTable(orderMan: String, coffee: Coffee) {
+    }
+    
+    func order(coffee: Coffee) {
+    }
+}
