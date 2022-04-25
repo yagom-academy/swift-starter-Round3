a584b6737b7e3c06529ed17083e51d172771f220
wolframhwang <hzw94@naver.com>
2022-04-26 08:09:01 +0900

    [feat] Mission3: Step1: PickUpTable 클래스 생성
    PickUpTable에 대한 클래스를 생성했습니다.
    프로퍼티로 pickUpTable을 가지고있는 형태인데, Coffee 를 Key로, 주문자들의 Name을 value로 한 구조입니다.
    주문을 받으면 pickUpTable에 Coffee Key와,이름 value로 pickUpTable에 달아두는 형태로 설계했습니다.

---
+++ CodeStarterCamp_Week3/PickUpTable.swift
@@ -0,0 +1,21 @@
+//
+//  PickUpTable.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 황지웅 on 2022/04/26.
+//
+
+import Foundation
+
+class PickUpTable {
+    private var pickUpTable: [Coffee: [String]] = [:]
+    
+    func checkTable(coffee: Coffee) -> Bool {
+    }
+    
+    func releasePickUpTable(coffee: Coffee) {
+    }
+    
+    func insertOrderToPickUpTable(name: String, coffee: Coffee) {
+    }
+}
