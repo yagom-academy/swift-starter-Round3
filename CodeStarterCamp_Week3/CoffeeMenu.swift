d3546e796f16319a5d7da9bc4f791cf8f32ef7a8
wolframhwang <hzw94@naver.com>
2022-04-26 08:05:31 +0900

    [feat] Mission3: Step1: CoffeeMenu 클래스 추가
    커피 메뉴관련 클래스를 추가했습니다.
    클래스에는 커피샵에 있는 커피 리스트를 보여줄수 있는 저장 프로퍼티를 보유하고있습니다.
    이 클래스는 getPriceOfCoffee라는 메소드를 통해, 메뉴가 보유하고있는 커피의 매칭된 "금액" 을 알려줘야 하는 책임이 있습니다

---
+++ CodeStarterCamp_Week3/CoffeeMenu.swift
@@ -0,0 +1,24 @@
+//
+//  CoffeeMenu.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 황지웅 on 2022/04/26.
+//
+
+import Foundation
+
+class CoffeeMenu {
+    private var coffeeListsInShop: [Coffee: Int] = [:]
+    
+    init(coffeeList: [Coffee: Int]) {
+        coffeeListsInShop = coffeeList
+    }
+    
+    func getPriceOfCoffee(coffee: Coffee) -> Int {
+        guard let coffeePrice = coffeeListsInShop[coffee] else {
+            return -1
+        }
+        
+        return coffeePrice
+    }
+}
