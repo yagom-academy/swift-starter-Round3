0b704002b62c476533ee6dea89e86ba85433365c
wolframhwang <hzw94@naver.com>
2022-04-26 07:55:57 +0900

    [feat] Mission3: Step1: Person 에 대한 정의
    1. 공통적인 프로퍼티로 Name / Money 를 선정했습니다.
    2. 추가될 예정이지만, 공통적으로 쓸수 있는 메소드로 orderCoffee 라는 메소드를 파라미터와 함께 정의했습니다.

---
+++ CodeStarterCamp_Week3/Person.swift
@@ -0,0 +1,24 @@
+//
+//  Person.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 황지웅 on 2022/04/26.
+//
+
+import Foundation
+
+class Person {
+    private var name: String
+    private var money: Int
+    
+    init(name: String, money: Int) {
+        self.name = name
+        self.money = money
+    }
+    
+    func submitMoney(coffeePrice: Int) {
+    }
+    
+    func orderCoffee(coffee: Coffee, coffeeShop: CoffeeShop) {
+    }
+}
