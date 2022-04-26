d3912a079041e2f8c609f178aeb0d5045ab3c0b3
Dylan-yoon <d.dylany21@gmail.com>
2022-04-26 15:51:33 +0900

    refactor: CoffeeShop StructTpye menuCoffee 매뉴를 입력해야하므로 빈 Dictionary로 변경

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -32,24 +32,7 @@ struct CoffeeShop {
     var isFranchise: Bool
     var isPickUpTable: Bool
     var totalSale: Int
-    var menuCoffee = [
-        "에스프레소": 4000,
-        "아메리카노": 4500,
-        "콜드 브루": 4900,
-        "카페 라떼": 5000,
-        "카라멜 마키아또": 5900,
-        "화이트 초콜릿 모카": 5900,
-        "돌체라떼": 5900,
-        
-        "더블 에스프레소 칩 프라푸치노": 6300,
-        "자바 칩 프라푸치노": 6300,
-        "초콜릿 크림 칩 프라푸치노": 6000,
-        "카라멜 프라푸치노": 5900,
-        
-        "펀치 그래피티 블렌디드": 6300,
-        "롤린 민트 초코 콜드 브루": 6100,
-        "포멜로 플로우 그린티": 6100
-    ]
+    var menuCoffee = [String: Int]()
     
     func acceptOrderCoffee() {
         
