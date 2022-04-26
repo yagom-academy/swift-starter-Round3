1ccd23b9fb1b4be77d683e84cbd894d3831937d8
Onestar <onestar7984@gmail.com>
2022-04-26 19:28:35 +0900

    feat: Person Struct 구현. 이름, 성별, 나이, 키, 몸무게, 돈을 프로퍼티로 정의 인사, 자기소개, 나이증가, 가진 돈 계산 메서드 정의 이니셜라이징 정의.

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,46 @@
 
 import Foundation
 
-print("Hello, World!")
+enum Gender: String {
+    case men = "남자"
+    case women = "여자"
+}
 
+struct Person {
+    let name: String
+    var age: Int
+    let gender: Gender
+    var hight: Int
+    var weight: Int
+    var money: Int = 0
+    
+    mutating func greet() -> String {
+        return "안녕하세요!!!"
+    }
+    mutating func introduce() -> String {
+        return "저의 이름은 \(self.name)입니다. 저는 \(self.age)살이고 키는 \(self.hight)cm 몸무게는 \(self.weight)kg 입니다."
+    }
+    mutating func addOneToAge() {
+        self.age += 1
+    }
+    mutating func calculateMoney(by amount: Int, sign: Int) -> Bool {
+        if sign == -1 {
+            if self.money < amount { return false }
+            self.money -= amount
+        } else {
+            self.money += amount
+        }
+        return true
+    }
+    
+    init?(name: String, age: Int, gender: Gender, hight: Int, weight: Int) {
+        if name.isEmpty || !(0...120).contains(age) || hight <= 0 || weight <= 0 {
+            return nil
+        }
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.hight = hight
+        self.weight = weight
+    }
+}
