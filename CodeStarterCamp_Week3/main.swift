397ad40ad5e6efe41dc3646744fc89bdce542329
jeongmin <lj845854@gmail.com>
2022-04-27 03:05:29 +0900

    add : 인스턴스 생성, 프로퍼티 값 할당

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,8 @@
 
 import Foundation
 
-print("Hello, World!")
+var misterLee = Person(name: "misterLee", height: 180.0, weight: 70.0, money: 100_000)
+var missKim = Person(name: "missKim", height: 165.0, weight: 55.0, money: 200_000)
 
+var yagombucks = CoffeeShop(owner: "yagom", location: "seoul", pickUpTable: true)
+yagombucks.barista = misterLee
