02cdd9bc70fb09f2f82d7aa8ee36a883dd4e5df6
JAY-Winter <rubat0@naver.com>
2022-04-28 13:11:06 +0900

    feat: customer 커피구매<-> coffeeShop order 기능 구현

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -20,17 +20,25 @@ class Person {
 }
 
 class Customer: Person {
+    
     func buyDrink(at coffeeShop: CoffeeShop, which menuName: String){
         guard let menu = coffeeShop.menuList[menuName] else { return }
         guard var buget = self.buget else { return }
+        
         switch menu {
         case .americano(let price), .vanillaLatte(let price), .latte(let price), .einspanner(let price):
-            buget -= price
-            self.buget = buget
+            if buget < price {
+                print("잔액이 \(price)원 만큼 부족합니다")
+            } else {
+                let 커피샵 = coffeeShop
+                커피샵.order(menu, who: self)
+                
+                buget -= price
+                self.buget = buget
+            }
         }
     }
 }
-
 
 enum Coffee {
     case americano(price: Int)
@@ -40,7 +48,7 @@ enum Coffee {
 }
 
 class CoffeeShop {
-    var revenue: Int = 0
+    var revenue: Int?
     var barista: Person?
     var pickUpTable: Array<String?> = []
     var menuList: Dictionary<String, Coffee> = [
@@ -49,11 +57,33 @@ class CoffeeShop {
         "Latte": .latte(price: 4000),
         "Vanilla Latte" : .vanillaLatte(price: 4500)
     ]
-
-    func order(_ coffee: Coffee) { }
+    
+    func order(_ coffee: Coffee, who order: Person) {
+        
+        if var revenue = self.revenue {
+            switch coffee {
+            case .americano(let price), .vanillaLatte(let price), .latte(let price), .einspanner(let price):
+                revenue += price
+                self.revenue = revenue
+            }
+        }
+        
+    }
     
     func makeCoffee() { }
 }
 
 // MARK: - 실행부
+
+
+let shop = CoffeeShop()
+shop.revenue = 0
+let 주문자 = Customer(name: "재현")
+주문자.buget = 4600
+
+print("주문자.buget : \(주문자.buget!)")
+print("shop.revenue : \(shop.revenue!)")
+주문자.buyDrink(at: shop, which: "Americano")
+print("주문자.buget : \(주문자.buget!)")
+print("shop.revenue : \(shop.revenue!)")
 
