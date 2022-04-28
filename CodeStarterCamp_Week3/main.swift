bfb6a5a834f23ba6e4bfb83146312a8adce08e46
JAY-Winter <rubat0@naver.com>
2022-04-28 22:10:57 +0900

    refactor: Coffee 연관값 추가에 따른 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -20,70 +20,52 @@ class Person {
 }
 
 class Customer: Person {
-    
-    func buyDrink(at coffeeShop: CoffeeShop, which menuName: String){
+    func buyDrink(at coffeeShop: CoffeeShop, which menuName: String) {
         guard let menu = coffeeShop.menuList[menuName] else { return }
         guard var buget = self.buget else { return }
         
         switch menu {
-        case .americano(let price), .vanillaLatte(let price), .latte(let price), .einspanner(let price):
-            if buget < price {
-                print("잔액이 \(price)원 만큼 부족합니다")
-            } else {
-                let 커피샵 = coffeeShop
-                커피샵.order(menu, who: self)
-                
+        case .americano(let price, _), .vanillaLatte(let price, _), .latte(let price, _), .einspanner(let price, _):
+            if buget >= price {
+                let coffeeShop = coffeeShop
+                coffeeShop.order(menu, who: self)
                 buget -= price
                 self.buget = buget
+            } else {
+                print("잔액이 \(price)원만큼 부족합니다")
             }
         }
     }
 }
 
 enum Coffee {
-    case americano(price: Int)
-    case latte(price: Int)
-    case vanillaLatte(price: Int)
-    case einspanner(price: Int)
+    case americano(price: Int, kr: String)
+    case latte(price: Int, kr: String)
+    case vanillaLatte(price: Int, kr: String)
+    case einspanner(price: Int, kr: String)
 }
+
 
 class CoffeeShop {
     var revenue: Int?
     var barista: Person?
     var pickUpTable: Array<String?> = []
     var menuList: Dictionary<String, Coffee> = [
-        "Americano": .americano(price: 3500) ,
-        "Einspanner": .einspanner(price: 5500),
-        "Latte": .latte(price: 4000),
-        "Vanilla Latte" : .vanillaLatte(price: 4500)
+        "Americano": .americano(price: 3500, kr :"아메리카노"),
+        "Einspanner": .einspanner(price: 5500, kr: "아인슈페너"),
+        "Latte": .latte(price: 4000, kr: "라떼"),
+        "Vanilla Latte" : .vanillaLatte(price: 4500, kr: "바닐라 라떼")
     ]
     
     func order(_ coffee: Coffee, who order: Person) {
+        guard var revenue = self.revenue else { return }
         
-        if var revenue = self.revenue {
-            switch coffee {
-            case .americano(let price), .vanillaLatte(let price), .latte(let price), .einspanner(let price):
-                revenue += price
-                self.revenue = revenue
-            }
+        switch coffee {
+        case .americano(let price, let kr), .vanillaLatte(let price, let kr), .latte(let price, let kr), .einspanner(let price, let kr):
+            revenue += price
+            self.revenue = revenue
         }
-        
     }
     
     func makeCoffee() { }
 }
-
-// MARK: - 실행부
-
-
-let shop = CoffeeShop()
-shop.revenue = 0
-let 주문자 = Customer(name: "재현")
-주문자.buget = 4600
-
-print("주문자.buget : \(주문자.buget!)")
-print("shop.revenue : \(shop.revenue!)")
-주문자.buyDrink(at: shop, which: "Americano")
-print("주문자.buget : \(주문자.buget!)")
-print("shop.revenue : \(shop.revenue!)")
-
