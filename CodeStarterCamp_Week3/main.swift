e24a114255241edcbc35f4661f97c5630bbb8ace
Onestar <onestar7984@gmail.com>
2022-04-28 12:35:05 +0900

    style: 오타수정 및 네이밍수정.

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -13,27 +13,30 @@ enum Gender: String {
     case women = "여자"
 }
 
-enum Coffe: String {
-    case americano, espresso, cafeLatte, coldBrew, milkTea, lomonAde, earlGrey
+enum Coffee: String {
+    case americano, espresso, cafeLatte, coldBrew, milkTea, lemonAde, earlGrey
 }
 
 struct Person {
     let name: String
     var age: Int
     let gender: Gender
-    var hight: Int
+    var height: Int
     var weight: Int
     var money: Int = 0
     
     mutating func greet() -> String {
         return "안녕하세요!!!"
     }
+    
     mutating func introduce() -> String {
-        return "저의 이름은 \(self.name)입니다. 저는 \(self.age)살이고 키는 \(self.hight)cm 몸무게는 \(self.weight)kg 입니다."
+        return "저의 이름은 \(self.name)입니다. 저는 \(self.age)살이고 키는 \(self.height)cm 몸무게는 \(self.weight)kg 입니다."
     }
+    
     mutating func addOneToAge() {
         self.age += 1
     }
+    
     mutating func calculateMoney(by amount: Int, sign: Int) -> Bool {
         if sign == -1 {
             if self.money < amount { return false }
@@ -44,77 +47,82 @@ struct Person {
         return true
     }
     
-    init?(name: String, age: Int, gender: Gender, hight: Int, weight: Int) {
-        if name.isEmpty || !(0...120).contains(age) || hight <= 0 || weight <= 0 {
+    init?(name: String, age: Int, gender: Gender, height: Int, weight: Int) {
+        if name.isEmpty || !(0...120).contains(age) || height <= 0 || weight <= 0 {
             return nil
         }
         self.name = name
         self.age = age
         self.gender = gender
-        self.hight = hight
+        self.height = height
         self.weight = weight
     }
 }
 
-struct CoffeShop {
-    let cafeName: String
-    var adress: String
+struct CoffeeShop {
+    let name: String
+    var address: String
     var sales = 0
-    var menu: [Coffe: Int]
-    var pickUpTable: Coffe?
+    var menu: [Coffee: Int]
+    var pickUpTable: Coffee?
     var barista: Person?
     
     mutating func greetToCustomer() -> String {
-        return "어서오세요!! \(cafeName)입니다!!"
+        return "어서오세요!! \(self.name)입니다!!"
     }
+    
     mutating func introduceCoffeShop() -> String {
-        return "저희 \(cafeName)는(은) \(adress)에 위치한 카페입니다!!!"
+        return "저희 \(self.name)는(은) \(self.address)에 위치한 카페입니다!!!"
     }
+    
     mutating func introduceMenu() -> String {
-        var menuContents = "\(cafeName) 메뉴판\n"
-        if !menu.isEmpty {
-            for (coffe, price) in self.menu {
-                menuContents += "\(coffe.rawValue) : \(price)원\n"
+        var menuContents = "\(self.name) 메뉴판\n"
+        if !self.menu.isEmpty {
+            for (coffee, price) in self.menu {
+                menuContents += "\(coffee.rawValue) : \(price)원\n"
             }
         } else {
             menuContents = "죄송합니다! 현재 오픈 준비중 입니다."
         }
         return menuContents
     }
-    mutating func takeAnOrder(_ coffe: Coffe) {
-        if let _ = menu[coffe] {
-            print("\(coffe)이 주문이 접수되었습니다.")
+    
+    mutating func order(_ coffee: Coffee) {
+        if let _ = menu[coffee] {
+            print("\(coffee)이 주문이 접수되었습니다.")
         } else {
             print("죄송합니다. 주문하신 메뉴는 저희 가게에서 판매하지 않습니다.")
         }
     }
-    mutating func makeCoffe(_ coffe: Coffe) {
+    
+    mutating func makeCoffee(_ coffee: Coffee) {
         if let _ = self.barista {
-            print("주문하신 \(coffe)가 준비되었습니다.")
+            print("주문하신 \(coffee)가 준비되었습니다.")
         } else {
             print("죄송합니다. 현재 바리스타가 출근하지 않았습니다.")
         }
     }
-    mutating func upsertCoffe(coffe: Coffe, price: Int) {
-        if let oldprice = self.menu.updateValue(price, forKey: coffe){
-            print("\(coffe)의 가격이 \(oldprice)에서 \(price)로 변경되었습니다.")
+    
+    mutating func upsertCoffee(coffee: Coffee, price: Int) {
+        if let oldPrice = self.menu.updateValue(price, forKey: coffee){
+            print("\(coffee)의 가격이 \(oldPrice)에서 \(price)로 변경되었습니다.")
         } else {
-            print("가격이 \(price)인 \(coffe)가 추가되었습니다.")
+            print("가격이 \(price)인 \(coffee)가 추가되었습니다.")
         }
     }
     
-    init?(cafeName: String, adress: String) {
-        if cafeName.isEmpty || adress.isEmpty {
+    init?(name: String, address: String) {
+        if name.isEmpty || address.isEmpty {
             return nil
         }
-        self.cafeName = cafeName
-        self.adress = adress
-        menu = [Coffe: Int]()
+        self.name = name
+        self.address = address
+        menu = [Coffee: Int]()
     }
 }
 
-var misterLee = Person(name: "이병현", age: 23, gender: Gender.men, hight: 183, weight: 84)
-var missKim = Person(name: "김태희", age: 21, gender: Gender.women, hight: 168, weight: 48)
-var yagombuks = CoffeShop(cafeName: "Yagom Bucks", adress: "서울시 강남구 강남대로 390")
-yagombuks?.barista = misterLee
+var misterLee = Person(name: "이병현", age: 23, gender: Gender.men, height: 183, weight: 84)
+var missKim = Person(name: "김태희", age: 21, gender: Gender.women, height: 168, weight: 48)
+var yagombucks = CoffeeShop(name: "Yagom Bucks", address: "서울시 강남구 강남대로 390")
+yagombucks?.barista = misterLee
 
