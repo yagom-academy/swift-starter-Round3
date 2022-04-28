5985db60d0c6ad49867706e971fc8a34a7b3f6b7
홍승주 <hongseungju@hongseungjuui-MacBookAir.local>
2022-04-28 20:09:45 +0900

    CoffeeShop

--- CodeStarterCamp_Week3/CoffeeShop.swift
+++ CodeStarterCamp_Week3/CoffeeShop.swift
@@ -6,3 +6,16 @@
 //
 
 import Foundation
+
+struct CoffeeShop {
+   var sales: Int?
+    var pickUpTable: Coffee?
+   var barista: Person?
+   
+   func getOrder() {
+        print("어떤 커피를 드릴까요?")
+    }
+    func makeCoffee() {
+      print("커피를 만들고 있습니다. 잠시만 기다려 주세요.")
+    }
+}
