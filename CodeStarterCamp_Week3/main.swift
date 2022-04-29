62933055914a62d30c2737bb93f3956a0c3a3c20
ssyoun4092 <ssyoun4092@naver.com>
2022-04-29 19:54:53 +0900

    yagombucks 생성, 바리스타 할당, CoffeeShop 클래스 대폭 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -17,22 +17,28 @@ class Person {
 }
 
 class CoffeeShop {
-    var sales: Int
-    let coffeeMenu: Coffee
-    let price: Int
-    let pickUpTable: Bool
-    let barista: Person
+    var sales: Int = 0
+    var coffeeMenuWithPrice: [CoffeeMenu: Int]
+    var barista: Person
+    var pickUpTable: CoffeeMenu? {
+        didSet {
+            if let menu = self.pickUpTable {
+                print("주문하신 \(menu.rawValue) 픽업테이블 위에 있습니다~")
+            }
+        }
+    }
 
-    init(sales: Int, coffeeMenu: Coffee, price: Int, pickUpTable: Bool, barista: Person) {
-        self.sales = sales
-        self.coffeeMenu = coffeeMenu
-        self.price = price
-        self.pickUpTable = pickUpTable
+    init(coffeeMenuWithPrice: [CoffeeMenu: Int], barista: Person) {
+        self.coffeeMenuWithPrice = coffeeMenuWithPrice
         self.barista = barista
     }
 
-    func getOrder() {
-        print("get Order")
+    func getOrder(menu: CoffeeMenu, size: Size, iceOrHot: IceOrHot, quantity: Int) {
+        var price: Int = 0
+        if let defaultPrice = coffeeMenuWithPrice[menu] {
+            price =  (defaultPrice + size.rawValue + iceOrHot.rawValue) * quantity
+        }
+        print("감사합니다 주문하신 음료 총 가격은 \(price)원 입니다~")
     }
 
     func makeCoffee() {
@@ -40,11 +46,24 @@ class CoffeeShop {
     }
 }
 
-enum Coffee: String {
-    case americano = "Americano"
-    case capouccino = "Cappuccino"
-    case latte = "Latte"
+enum CoffeeMenu: String {
+    case americano = "아메리카노"
+    case capouccino = "카푸치노"
+    case latte = "라떼"
+}
+
+enum IceOrHot: Int {
+    case ice = 300
+    case hot = 0
+}
+
+enum Size: Int {
+    case small = 0
+    case medium = 500
+    case large = 1000
 }
 
 let misterLee = Person(name: "Lee", age: 26, money: 10000)
 let missKim = Person(name: "Kim", age: 31, money: 15000)
+
+let yagombucks = CoffeeShop(coffeeMenuWithPrice: [.americano: 4500, .capouccino: 4800, .latte: 5000], barista: misterLee)
