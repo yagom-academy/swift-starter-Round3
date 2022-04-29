8fe7a063619ca31f2882943cd72897209f07d4df
wolframhwang <hzw94@naver.com>
2022-04-29 21:11:07 +0900

    [feat] Mission3: Step2: 메인시나리오
    1. 미스김, 미스터리가 생성됩니다.
    2. coffeeMenu가 생성되고 이를 바탕으로 coffeeShop이 생성됩니다.
    3. coffeeShop은 바리스타로 misterLee를 고용합니다.
    4. 그리고 missKim은 coffee를 coffeeShop을 통해 주문합니다.

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -9,13 +9,14 @@
 import Foundation
 
 
-let missKim: Person = Person(name: "missKim", money: 1000)
-let misterLee: Person = Person(name: "misterLEe", money: 2000)
+let missKim: Person = Person(name: "missKim", money: 5000)
+let misterLee: Barista = Barista(name: "misterLEe", money: 2000)
 
 let coffeeMenu = CoffeeMenu(coffeeList: [.americano: 3000, .espresso: 1000, .caffeelatte: 4000, .deCaffeine: 4000])
 let coffeeShop: CoffeeShop = CoffeeShop(coffeeMenu: coffeeMenu)
 
 coffeeShop.hireBarista(barista: misterLee)
 
+missKim.orderCoffee(coffee: .americano, coffeeShop: coffeeShop)
 
 
