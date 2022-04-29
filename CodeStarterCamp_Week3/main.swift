80a255150de07f3da8892054f7a7b9b2dbb24ce5
ssyoun4092 <ssyoun4092@naver.com>
2022-04-29 10:38:09 +0900

    feat: Step1 CoffeeShop 타입 생성

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -11,3 +11,25 @@ class Person {
         print("bought Coffee")
     }
 }
+
+class CoffeeShop {
+    var sales: Int
+    let coffeeOptions: String
+    let price: Int
+    let pickUpTable: Bool
+
+    init(sales: Int, coffeeOptions: String, price: Int, pickUpTable: Bool) {
+        self.sales = sales
+        self.coffeeOptions = coffeeOptions
+        self.price = price
+        self.pickUpTable = pickUpTable
+    }
+
+    func getOrder() {
+        print("get Order")
+    }
+
+    func makeCoffee() {
+        print("making Coffee")
+    }
+}
