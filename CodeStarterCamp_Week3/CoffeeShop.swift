8b27ce92e7824aad029115a8a4c5b8c33b7385de
홍승주 <hongseungju@hongseungjuui-MacBookAir.local>
2022-04-29 19:48:59 +0900

    커피숍 타입

--- CodeStarterCamp_Week3/CoffeeShop.swift
+++ CodeStarterCamp_Week3/CoffeeShop.swift
@@ -9,9 +9,10 @@ import Foundation
 
 struct CoffeeShop {
    var sales: Int?
-    var pickUpTable: Coffee?
+   var pickUpTable: Coffee?
    var barista: Person?
-   
+   var menu: [Coffee: Int]? = [.아메리카노: 4000, .에스프레소: 2500, .바닐라라떼: 4500, .복숭아스무디: 5000, .아인슈패너: 5000, .카페라떼: 4500, .딸기스무디: 5000, .시그니쳐초콜릿: 6000]
+
    func getOrder() {
         print("어떤 커피를 드릴까요?")
     }
