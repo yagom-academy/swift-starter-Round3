6e86cd9229e51c3f44af5be5dc52008be64b8fa8
wolframhwang <hzw94@naver.com>
2022-04-29 20:35:28 +0900

    [feat] Mission3: Step2: 커피샵이 바리스타를 보유하는데, 바리스타가 커피샵에게 통신을 하기 위해 필요한 프로토콜
    Delegate 패턴을 통해 PickUpTable에 바리스타가 다 된 커피를 놓을수 있도록 하는 함수를 가지는 Protocol Shop을 반영합니다.

---
+++ CodeStarterCamp_Week3/Shop.swift
@@ -0,0 +1,12 @@
+//
+//  Shop.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 황지웅 on 2022/04/29.
+//
+
+import Foundation
+
+protocol Shop: AnyObject {
+    func insertCoffeeToPickUpTable(coffee: Coffee)
+}
