f006a6b8fcf6251c5abe7f41863411d1cc7c15a5
wolframhwang <hzw94@naver.com>
2022-04-29 20:43:09 +0900

    [feat] Mission3: Step2: CoffeeShop 패치 골자 추가 반영
    1. coffeeShop이 바리스타를 소유할수 있게 선언 명을 변경했습니다.
    2. CoffeeShop운영에 필요한 함수들을 추가 반영했습니다.
    3. CoffeeShop의 Delegate 패턴을 위해 CoffeeShop이 Shop 프로토콜을 채택할수 있게 반영했습니다.

--- CodeStarterCamp_Week3/CoffeeShop.swift
+++ CodeStarterCamp_Week3/CoffeeShop.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 class CoffeeShop {
-    var barista: Person? = nil
+    var barista: Barista? = nil
     var coffeeMenu: CoffeeMenu
     var salesMoney: Int
     var pickUpTable: PickUpTable = PickUpTable()
@@ -18,16 +18,28 @@ class CoffeeShop {
         self.coffeeMenu = coffeeMenu
     }
     
-    func hireBarista(barista: Person) {
+    func hireBarista(barista: Barista) {
         self.barista = barista
     }
     
     func depositMoney(coffeePrice: Int) {
+        salesMoney += coffeePrice
     }
     
     func insertOrderToPickUpTable(orderMan: String, coffee: Coffee) {
+        pickUpTable.insertOrderToPickUpTable(name: orderMan, coffee: coffee)
     }
     
     func order(coffee: Coffee) {
+        let coffeePrice = coffeeMenu.getPriceOfCoffee(coffee: coffee)
+        
+        depositMoney(coffeePrice: coffeePrice)
+        barista?.makeCoffeeOrderFromShop(coffee: coffee)
+    }
+}
+
+extension CoffeeShop: Shop {
+    func insertCoffeeToPickUpTable(coffee: Coffee) {
+        pickUpTable.releasePickUpTable(coffee: coffee)
     }
 }
