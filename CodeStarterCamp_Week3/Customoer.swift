6a1dbdbb17883ec5ab4458bc4e21856b6ce45f66
wolframhwang <hzw94@naver.com>
2022-05-01 22:37:53 +0900

    [refactor] Mission3: Step2: Person -> Customer, Barista 상속화
    - Person을 통해, Customer와 Barista가 상속을 받아 클래스를 구현할수 있도록 반영했습니다.

---
+++ CodeStarterCamp_Week3/Customoer.swift
@@ -0,0 +1,34 @@
+//
+//  Customoer.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 황지웅 on 2022/05/01.
+//
+
+import Foundation
+
+class Customoer: Person {
+    override init(name: String, money: Int) {
+        super.init(name: name, money: money)
+    }
+    
+    func isSufficeMoney(coffeePrice: Int) -> Bool {
+        return coffeePrice <= money
+    }
+    
+    func orderCoffee(coffee: Coffee, coffeeShop: CoffeeShop) {
+        guard let coffeePrice = coffeeShop.getPriceOfCoffeeFromMenu(coffee: coffee) else {
+            print("존재하지 않는 메뉴입니다.")
+            return
+        }
+        
+        if !isSufficeMoney(coffeePrice: coffeePrice) {
+            print("잔액이 \(coffeePrice - money)만큼 부족합니다.")
+            return
+        }
+        
+        submitMoney(coffeePrice: coffeePrice)
+        coffeeShop.insertOrderToPickUpTable(orderMan: name, coffee: coffee)
+        coffeeShop.order(coffee: coffee)
+    }
+}
--- CodeStarterCamp_Week3/Person.swift
+++ CodeStarterCamp_Week3/Person.swift
@@ -8,35 +8,15 @@
 import Foundation
 
 class Person {
-    private var name: String
-    private var money: Int
+    var name: String
+    var money: Int
     
     init(name: String, money: Int) {
         self.name = name
         self.money = money
     }
     
-    func isSufficeMoney(coffeePrice: Int) -> Bool {
-        return coffeePrice <= money
-    }
-    
     func submitMoney(coffeePrice: Int) {
         money -= coffeePrice
-    }
-    
-    func orderCoffee(coffee: Coffee, coffeeShop: CoffeeShop) {
-        guard let coffeePrice = coffeeShop.getPriceOfCoffeeFromMenu(coffee: coffee) else {
-            print("존재하지 않는 메뉴입니다.")
-            return
-        }
-        
-        if !isSufficeMoney(coffeePrice: coffeePrice) {
-            print("잔액이 \(coffeePrice - money)만큼 부족합니다.")
-            return
-        }
-        
-        submitMoney(coffeePrice: coffeePrice)
-        coffeeShop.insertOrderToPickUpTable(orderMan: name, coffee: coffee)
-        coffeeShop.order(coffee: coffee)
     }
 }
