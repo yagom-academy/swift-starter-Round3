c28b32a8425476b3137aeb77c83df7ab2df18ca6
hansm <hanmix2003@naver.com>
2022-05-02 06:59:42 +0900

    Feat: Coffee 열거형 정의
    Coffee 열거형의 각 case들은 Int타입으로 지정합니다.
    각 case의 rawValue로 Person 인스턴스가 가지고 있는 money 프로퍼티와 연산을 합니다.

---
+++ CodeStarterCamp_Week3/Week3Step1Coffee.swift
@@ -0,0 +1,14 @@
+//
+//  Week3Step1Coffee.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by mingmac on 2022/05/01.
+//
+
+import Foundation
+
+enum Coffee: Int {
+    case espresso = 3000
+    case americano = 3500
+    case latte = 4500
+}
