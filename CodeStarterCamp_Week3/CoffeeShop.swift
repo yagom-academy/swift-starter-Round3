7317a22e1d5907dd251ed8ff24d1f12421e9ca08
jeongmin <lj845854@gmail.com>
2022-05-02 19:22:01 +0900

    refactor : pickUpTable에 customer변수 활용

--- CodeStarterCamp_Week3/CoffeeShop.swift
+++ CodeStarterCamp_Week3/CoffeeShop.swift
@@ -10,10 +10,11 @@ class CoffeeShop {
     private(set) var location: String
     private(set) var sales: Int = 0
     private(set) var menu: [Coffee: Int] = [:]
-    private var pickUpTable: PickUpTableInfo? {
-        willSet(newOrder) {
-            if let order = newOrder {
-                print("\(order.customer) 님의 커피가 준비되었습니다. 픽업대에서 가져가주세요.")
+    private(set) var customer: Person?
+    private var pickUpTable: Coffee? {
+        didSet {
+            if let customer = customer {
+                print("\(customer.name) 님의 커피가 준비되었습니다. 픽업대에서 가져가주세요.")
             }
         }
     }
@@ -30,11 +31,13 @@ class CoffeeShop {
     }
     
     func order(_ coffee: Coffee, for person: Person) {
+        self.customer = person
+        
         if let coffeePrice = self.menu[coffee] {
             if isEnough(money: person.money, for: coffeePrice) {
                 person.pay(money: coffeePrice)
                 sales += coffeePrice
-                pickUpTable = PickUpTableInfo(customer: person.name, coffee: coffee)
+                pickUpTable = coffee
             }
         }
     }
