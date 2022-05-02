5fc6c3af1987948415d8507d55e7a5b4c156c32c
홍승주 <hongseungju@hongseungjuui-MacBookAir.local>
2022-05-02 21:33:33 +0900

    CoffeeShop

--- CodeStarterCamp_Week3/CoffeeShop.swift
+++ CodeStarterCamp_Week3/CoffeeShop.swift
@@ -7,16 +7,30 @@
 
 import Foundation
 
-struct CoffeeShop {
-   var sales: Int?
-   var pickUpTable: Coffee?
-   var barista: Person?
-   var menu: [Coffee: Int]? = [.아메리카노: 4000, .에스프레소: 2500, .바닐라라떼: 4500, .복숭아스무디: 5000, .아인슈패너: 5000, .카페라떼: 4500, .딸기스무디: 5000, .시그니쳐초콜릿: 6000]
-
-   func getOrder() {
-        print("어떤 커피를 드릴까요?")
+class CoffeeShop {
+    var sales: Int
+    var pickUpTable: Coffee?
+    var barista: Person?
+    var menu: [Coffee: Int] = [.아메리카노: 4000, .에스프레소: 2500, .바닐라라떼: 4500, .복숭아스무디: 5000, .아인슈패너: 5000, .카페라떼: 4500, .딸기스무디: 5000, .시그니쳐초콜릿: 6000]
+   
+    init(sales: Int) {
+        self.sales = sales
     }
-    func makeCoffee() {
-      print("커피를 만들고 있습니다. 잠시만 기다려 주세요.")
+   
+    func getOrder(_ coffee: Coffee, _ person: Person) {
+        if let coffeePrice = menu[coffee] {
+            if coffeePrice <= person.money {
+                self.sales += coffeePrice
+                makeCoffee(coffee, person)
+            } else {
+                print("잔액이 \(coffeePrice - person.money)원 만큼 부족합니다.")
+            }
+        }
+    }
+   
+    func makeCoffee(_ coffee: Coffee, _ person: Person) {
+        pickUpTable = coffee
+        print("\(person.name)님의 커피가 준비되었습니다. 픽업대에서 가져가주세요")
     }
 }
+
