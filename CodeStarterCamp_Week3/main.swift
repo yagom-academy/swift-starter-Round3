e75f2fe38e146761b721ff03f7ce4d25033c6c37
MANSU.J <seunghyun.jung.84@gmail.com>
2022-05-02 22:47:21 +0900

    Refactor: 실행코드만 남김
    - 실행 코드만 남기고 기능코드 분리

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -1,57 +1,5 @@
 import Foundation
 
-struct Person {
-	var money: Int
-	init(money: Int) {
-		self.money = money
-	}
-	func buy() {
-		print("구입합니다.")
-	}
-}
-
-struct CoffeeShop {
-	var sales: Int = 0
-	var menu = Coffee.allCases
-	var pickupTable: Bool = false
-	var barista: Person?
-	func order(_ coffee: Coffee) {
-		print("\(coffee.getName()) 골랐군요!")
-	}
-	func makeCoffee(_ coffee: Coffee) {
-		print("\(coffee.getName())를 만듭니다.")
-	}
-}
-
-enum Coffee: CaseIterable {
-	case americano, cafeLatte, dolceLatte, frappuccino
-	var price: Int {
-		switch self {
-		case .americano:
-			return 4800
-		case .cafeLatte:
-			return 5000
-		case .dolceLatte:
-			return 4900
-		case .frappuccino:
-			return 4900
-		}
-	}
-	func getName() -> String {
-		switch self {
-		case .americano:
-			return "아메리카노"
-		case .cafeLatte:
-			return "카페라떼"
-		case .dolceLatte:
-			return "돌체라떼"
-		case .frappuccino:
-			return "프라푸치노"
-		}
-	}
-}
-
 var misterLee = Person(money: 100000)
 var missKim = Person(money: 1000000)
-var yagombucks = CoffeeShop()
-yagombucks.barista = misterLee
+var yagombucks = CoffeeShop(barista: misterLee)
