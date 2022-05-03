0556b78b6554165755159f2e6a8e64a3ad8c1ada
JAY-Winter <rubat0@naver.com>
2022-05-03 16:22:45 +0900

    style: 변수명 변경

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -24,10 +24,10 @@ class Customer: Person {
         let menuNameArr = Coffee.allCases.map { $0.rawValue }
         
         if menuNameArr.contains(menuName) {
-            guard let caseValues = Coffee(rawValue: menuName), let price = coffeeShop.menuList[caseValues], let buget = self.buget else { return }
+            guard let coffee = Coffee(rawValue: menuName), let price = coffeeShop.menuList[coffee], let buget = self.buget else { return }
             
             if buget >= price {
-                coffeeShop.order(caseValues)
+                coffeeShop.order(coffee)
             } else {
                 print("잔액이 \(price)원만큼 부족합니다")
             }
