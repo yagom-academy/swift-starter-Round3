011c03c8450562156ea4e132110d934253039997
delight010 <81559026+delight010@users.noreply.github.com>
2022-05-03 18:46:25 +0900

    - 타입 Person, CoffeeShop 생성 - 열거형 Coffee 생성

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, World!")
+class Person {
+    var name: String
+    var money: Int
+    
+    init(name: String, money: Int) {
+        self.name = name
+        self.money = money
+    }
+    
+    func buyCoffee(ordered: Coffee, shop: CoffeeShop) {
+    }
+}
+
+class CoffeeShop {
+    var revenue: Int
+    var menu: [Coffee : Int]
+    var barista: [Person] = []
+    var pickUpTable: Bool?
+    
+    init(revenue: Int, menu: [Coffee: Int]) {
+        self.revenue = revenue
+        self.menu = menu
+    }
+    
+    func orderedCoffee(coffee: Coffee) {
+    }
+}
+
+enum Coffee {
+    case espresso
+    case americano
+    case vanillaLatte
+    case cafeMocha
+}
 
