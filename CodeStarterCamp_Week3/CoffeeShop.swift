9259630a12afc88eb7d6141888d4eae53e8ba9c2
hansm <hanmix2003@naver.com>
2022-05-05 03:08:48 +0900

    Refactor: Coding Convention에 따른 order 메소드 코드 블럭 Indent 수정
    - Coding Convention에 따라 들여쓰기(Indent) 수정함

--- CodeStarterCamp_Week3/CoffeeShop.swift
+++ CodeStarterCamp_Week3/CoffeeShop.swift
@@ -26,18 +26,18 @@ class CoffeeShop {
     }
     
     func order(_ order: Coffee, for customer: Person) {
-            print("[커피숍] \(customer.name)님이 \(order)를 주문하였습니다.")
-            if customer.money < order.price {
-                let changes = order.price - customer.money
-                print("[안내] 잔액이 \(changes)원 부족합니다.")
-            } else {
-                sales += order.price
-                print("[안내] 현재 \(cafeName)의 매출액은 \(sales)원 입니다.")
-                customer.money -= order.price
-                print("[안내] \(customer.name)님의 잔액은 \(customer.money)원 입니다.")
-                brewCoffee(order, for: customer)
-            }
+        print("[커피숍] \(customer.name)님이 \(order)를 주문하였습니다.")
+        if customer.money < order.price {
+            let changes = order.price - customer.money
+            print("[안내] 잔액이 \(changes)원 부족합니다.")
+        } else {
+            sales += order.price
+            print("[안내] 현재 \(cafeName)의 매출액은 \(sales)원 입니다.")
+            customer.money -= order.price
+            print("[안내] \(customer.name)님의 잔액은 \(customer.money)원 입니다.")
+            brewCoffee(order, for: customer)
         }
+    }
     
     func brewCoffee(_ drink: Coffee, for customer: Person) {
         print("[커피숍] \(barista.name)이(가) \(customer.name)님의 \(drink)를 제조합니다.")
