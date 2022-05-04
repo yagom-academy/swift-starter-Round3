0c00f0806308cb555f9440d2ea11eeb6341c2312
hansm <hanmix2003@naver.com>
2022-05-05 04:29:07 +0900

    Refactor: 인스턴스를 생성하고 메소드를 실행 후 메모리에서 해제하는 코드 구현
    - 각각의 인스턴스가 주문한 커피 종류를 확인하기 위해 keyster 라는 인스턴스를 추가로 생성함
    - Person 인스턴스를 상수에서 변수로 재선언하고 nil을 할당하기 위해 타입에 옵셔널을 추가함
    - 옵셔널 인스턴스가 옵셔널 체이닝으로 메소드에 접근할 수 있도록 함
    - 주문을 완료하고 커피를 픽업테이블에서 수령한 인스턴스는 카페를 나가므로 인스턴스에 nil을 할당함
    - 터미널의 가독성을 높이기 위해 빈 print문을 추가함

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -10,5 +10,17 @@ import Foundation
 
 // Week3Step1
 let misterLee: Person = Person(name: "misterLee", job: "바리스타")
-let missKim: Person = Person(name: "missKim")
+var missKim: Person? = Person(name: "missKim")
+var keyster: Person? = Person(name: "keyster")
 let yagombucks: CoffeeShop = CoffeeShop(barista: misterLee, cafeName: "yagombucks")
+
+missKim?.buy(kindOf: .americano, at: yagombucks)
+print("")
+keyster?.buy(kindOf: .espresso, at: yagombucks)
+keyster = nil
+print("")
+missKim?.buy(kindOf: .latte, at: yagombucks)
+print("")
+missKim?.buy(kindOf: .espresso, at: yagombucks)
+print("")
+missKim = nil
