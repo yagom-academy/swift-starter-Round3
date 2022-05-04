b2cbee9171c667466159bacba284c5070d2b6497
jsa0224 <puccav@naver.com>
2022-05-04 15:43:42 +0900

    Person 소스 파일 - spandCash() 메서드 추가

--- CodeStarterCamp_Week3/Person.swift
+++ CodeStarterCamp_Week3/Person.swift
@@ -20,6 +20,15 @@ class Person {
         self.personalMBTI = personalMBTI
         self.walletInCash = walletInCash
     }
+    
+    func spandCash (amount: Int) {
+        if isPayable(amount) == false {
+            print("잔돈이 부족합니다.")
+        } else {
+            walletInCash -= amount
+        }
+    }
+    
     func buyCoffee(_ coffee: Coffee) {
         print("\(name)이 \(coffee)를 삽니다.")
         print("\(name)에게 \(walletInCash)원이 남았어요")
