cf61cc7c1aea896286222a353d39ab6c46325b50
MANSU.J <seunghyun.jung.84@gmail.com>
2022-05-06 01:22:23 +0900

    Refactor: 커피구입을 실행
    - 사람, 커피숍을 생성하고 구입을 한다.

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -1,5 +1,11 @@
 import Foundation
 
-var misterLee = Person(money: 100000)
-var missKim = Person(money: 1000000)
-var yagombucks = CoffeeShop(barista: misterLee)
+var misterLee = Person(name:"misterLee", money: 10000)
+var missKim = Person(name:"missKim", money: 20000)
+var yagombucks = CoffeeShop(totalSales: 0, barista: misterLee)
+
+print("메뉴 : \(yagombucks.menu) \n")
+missKim.buy(shop: &yagombucks, coffee: .americano)
+missKim.buy(shop: &yagombucks, coffee: .cafeLatte)
+missKim.buy(shop: &yagombucks, coffee: .dolceLatte)
+missKim.buy(shop: &yagombucks, coffee: .frappuccino)
