e4efa5f847de3a3eaf9ddaab1728e3b1e650016d
MANSU.J <seunghyun.jung.84@gmail.com>
2022-05-07 04:10:07 +0900

    Refactor: 픽업테이블
    - 커피제조가 완료되면 픽업테이블에 커피가 올라감
    - 주문자의 이름과 주문한 커피이름이 나오는 메시지가 출력

--- CodeStarterCamp_Week3/CoffeeShop.swift
+++ CodeStarterCamp_Week3/CoffeeShop.swift
@@ -6,21 +6,27 @@ struct CoffeeShop {
 	let menuNames: [String] = Coffee.allCases.map{ String($0.name) }
 	let menuPrices: [Int] = Coffee.allCases.map{ Int($0.price) }
 	lazy var menu = Array(zip(menuNames, menuPrices))
+	
 	init(totalSales: Int, barista: Person){
 		self.totalSales = totalSales
 		self.barista = barista
 	}
-	var pickupTable: String = "" {
+	
+	var pickupTable: (Coffee: Coffee, Person: Person)? {
 		didSet {
-			print(pickupTable, "\n")
+			if let customer = pickupTable {
+				print("\(customer.Person.name)님의 \(customer.Coffee.name) 준비되었습니다. 픽업대에서 가져가주세요. \n")
+			}
 		}
 	}
-	mutating func order(_ coffee: Coffee, _ customersName: String) {
+	
+	mutating func order(_ coffee: Coffee, _ customer: Person) {
 		totalSales += coffee.price
-		makeCoffee(coffee, customersName)
+		makeCoffee(coffee, customer)
 	}
-	mutating func makeCoffee(_ coffee: Coffee, _ customersName: String) {
-		let coffeeName: String = "\(customersName) 님의 \(coffee.name)가 준비되었습니다. 픽업대에서 가져가주세요."
-		pickupTable = coffeeName
+	
+	mutating func makeCoffee(_ coffee: Coffee, _ customer: Person) {
+		print("바리스타 : \(barista.name), \(customer.name)의 \(coffee.name)를 만듭니다.")
+		pickupTable = (Coffee: coffee, Person: customer)
 	}
 }
