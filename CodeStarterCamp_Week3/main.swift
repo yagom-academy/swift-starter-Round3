edb8a9bf1caecd4f9a7c04d05b888d8d05dbae15
jsa0224 <puccav@naver.com>
2022-05-07 17:10:05 +0900

    main 소스 파일 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -13,9 +13,8 @@ let missKim = Person(name: "missKim", age: 35, walletInCash: 10000)
 let misterLee = Person(name: "misterLee", age: 31, habit: "농구", MBTI: "INFJ", walletInCash: 20000)
 
 //CoffeeShop 타입의 인스턴스로 yagombucks 을 생성
-var yagombucks = CoffeeShop(owner: "야곰", openingHours: "10:00 ~ 20:00", menu: Coffee.menu, barista: misterLee)
+let yagombucks = CoffeeShop(owner: "야곰", openingHours: "10:00 ~ 20:00", menu: Coffee.menu, barista: misterLee)
 
-missKim.buyCoffee(Coffee.caramelMacchiato, at: yagombucks)
-missKim.walletInCash = 4000
-missKim.buyCoffee(Coffee.caramelMacchiato, at: yagombucks)
-
+missKim.buyCoffee(.caramelMacchiato, at: yagombucks)
+missKim.buyCoffee(.americano, at: yagombucks)
+missKim.buyCoffee(.caffeLatte, at: yagombucks)
