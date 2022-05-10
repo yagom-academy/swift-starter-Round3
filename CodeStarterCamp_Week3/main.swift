2c158f774806cc5f6a5b72b0c8690e1663d8d556
OneE <top9712@daum.net>
2022-05-10 20:10:06 +0900

    feat:계산하기, 호출 추가중

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -7,8 +7,17 @@
 //
 import Foundation
 
+enum Coffee: String {
+    case espresso = "에스프레소",
+         americano = "아메리카노",
+         cafeLatte = "카페 라떼" ,
+         cappuccino = "카푸치노",
+         cafeMocha = "카페 모카",
+         caramelMacchiato = "카라멜 마끼아또"
+}
+
 class Person {
-    var name: String
+    let name: String
     var age: Int
     var height: Int
     var money: Int
@@ -21,7 +30,17 @@ class Person {
     }
     
     func buy(_ menu: Coffee) {
-        yagomBucks.order(menu)
+        if money >= (yagomBucks.coffeeMenuboard[menu] ?? 0) {
+            money = money - (yagomBucks.coffeeMenuboard[menu] ?? 0)
+            if money == 0 {
+                print("감사합니다.")
+            } else {
+                print("거스름돈 \(money)원 입니다." , terminator: "")
+            }
+            yagomBucks.order(menu)
+        } else {
+            print("잔액이 \(yagomBucks.coffeeMenuboard[menu] ?? 0)원 만큼 부족합니다.")
+        }
     }
 }
 
@@ -29,50 +48,38 @@ class CoffeeShop {
     var barista: Person
     var sales: Int
     var coffeeMenuboard = [Coffee : Int]()
-    var pickUpTable: Coffee?
+    var pickupTable: Coffee? = nil {
+        willSet(orderedCoffee) {
+            print("\(String(describing: orderedCoffee)))가 준비되었습니다. 픽업 테이블에서 가져가주세요.")
+        }
+    }
     
-    init(barista: Person, sales: Int = 0, coffeeMenuBoard: [Coffee : Int]) {
+    init(barista: Person, sales: Int = 0, coffeeMenuBoard: [Coffee : Int], pickupTable: Coffee?) {
         self.barista = barista
         self.sales = sales
         self.coffeeMenuboard = coffeeMenuBoard
     }
     
-    convenience init(barista: Person, sales: Int = 0, coffeeMemuBoard: [Coffee : Int], pickUpTable:Coffee) {
-        self.init(barista: barista, sales: sales, coffeeMenuBoard: coffeeMemuBoard)
-        self.pickUpTable = pickUpTable
-    }
-    
-    func make(_ menu: Coffee) {
-        print("\(menu)를 제조 중입니다.")
-        yagomBucks.callCustomer(menu)
-    }
-    
     func order(_ coffee: Coffee) {
-        yagomBucks.make(coffee)
-        
+        make(coffee)
+        sales = sales + (coffeeMenuboard[coffee] ?? 0)
     }
     
-    func callCustomer(_ pickupMenu: Coffee) {
-        print("\(pickupMenu)가 준비되었습니다. 픽업대에서 가져가주세요.")
+    func make(_ coffee: Coffee) {
+        print("\(coffee.rawValue)를 제조 중입니다.")
+        yagomBucks.pickupTable = coffee
     }
 }
 
-enum Coffee: String {
-    case espresso = "에스프레소",
-         americano = "아메리카노",
-         cafeLatte = "카페라떼" ,
-         cappuccino = "카푸치노",
-         cafeMocha = "카페모카",
-         caramelMacchiato = "카라멜마끼아또"
-}
-let missKim: Person = Person(name: "MissKim", age: 23, height: 160, money: 50000)
-let misterLee: Person = Person(name: "MisterLee", age: 26, height: 180, money: 40000)
+let missKim: Person = Person(name: "김미영", age: 23, height: 160, money: 4600)
+let misterLee: Person = Person(name: "이삼사", age: 26, height: 180, money: 40000)
 let yagomBucks: CoffeeShop = CoffeeShop(barista: misterLee, coffeeMenuBoard:
                                             [Coffee.espresso: 3100,
                                              Coffee.americano: 4000,
                                              Coffee.cafeLatte: 4200,
                                              Coffee.cafeMocha: 4500,
                                              Coffee.cappuccino: 4600,
-                                             Coffee.caramelMacchiato: 5000])
+                                             Coffee.caramelMacchiato: 5000], pickupTable: nil)
 
-missKim.buy(Coffee.espresso)
+missKim.buy(.cappuccino)
+misterLee.buy(.caramelMacchiato)
