f38013677c05306bb68701d0cdeb9fdab87e7842
OneE <top9712@daum.net>
2022-05-12 16:19:50 +0900

    feat: 손님 이름, 메뉴 출력 마무리

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -29,17 +29,12 @@ class Person {
         self.money = money
     }
     
-    func buy(_ menu: Coffee) {
-        if money >= (yagomBucks.coffeeMenuboard[menu] ?? 0) {
-            money = money - (yagomBucks.coffeeMenuboard[menu] ?? 0)
-            if money == 0 {
-                print("감사합니다.")
-            } else {
-                print("거스름돈 \(money)원 입니다." , terminator: "")
-            }
-            yagomBucks.order(menu)
+    func buy(_ coffee: Coffee, at coffeeShop: CoffeeShop) {
+        if money >= (yagomBucks.coffeeMenuboard[coffee] ?? 0) {
+            money = money - (yagomBucks.coffeeMenuboard[coffee] ?? 0)
+            yagomBucks.order(coffee, by: self)
         } else {
-            print("잔액이 \(yagomBucks.coffeeMenuboard[menu] ?? 0)원 만큼 부족합니다.")
+            print("잔액이 \((coffeeShop.coffeeMenuboard[coffee] ?? 0) - money)원 부족합니다.")
         }
     }
 }
@@ -51,22 +46,21 @@ class CoffeeShop {
     var customer: Person?
     var pickupTable: Coffee? {
         didSet {
-            if let coffeeMenu = pickupTable {
-                if let customer = self.customer{
+            if let coffeeMenu = pickupTable, let customer = self.customer  {
                     print("\(customer.name)의 \(coffeeMenu.rawValue)가 준비되었습니다. 픽업 테이블에서 가져가주세요.")
-                }
             }
         }
     }
 
-    init(barista: Person, sales: Int = 0, coffeeMenuBoard: [Coffee : Int], pickupTable: Coffee?, customer: Person?) {
+    init(barista: Person, sales: Int = 0, coffeeMenuBoard: [Coffee : Int], pickupTable: Coffee?, customer : Person?) {
         self.barista = barista
         self.sales = sales
         self.coffeeMenuboard = coffeeMenuBoard
         self.customer = customer
     }
     
-    func order(_ coffee: Coffee) {
+    func order(_ coffee: Coffee, by guest: Person) {
+        customer = guest
         make(coffee)
         sales = sales + (coffeeMenuboard[coffee] ?? 0)
     }
@@ -77,7 +71,7 @@ class CoffeeShop {
     }
 }
 
-let missKim: Person = Person(name: "김미영", age: 23, height: 160, money: 4600)
+let missKim: Person = Person(name: "김미영", age: 23, height: 160, money: 6600)
 let misterLee: Person = Person(name: "이삼사", age: 26, height: 180, money: 40000)
 let yagomBucks: CoffeeShop = CoffeeShop(barista: misterLee, coffeeMenuBoard:
                                             [Coffee.espresso: 3100,
@@ -86,6 +80,7 @@ let yagomBucks: CoffeeShop = CoffeeShop(barista: misterLee, coffeeMenuBoard:
                                              Coffee.cafeMocha: 4500,
                                              Coffee.cappuccino: 4600,
                                              Coffee.caramelMacchiato: 5000],
-                                        pickupTable: nil, customer: missKim)
+                                        pickupTable: nil, customer: nil)
 
-missKim.buy(.cappuccino)
+missKim.buy(.cappuccino, at: yagomBucks)
+misterLee.buy(.americano, at: yagomBucks)
