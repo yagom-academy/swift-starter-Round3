d659bae29f4d2dcdf93d329929e91517d8acd187
codesparrow12 <kanghd97@gmail.com>
2022-07-07 18:22:49 +0900

    missKim, misterLee, yagombucks 인스턴스 구현

--- step1/main.swift
+++ step1/main.swift
@@ -25,14 +25,17 @@ class CoffeeShop {
     var shopName: String
     var sales: Int
     var barista: Person
-    var menu: [String: Int]
+    var menu: [Coffee: Int]
     var pickUpTable: [String]
-    init(shopName: String, sales: Int, barista: Person, menu: [String: Int], pickUpTable: [String]) {
+    init(shopName: String, sales: Int, barista: Person, menu: [Coffee: Int], pickUpTable: [String]) {
         self.shopName = shopName
         self.sales = sales
         self.barista = barista
         self.menu = menu
         self.pickUpTable = pickUpTable
+    }
+    func make(_ coffee: Coffee, from name: String) {
+        
     }
 }
 
@@ -44,3 +47,16 @@ enum Coffee: String {
     case einspanner = "아인슈패너"
     case caramelMacchiato = "카라멜 마끼아또"
 }
+
+var misterLee: Person = Person(name: "Minsu Lee", age: 26, money: 10000)
+var missKim: Person = Person(name: "Jisu Kim", age: 23, money: 10000)
+var yagombucks: CoffeeShop = CoffeeShop(shopName: "yagombucks",
+                                        sales: 0,
+                                        barista: misterLee,
+                                        menu: [Coffee.americano : 3500,
+                                               Coffee.caffeeLatte : 4500,
+                                               Coffee.cappuccino : 4000,
+                                               Coffee.decaf : 4000,
+                                               Coffee.einspanner : 5000,
+                                               Coffee.caramelMacchiato : 5000],
+                                        pickUpTable: [])
