3b941d23038580c25822c13b73505ef6074748a8
codesparrow12 <kanghd97@gmail.com>
2022-07-07 18:04:59 +0900

    Coffee 열거형 수정 및 Person 클래스 구현

--- step1/main.swift
+++ step1/main.swift
@@ -7,6 +7,29 @@
 
 import Foundation
 
-enum Coffee {
-    case americano, caffeeLatte, cappuccino, decaf, einspanner, caramelMacchiato
+class Person {
+    var name: String
+    var age: Int
+    var money: Int
+    func order(_ coffee: Coffee) -> Coffee {
+        return coffee
+    }
+    init(name: String, age: Int, money: Int) {
+        self.name = name
+        self.age = age
+        self.money = money
+    }
+}
+
+class CoffeeShop {
+    
+}
+
+enum Coffee: String {
+    case americano = "아메리카노"
+    case caffeeLatte = "카페라떼"
+    case cappuccino = "카푸치노"
+    case decaf = "디카페인"
+    case einspanner = "아인슈패너"
+    case caramelMacchiato = "카라멜 마끼아또"
 }
