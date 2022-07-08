8858eefadc94ef9ea02b91569cdbf82a1732aaa6
Jae Hyung Kim <smfc.mene@gmail.com>
2022-07-08 18:54:49 +0800

    feat: Coffee enum 생성

---
+++ CodeStarterCamp_Week3/step2.swift
@@ -0,0 +1,18 @@
+//
+//  step2.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by smfc on 8/7/2022.
+//
+
+import Foundation
+
+enum Coffee: Int {
+    case 에스프레소 = 3800
+    case 아메리카노 = 4200
+    case 라떼 = 4500
+    case 카푸치노 = 5100
+    case 카페모카 = 5600
+    case 오늘의커피 = 4600
+    case 허브티 = 3500
+}
