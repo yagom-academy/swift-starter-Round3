92197cf50065ff5de6c80205b101b1e1eb91804c
hanjongwoo <gwd0311@naver.com>
2022-07-09 01:53:13 +0900

    STEP2 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -12,24 +12,30 @@ struct Person {
     var money: Int?
     var name: String?
     
+    mutating func buySomething(_ price: Int) {
+        guard var money = money else {
+            print("There is nil in money")
+            return
+        }
+        money = money - price
+    }
+    
     mutating func order(_ coffee: Coffee) {
-        if var money = money {
-            if let price = yagombucks.menu[coffee] {
-                if money < price {
-                    print("잔액이 \(price - money)원만큼 부족합니다.")
-                }
-                else {
-                    yagombucks.make(coffee, from: self.name ?? "이름없음")
-                    money = money - price
-                    yagombucks.salesRevenue = yagombucks.salesRevenue + price
-                }
-            }
-            else {
-                print("There is nil in price")
-            }
+        guard let money = money else {
+            print("There is nil in money")
+            return
+        }
+        guard let price = yagombucks.menu[coffee] else {
+            print("There is nil in price")
+            return
+        }
+        if money < price {
+            print("잔액이 \(price - money)원만큼 부족합니다.")
         }
         else {
-            print("There is nil in money")
+            yagombucks.make(coffee, from: self.name ?? "이름없음")
+            buySomething(price)
+            yagombucks.addRevenue(price)
         }
     }
 }
@@ -37,14 +43,23 @@ struct Person {
 struct CoffeeShop {
     var salesRevenue: Int = 10000000
     var coffeeType: Coffee?
-    var pickUpTable: Array<Coffee> = []
+    var pickUpTable: [(String, Coffee)] = []{
+        willSet(newValue) {
+            guard let orderInfo = newValue.last else {
+                print("There is nil in orderInfo")
+                return
+            }
+            print("\(orderInfo.0)님이 주문하신 \(orderInfo.1)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
+        }
+    }
     var barista: Person?
     var menu: Dictionary = [Coffee.아메리카노:4000, .카푸치노:4500, .에스프레소: 3800]
     
     mutating func make(_ coffee: Coffee, from name: String) {
-        // pickUpTable에 올리기, 문구 출력하기
-        pickUpTable.append(coffee)
-        print("\(name) 님이 주문하신 \(coffee)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
+        pickUpTable.append((name, coffee))
+    }
+    mutating func addRevenue(_ revenue: Int) {
+        salesRevenue = salesRevenue + revenue
     }
 }
 
@@ -52,11 +67,10 @@ enum Coffee {
     case 아메리카노, 카푸치노, 에스프레소
 }
 
-var missKim = Person()
+var missKim = Person(money: 5000, name: "김여자")
+var misterKim = Person(money: 6000, name: "김남자")
 var yagombucks = CoffeeShop()
-
-missKim.money = 500
-missKim.name = "김여자"
 missKim.order(.아메리카노)
+misterKim.order(.카푸치노)
 
 
