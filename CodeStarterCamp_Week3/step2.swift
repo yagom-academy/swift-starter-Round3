a1f1f37444180813eba2df5f20f24c64e89933ff
Jae Hyung Kim <smfc.mene@gmail.com>
2022-07-08 18:56:35 +0800

    feat: Person 구조체 생성

--- CodeStarterCamp_Week3/step2.swift
+++ CodeStarterCamp_Week3/step2.swift
@@ -16,3 +16,26 @@ enum Coffee: Int {
     case 오늘의커피 = 4600
     case 허브티 = 3500
 }
+
+struct Person {
+    var name: String
+    var age: Int
+    var height: Double?
+    var moneyAmount: Int
+    
+    mutating func order(_ coffee: Coffee) {
+        let drinkPrice = coffee.rawValue
+        if moneyAmount >= drinkPrice {
+            print("\(coffee)를 \(drinkPrice)원에 구입합니다, ", terminator: "")
+            self.moneyAmount = moneyAmount - drinkPrice
+            print("남은 돈: \(self.moneyAmount)원")
+        }
+        else {
+            print("잔액이 \(drinkPrice - moneyAmount)원 만큼 부족합니다.")
+        }
+    }
+}
+
+var misterLee: Person = Person(name: "misterLee", age: 20, height: 176.8, moneyAmount: 3500)
+var missKim: Person = Person(name: "missKim", age: 24, moneyAmount: 20000)
+var coda: Person = Person(name: "Coda", age: 50, moneyAmount: 1000000)
