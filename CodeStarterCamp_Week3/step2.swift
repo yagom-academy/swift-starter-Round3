cc689c03a9cfbe59fea7544e16531dd92c1355c8
Jae Hyung Kim <smfc.mene@gmail.com>
2022-07-08 18:57:41 +0800

    feat: CoffeeShop 구조체 생성

--- CodeStarterCamp_Week3/step2.swift
+++ CodeStarterCamp_Week3/step2.swift
@@ -29,6 +29,8 @@ struct Person {
             print("\(coffee)를 \(drinkPrice)원에 구입합니다, ", terminator: "")
             self.moneyAmount = moneyAmount - drinkPrice
             print("남은 돈: \(self.moneyAmount)원")
+            yagombucks.make(coffee, from: self.name)
+            yagombucks.salesAmount += drinkPrice
         }
         else {
             print("잔액이 \(drinkPrice - moneyAmount)원 만큼 부족합니다.")
@@ -39,3 +41,18 @@ struct Person {
 var misterLee: Person = Person(name: "misterLee", age: 20, height: 176.8, moneyAmount: 3500)
 var missKim: Person = Person(name: "missKim", age: 24, moneyAmount: 20000)
 var coda: Person = Person(name: "Coda", age: 50, moneyAmount: 1000000)
+
+struct CoffeeShop {
+    var name: String
+    var barista: Person?
+    var salesAmount: Int
+    var pickUpTable: Array<String> = []
+
+    mutating func make(_ coffee: Coffee, from name: String) {
+        pickUpTable.append("\(coffee)")
+        print("\(name)님이 주문하신 \(coffee)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
+    }
+}
+
+var yagombucks: CoffeeShop = CoffeeShop(name: "yagomBucks", barista: misterLee, salesAmount: 10000000)
+
