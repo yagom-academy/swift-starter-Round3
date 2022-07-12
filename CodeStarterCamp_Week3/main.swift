1269d20f318c09823c076d2d55e960b956d61bc4
sunny <maengseon-a@maengseon-aui-MacBookAir.local>
2022-07-12 12:22:37 +0900

    프로퍼티 타입변경, enum내부 switch구문 추가, self삭제

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -1,15 +1,14 @@
 import Foundation
 
+let yagombucksMenu: Dictionary<Coffee, Int> = [.americano: 4200, .latte: 4500, .frappucchino: 5000, .smoothe: 6000]
 var misterLee = Person(money: 50000)
 var missKim = Person(money: 100000)
-var yagombucks = CoffeeShop()
-
+var yagombucks = CoffeeShop(barista: misterLee, menu: yagombucksMenu)
+                            
 misterLee.name = "leeJoo"
 misterLee.age = 25
 misterLee.buy("bread", for: 3800)
 misterLee.buy("ticket", for: 55000)
-yagombucks.barista = misterLee
-yagombucks.menu = [.americano: 4200, .latte: 4500, .frappucchino: 5000, .smoothe: 6000]
 yagombucks.takeOrder(.americano)
 
 
--- CodeStarterCamp_Week3/step1.swift
+++ CodeStarterCamp_Week3/step1.swift
@@ -2,6 +2,21 @@ import Foundation
 
 enum Coffee {
     case americano, latte, frappucchino, smoothe, ade
+    
+    var name: String{
+        switch self {
+        case .americano:
+            return "아메리카노"
+        case .latte:
+            return "라떼"
+        case .frappucchino:
+            return "프라푸치노"
+        case .smoothe:
+            return "스무디"
+        case .ade:
+            return "에이드"
+        }
+    }
 }
 
 struct Person {
@@ -26,20 +41,20 @@ struct Person {
 
 struct CoffeeShop {
     var sales: Int = 0,
-        barista: Person?,
-        menu: Dictionary<Coffee, Int>? = [:],
-        pickUpTable: Array<Coffee>? = []
+        barista: Person,
+        menu: Dictionary<Coffee, Int> = [:],
+        pickUpTable: Array<Coffee> = []
     
     mutating func takeOrder(_ coffee: Coffee) {
-        if let price: Int = menu?[coffee] {
-            print("\(coffee)를 주문받았습니다")
-            self.make(coffee)
+        if let price: Int = menu[coffee] {
+            print("\(coffee.name)를 주문받았습니다")
+            make(coffee)
             sales = sales + price
         }
     }
     
     mutating func make(_ coffee: Coffee) {
-        pickUpTable?.append(coffee)
-        print("픽업대에 주문하신 \(coffee)가 준비되었습니다")
+        pickUpTable.append(coffee)
+        print("픽업대에 주문하신 \(coffee.name)가 준비되었습니다")
     }
 }
