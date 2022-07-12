2cce6d0c2fd64b5d024a1891d2242e1e912912a6
LimKiWoong <lkw4670@naver.com>
2022-07-12 22:50:42 +0900

    step2진행

--- CodeStarterCamp_Week3/step1.swift
+++ CodeStarterCamp_Week3/step1.swift
@@ -19,21 +19,6 @@ struct Person {
     
     mutating func purchaseCoffee(_ coffee: Coffee, shop: CoffeeShop) {
         var offer = shop
-        //         if let guard  둘중 머쓸지 고민하기 일단 구현먼저
-        
-        //        if let price = offer.menu[coffee] {
-        //            if money >= price {
-        //                offer.makeCoffee(coffee, from: name)
-        //                money -= price
-        //                offer.sales += price
-        //            } else {
-        //                let ininsufficientPrice = price - money
-        //                print("금액이 \(ininsufficientPrice)원만큼 부족합니다.")
-        //            }
-        //         } else {
-        //             print("메뉴에 해당하는 커피가 없습니다.")
-        //         }
-        
         guard let price = offer.menu[coffee] else {
             print("메뉴에 해당하는 커피가 없습니다.")
             return
