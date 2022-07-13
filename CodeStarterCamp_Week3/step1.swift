1432fd19760c8d9cb4de27adae57fb19fa0059b9
LimKiWoong <lkw4670@naver.com>
2022-07-13 22:15:16 +0900

    옵저버프로퍼티 적용

--- CodeStarterCamp_Week3/step1.swift
+++ CodeStarterCamp_Week3/step1.swift
@@ -28,7 +28,7 @@ struct Person {
             money -= price
             coffeeShop.sales += price
             print("\(name)의 소지금액 : \(money+price) -> \(money)")
-            print("가게 매출액 : \(coffeeShop.sales - price) -> \(coffeeShop.sales)")
+            print("가게 매출액 : \(coffeeShop.sales - price) -> \(coffeeShop.sales) \n")
         } else {
             let ininsufficientPrice = price - money
             print("금액이 \(ininsufficientPrice)원만큼 부족합니다.")
@@ -39,7 +39,14 @@ struct Person {
 class CoffeeShop {
     var sales: Int = 0
     var barista: Person
-    var pickUpTable: [Coffee] = []
+    var pickUpTable: [Coffee] = [] {
+        didSet {
+            guard let coffee = pickUpTable.last?.rawValue else {
+                return
+            }
+            print("주문한 \(coffee)이(가) 나왔습니다. 픽업대에서 가져가주세요")
+        }
+    }
     var menu: [Coffee:Int] = [
         .americano: 2000,
         .espresso: 3000,
@@ -53,7 +60,6 @@ class CoffeeShop {
     
     func makeCoffee(_ coffee: Coffee, from name: String) {
         pickUpTable.append(coffee)
-        print("\(name) 님이 주문하신 \(coffee.rawValue)이(가) 준비되었습니다. 픽업대에서 가져가주세요")
     }
 }
 
