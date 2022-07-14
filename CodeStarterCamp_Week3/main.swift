32bcf00c03183177dd353baa26f7ce5d379f3dda
ericwonjoonk98 <wonjoonkang94@gmail.com>
2022-07-14 19:59:57 +0900

    week3 step1 초기본

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,37 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Person {
+    var haveMoney: Int
+    
+    func buyCoffee(orderedCoffee: Coffee) {
+        print("\(orderedCoffee)주문할게요!")
+    }
+}
+struct CoffeeShop {
+    var sales: Int
+    
+    var coffeeMenu: Dictionary<Coffee, Int> = [Coffee: Int]()
+    
+    var pickUPTable: [Coffee] = []
+    
+    mutating func takeOrderAndBrewCoffee(coffee: Coffee ) {
+        print("\(coffee)커피 주문받았습니다!")
+        print("\(coffee)커피 나왔습니다!")
+        pickUPTable.append(coffee)
+    }
+    var barista: Person
+    
+    
+}
+enum Coffee {
+    case americano, latte, macchiato, affocato, vanillaLatte
+}
 
+var misterLee = Person(haveMoney: 10000)
+misterLee.buyCoffee(orderedCoffee: Coffee.americano)
+var missKim = Person(haveMoney: 10000)
+missKim.buyCoffee(orderedCoffee: Coffee.latte)
+var yagombucks = CoffeeShop(sales: 0, barista: misterLee)
+yagombucks.takeOrderAndBrewCoffee(coffee: Coffee.affocato)
+yagombucks.takeOrderAndBrewCoffee(coffee: Coffee.latte)
