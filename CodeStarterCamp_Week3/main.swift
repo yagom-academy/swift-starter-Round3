a4867c8839aa1eac6ecf6ac3d1a89ed0ced2fc69
junhoL15 <junhoL1046@gmail.com>
2022-07-15 22:04:50 +0900

    feat: yagombucks 생성 시 이니셜라이저에 menuBoard 전달.

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -11,7 +11,13 @@ import Foundation
 let misterLee: Person = Person(name: "미스터 리", cashInWallet: 10000)
 let missKim: Person = Person(name: "미스 김", cashInWallet: 5000)
 
-let yagombucks: CoffeeShop = CoffeeShop(name: "야곰벅스", barista: misterLee)
+let menuBoard: Dictionary<Coffee, Int> = [.americano: 2500,
+                                          .caffeLatte: 3000,
+                                          .cappuccino: 3500,
+                                          .macchiato: 4000]
+let yagombucks: CoffeeShop = CoffeeShop(name: "야곰벅스",
+                                        barista: misterLee,
+                                        menuBoard: menuBoard)
 
 missKim.orderCoffee(type: .americano, at: yagombucks)
 missKim.orderCoffee(type: .caffeLatte, at: yagombucks)
