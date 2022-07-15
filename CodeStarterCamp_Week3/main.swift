0903476be8b160264a421b9f7caa7fced261f377
oh namjun <ohnamjun@ohui-MacBookAir.local>
2022-07-15 23:02:36 +0900

    STEP2

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 
-var misterLee: Person = Person(name: "Lee", age: 23, sex: "M", money: 50000)
-var missKim: Person = Person(name: "Kim", age: 21, sex: "F", money: 20000)
-var yagombucks = CoffeeShop(coffeeShopName: "yagombucks", barista: missKim)
+var missKim: Person = Person(name: "Kim", money: 10000)
+var yagombucks = CoffeeShop(sales: 0)
+
+missKim.order(Coffee.cafeMocha, coffeeShop: yagombucks)
