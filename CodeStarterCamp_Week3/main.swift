b4de2468611347e017ffaf1dbec68c89c8922ab8
chacha <hyhy0429_@nvaer.com>
2022-07-17 19:54:31 +0900

    Person&CoffeShop

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -1,41 +1,44 @@
-import Foundation
 
-struct Person {
+class Person {
     var name: String
     var gender: String
     var money: Int
-    
-    func buyCoffee() {
-        print("커피를 구매합니다.")
+    init (name: String, gender: String, money: Int){
+        self.name = name
+        self.gender = gender
+        self.money = money
+    }
+    func buy(coffee:Coffee, coffeeshop:CoffeeShop){
+        print("\(coffeeshop)에서 \(coffee)를 구매합니다.")
     }
 }
 
-class Coffeeshop {
+class CoffeeShop {
     var totalSales: Int
-    var barista: Person
-    enum MenuBoard: Int {
-        case espresso = 3500, americano = 1500, caffeLatte = 4000, cappuccino = 4500
+    enum menuBoard: Int {
+        case cappucino = 3500, iceamericano = 1500, latte = 1400, cafemoca = 3000
     }
     var pickupTable: Array<Coffee> = [Coffee]()
-    init(barista: Person){
+    var barista: Person
+    
+    init (barista: Person){
         self.totalSales = 0
         self.barista = barista
     }
-    func takeOrder() {
-        print("주문을 받았습니다.")
+    func takeOrder (name:String, coffee:Coffee){
+        print ("\(name)님의 \(coffee)를 주문받았습니다.")
+        make(coffee: coffee, name: name)
     }
-    func makeCoffee(x: Coffee) {
-        print("커피를 만들고 있습니다.")
-        pickupTable.append(x)
+    func make (coffee: Coffee, name: String){
+        print("\(name)님이 주문하신 \(coffee)를 만들고 있습니다.")
+        pickupTable.append(coffee)
     }
 }
 
-enum Coffee{
-    case espresso, americano, caffeLatte, cappuccino
+enum Coffee {
+    case cappucino, iceamericano, latte, cafemoca
 }
 
-let misterLee: Person = Person(name: "misterLee", gender: "male", money: 5000)
-
-let missKim: Person = Person(name: "missKim", gender: "female", money:3500)
-
-let yagombucks: Coffeeshop = Coffeeshop(barista: misterLee)
+let misterLee : Person = Person(name: "misterLee", gender: "male", money: 5000)
+let missKim: Person = Person(name: "missKim", gender: "female", money: 3500)
+let yagombucks: CoffeeShop = CoffeeShop(barista: misterLee)
