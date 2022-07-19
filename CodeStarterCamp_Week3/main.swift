73d294b2128261c14fe60afd46eab1437b4b228e
ericwonjoonk98 <wonjoonkang94@gmail.com>
2022-07-19 15:13:10 +0900

    함수 분리& 가격표 완성까지 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -9,36 +9,55 @@
 import Foundation
 
 struct Person {
-    var haveMoney: Int
+    var name: String
+    var balance: Int
     
-    func buyCoffee(orderedCoffee: Coffee) {
-        print("\(orderedCoffee)주문할게요!")
+    func buyCoffee(orderedCoffee: Coffee) -> Coffee {
+        return orderedCoffee
     }
 }
 struct CoffeeShop {
     var sales: Int
     
-    var coffeeMenu: Dictionary<Coffee, Int> = [Coffee: Int]()
+    var coffeeMenu: Dictionary<Coffee, Int> = [Coffee.americano : 4000, Coffee.latte : 4500, Coffee.macchiato: 5000]
     
     var pickUPTable: [Coffee] = []
     
-    mutating func takeOrderAndBrewCoffee(coffee: Coffee ) {
-        print("\(coffee)커피 주문받았습니다!")
-        print("\(coffee)커피 나왔습니다!")
+    func takeOrder(customer: Person, coffee: Coffee ) {
+        print("\(customer.name)의 \(coffee)를 주문받았습니다!")
+        
+    }
+    mutating func brewCoffee(coffee: Coffee) {
+        print("\(coffee) 커피 주문받고 내리겠습니다!")
         pickUPTable.append(coffee)
     }
-    var barista: Person
+    var barista: Person?
     
     
 }
 enum Coffee {
-    case americano, latte, macchiato, affocato, vanillaLatte
+    case americano, latte, macchiato
+    
+    var price: Int {
+        get {
+            switch self {
+            case .americano:
+                return 4000
+            case .latte:
+                return 4500
+            case .macchiato:
+                return 5000
+            }
+        }
+    }
 }
 
-var misterLee = Person(haveMoney: 10000)
-misterLee.buyCoffee(orderedCoffee: Coffee.americano)
-var missKim = Person(haveMoney: 10000)
-missKim.buyCoffee(orderedCoffee: Coffee.latte)
-var yagombucks = CoffeeShop(sales: 0, barista: misterLee)
-yagombucks.takeOrderAndBrewCoffee(coffee: Coffee.affocato)
-yagombucks.takeOrderAndBrewCoffee(coffee: Coffee.latte)
+
+var misterLee: Person = Person(name: "misterLee", balance: 10000)
+var yagombucks = CoffeeShop(sales: 0)
+yagombucks.takeOrder(customer: misterLee, coffee: .americano)
+yagombucks.brewCoffee(coffee: Coffee.americano)
+yagombucks.barista = misterLee
+var misterKim: Person = Person(name: "misterKim", balance: 20000)
+yagombucks.takeOrder(customer: misterKim, coffee: .latte)
+yagombucks.brewCoffee(coffee: Coffee.latte)
