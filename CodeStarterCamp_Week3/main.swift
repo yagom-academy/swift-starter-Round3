533e1e2393d2b844ce82ec76f2b286c88fd0ee13
ROWAN <kimkj93@naver.com>
2022-11-02 14:08:26 +0900

    Week3 STEP1 PR 수정요청 반영

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -18,20 +18,18 @@ struct Person {
     func orderCoffee(_ menu: String) -> String {
         return menu
     }
-    
 }
 
-struct CoffeShop {
+struct CoffeeShop {
     var revenue: Int?
     var menuList: Dictionary<Coffee, Int> = [:]
     var pickUpTable: Array<Coffee> = []
     var barista: Person
     
-    mutating func appendCoffeeMenu(_ name: Coffee, price: Int) {
-        let name = name
-        let price = price
-        
-        menuList[name] = price
+    mutating func appendCoffeeMenu() {
+        for menu in Coffee.allCases {
+            menuList[menu] = menu.returnPrice()
+        }
     }
     
     mutating func makeCoffee(_ order: Coffee) {
@@ -40,22 +38,43 @@ struct CoffeShop {
     }
 }
 
-enum Coffee: String {
+enum Coffee: String, CaseIterable {
     case americano = "아메리카노", cafeLatte = "카페라떼", blackTea = "홍차",
          lemonade = "레모네이드", espresso = "에스프레소", cafeMocca = "카페모카",
          smothie = "스무디", greenTea = "녹차"
+    
+    var price: Int {
+        switch self {
+        case .americano:
+            return 4000
+        case .cafeLatte:
+            return 4500
+        case .blackTea:
+            return 3500
+        case .lemonade:
+            return 5000
+        case .espresso:
+            return 2500
+        case .cafeMocca:
+            return 4500
+        case .smothie:
+            return 5500
+        case .greenTea:
+            return 3500
+        }
+    }
+    
+    func returnPrice() -> Int {
+        return self.price
+    }
 }
-
 
 var misterLee = Person(name: "misterLee",age: 27, money: 100000)
 var missKim = Person(name: "missKim", age: 30, money: 150000)
-var yagombucks = CoffeShop(barista: misterLee)
+var yagombucks = CoffeeShop(barista: misterLee)
 
 // yagombucks 메뉴 할당
-yagombucks.appendCoffeeMenu(.americano, price: 4500)
-yagombucks.appendCoffeeMenu(.blackTea, price: 3500)
-yagombucks.appendCoffeeMenu(.espresso, price: 3000)
-yagombucks.appendCoffeeMenu(.cafeLatte, price: 5000)
+yagombucks.appendCoffeeMenu()
 
 // barista 이름 확인
 print(yagombucks.barista.name)
@@ -75,5 +94,4 @@ checkMenuOfyagombucks(menu: .cafeLatte)
 
 // makeCoffee 메서드 확인
 yagombucks.makeCoffee(.americano)
-
 print(yagombucks.pickUpTable[0].rawValue)
