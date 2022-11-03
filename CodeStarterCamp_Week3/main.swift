30d9d960a856d3dc0be266c24173d0afd23f7175
kokkilE <kkuldak1@naver.com>
2022-11-03 13:01:17 +0900

    Week3 [STEP 1] 1st

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -1,12 +1,66 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week3
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+enum Coffee {
+    case espresso, latte, macchiato, cappuccino
+}
 
+class Person {
+    var name: String
+    var age: Int
+    var money: Int
+    var bag: [Any]?
+    
+    func buyCoffee(coffee: Coffee, coffeePrice: Int) {
+        if money > coffeePrice {
+            print("커피를 구매하였습니다.")
+            money -= coffeePrice
+            bag?.append(coffee)
+        }
+    }
+    
+    init(name: String, age: Int, money: Int) {
+        self.name = name
+        self.age = age
+        self.money = money
+    }
+}
+
+class CoffeeShop {
+    var barista: Person?
+    var sales: Int
+    var menu: Dictionary<Coffee, Int>
+    var pickUpTable: [Coffee]? = nil
+
+    func makeCoffee(coffee: Coffee) {
+        print("\(coffee)를 만들었습니다.")
+        pickUpTable?.append(coffee)
+    }
+    func takeOrder(coffee: Coffee) {
+        if let coffeePrice = menu[coffee] {
+            makeCoffee(coffee: coffee)
+            sales += coffeePrice
+        } else {
+            print("메뉴판에 없는 메뉴입니다.")
+        }
+    }
+    
+    func setMenu() {
+        self.menu[Coffee.espresso] = 4000
+        self.menu[Coffee.cappuccino] = 4500
+        self.menu[Coffee.latte] = 5000
+    }
+    
+    convenience init(barista: Person, sales: Int) {
+        self.init(sales: sales)
+        self.barista = barista
+    }
+    init(sales: Int) {
+        self.sales = sales
+        menu = [Coffee: Int]()
+        self.setMenu()
+    }
+}
+
+var misterLee: Person = Person(name: "misterLee", age: 30, money: 10000)
+var missKim: Person = Person(name: "missKim", age: 32, money: 8000)
+var yagombucks: CoffeeShop = CoffeeShop(barista: misterLee, sales: 5000000)
