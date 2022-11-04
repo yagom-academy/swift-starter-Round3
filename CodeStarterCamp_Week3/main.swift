e659ef15bfb03fd35255fb47f69be8717189041e
siwol <siwolssi@gmail.com>
2022-11-04 22:38:46 +0900

    STEP 2

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -14,26 +14,34 @@ struct Person {
     var age: Int
     var money: Int
     
-    func orderCoffee(_ coffee: Coffee) {
-        print("\(coffee.rawValue)로 주문하겠습니다.")
+    mutating func order(_ coffee: Coffee) {
+        if let coffeePrice = yagombucks.menu[coffee] {
+            if money >= coffeePrice {
+                yagombucks.make(coffee, from: name)
+                money -= coffeePrice
+                yagombucks.sales += coffeePrice
+            } else if money <= coffeePrice {
+                print("잔액이 \(coffeePrice - money)원만큼 부족합니다.")
+                }
+            }
+        }
     }
-}
 
 struct CoffeeShop {
     var cafeName: String
     var barista: Person
     var menu: [Coffee: Int] = [:]
     var pickUpTable: [Coffee] = []
-    var sales: Int?
+    var sales: Int = 0
     
     func takeOrder(_ coffee: Coffee) {
         if let price = menu[coffee] {
             print("\(coffee.rawValue) \(price)원 결제 도와드리겠습니다.")
         }
     }
-        mutating func makeCoffee(_ coffee: Coffee) {
+    mutating func make(_ coffee: Coffee, from name: String) {
             pickUpTable.append(coffee)
-            print("주문하신 \(coffee.rawValue) 나왔습니다.")
+            print("\(name) 님이 주문하신 \(coffee.rawValue)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
     }
 }
 
@@ -45,10 +53,8 @@ enum Coffee: String {
     case chamomileTea = "카모마일티"
 }
 
-var misterLee: Person = Person(name: "misterLee", gender: "male", age: 26, money: 20000)
-var missKim: Person = Person(name: "missKim", gender: "female", age: 25, money: 50000)
-var yagombucks: CoffeeShop = CoffeeShop(cafeName: "yagombucks", barista: misterLee, menu:  [.americano: 4000, .cafeLatte: 4500, .cafeMocha: 5000, .grapefruitAde: 5500, .chamomileTea: 5500])
+var misterLee: Person = Person(name: "misterLee", gender: "male", age: 25, money: 10000)
+var missKim: Person = Person(name: "missKim", gender: "female", age: 20, money: 30000)
+var yagombucks: CoffeeShop = CoffeeShop(cafeName: "yagombucks", barista: misterLee, menu: [.americano: 4000, .cafeLatte: 4500, .cafeMocha: 5000, .grapefruitAde: 5500, .chamomileTea: 5500])
 
-missKim.orderCoffee(.cafeMocha)
-yagombucks.takeOrder(.cafeMocha)
-yagombucks.makeCoffee(.cafeMocha)
+missKim.order(.americano)
