071aaf3eabe358d5ed50d4132a4affc9a25f8e0c
500beckwon <kjsx60@gmail.com>
2022-11-06 12:19:51 +0900

    Properties Observer didSet 제작

--- CodeStarterCamp_Week3/Step1/CoffeeShop.swift
+++ CodeStarterCamp_Week3/Step1/CoffeeShop.swift
@@ -10,10 +10,15 @@ import Foundation
 class CoffeeShop {
     let menu: [Coffee: Int]
     let barista: Person
-    lazy var pickUpTable = [Coffee]()
+    lazy var orderer = ""
     lazy var totalSales = 0
+    lazy var pickUpTable: [Coffee] = [] {
+        didSet {
+            printOrder(coffee: pickUpTable.last)
+        }
+    }
     
-    init(menu: [Coffee : Int], barista: Person, pickUpTable: [Coffee] = [Coffee](), totalSales: Int = 0) {
+    init(menu: [Coffee: Int], barista: Person, pickUpTable: [Coffee] = [Coffee](), totalSales: Int = 0) {
         self.menu = menu
         self.barista = barista
         self.pickUpTable = pickUpTable
@@ -26,12 +31,23 @@ class CoffeeShop {
     ///   - from: 주문자 성함
     func make(_ coffee: Coffee, from name: String) {
         if let coffeePrice = getCoffeePrice(coffee) {
+            orderer = name
             pickUpTable.append(coffee)
             totalSales += coffeePrice
-            print("\(name) 님이 주문하신 \(coffee.rawValue)(이/가) 준비되었습니다 픽업대에서 가져가주세요.")
-        } 
+        }
     }
     
+    /// 주문 내용 출력
+    /// - Parameter coffee: 주문한 커피
+    func printOrder(coffee: Coffee?) {
+        if let coffee = coffee {
+            print("\(orderer) 님이 주문하신 \(coffee.rawValue)(이/가) 준비되었습니다 픽업대에서 가져가주세요.")
+        }
+    }
+    
+    /// 커피 가격 얻기
+    /// - Parameter coffee: 알고 싶은 커피
+    /// - Returns: Optional
     func getCoffeePrice(_ coffee: Coffee) -> Int? {
         return menu[coffee]
     }
