09df30d50517db48b8c817800e2640eb05d20cd2
kimseongj <ksj8277@naver.com>
2022-11-06 19:08:16 +0900

    Step1: 주석 제거

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -1,8 +1,5 @@
 import Foundation
 
-//var yagombucks: CoffeeShop
-//yagombucks.baristar = misterLee
-
 var missKim: Person = .init(name: "missKim", age: 26, gender: "Woman", money: 10000)
 var misterLee: Person = .init(name: "misterLee", age: 34, gender: "man", money: 3500)
 var kim: Person = .init(name: "kim", age: 25, gender: "man", money: 4500)
