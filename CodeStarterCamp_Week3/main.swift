b8ec75a79f959a38dd807edededa0f00d8dd33e1
hemil0102 <hemil0102@gmail.com>
2022-11-07 16:50:52 +0900

    week3 Step1 is added.

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,45 @@
 
 import Foundation
 
-print("Hello, World!")
+/// Person 타입을 정의합니다.
+/// 1. 프로퍼티:  돈
+/// 2. 메서드: 커피 구입
+struct Person {
 
+    var money: Int = 0
+    
+    func orderCoffee() {
+    }
+    
+}
+
+/// CoffeeShop 타입을 정의합니다.
+/// 1. 프로퍼티 매출액, 메뉴판(커피, 가격), 픽업테이블, 바리스타
+/// 2. 메서드: 주문받기, 커피제작
+struct CoffeeShop {
+    
+    var revenue: Int = 0
+    var menu: [String : Int] = [:]
+    var pickUpTabel: [Coffee] = []
+    var barista: String = ""
+    
+    func takeOrder() {
+    }
+    
+    func dropCoffee(_ coffee: Coffee) -> Coffee {
+        return coffee
+    }
+    
+}
+
+enum Coffee {
+    case americano, cafelatte, greentea
+}
+
+var misterLee: Person = Person()
+var missKim: Person = Person()
+var yagombucks: CoffeeShop = CoffeeShop()
+
+yagombucks.barista = "misterLee"
+yagombucks.pickUpTabel.append(yagombucks.dropCoffee(.americano))
+print(yagombucks.pickUpTabel)
