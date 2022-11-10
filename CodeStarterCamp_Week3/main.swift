b2a42de6fc90eba358ed2e8bc3d25db7290fdfb2
hemil0102 <hemil0102@gmail.com>
2022-11-11 00:10:37 +0900

    week3 step2 is added.

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -11,13 +11,22 @@ import Foundation
 /// Person 타입을 정의합니다.
 /// 1. 프로퍼티:  돈
 /// 2. 메서드: 커피 구입
-struct Person {
+class Person {
  
     var name: String = "Harry"
     var age: Int = 20
     var money: Int = 0
     
-    func orderCoffee() {
+    init(name: String, age: Int, money: Int) {
+        self.name = name
+        self.age = age
+        self.money = money
+    }
+    
+    func orderCoffee(order coffee: Coffee, to shop: CoffeeShop) {
+        print("\(name)가 \(shop.name)에 \(coffee)를 주문합니다.")
+        shop.dropCoffee(coffee, from: name)
+        money = money - (shop.menu[coffee] ?? 0)
     }
     
 }
@@ -25,18 +34,26 @@ struct Person {
 /// CoffeeShop 타입을 정의합니다.
 /// 1. 프로퍼티 매출액, 메뉴판(커피, 가격), 픽업테이블, 바리스타
 /// 2. 메서드: 주문받기, 커피제작
-struct CoffeeShop {
-    
+class CoffeeShop {
+
+    var name: String = "yagombucks"
     var revenue: Int = 0
-    var menu: [Coffee : Int] = [:]
+    var menu: [Coffee : Int]
     var pickUpTabel: [Coffee] = []
-    var baristar = Person()
+    var baristar = Person(name: "Harry", age: 20, money: 10000)
     
-    func takeOrder() {
+    init(name: String, revenue: Int, menu: [Coffee : Int], pickUpTabel: [Coffee], baristar: Person = Person(name: "Harry", age: 20, money: 10000)) {
+        self.name = name
+        self.revenue = revenue
+        self.menu = menu
+        self.pickUpTabel = pickUpTabel
+        self.baristar = baristar
     }
     
-    func dropCoffee(_ coffee: Coffee) -> Coffee {
-        return coffee
+    func dropCoffee(_ coffee: Coffee, from name: String) {
+        pickUpTabel.append(coffee)
+        print("\(name)이 주문하신 \(coffee)가 준비되었습니다. 픽업대에서 가져가주세요.")
+        revenue = revenue + (menu[coffee] ?? 0)
     }
     
 }
@@ -45,13 +62,10 @@ enum Coffee {
     case americano, cafelatte, greentea
 }
 
-var misterLee = Person()
-var missKim = Person()
-var yagombucks = CoffeeShop()
+var missKim = Person(name: "Jane", age: 30, money: 15000)
+var yagombucks = CoffeeShop(name: "yagombucks", revenue: 3550, menu:  [ .americano : 1000, .cafelatte : 1800, .greentea : 1500], pickUpTabel: [])
 
-misterLee.name = "Potter"
-yagombucks.baristar = misterLee
-print(yagombucks.baristar)
+print(missKim.orderCoffee(order: .americano, to: yagombucks))
 
-yagombucks.pickUpTabel.append(yagombucks.dropCoffee(.americano))
-print(yagombucks.pickUpTabel)
+print("\(missKim.name)이 커피 주문 이후 남은 돈은 \(missKim.money) 입니다.")
+print("\(yagombucks.name)이 현재 매출은 \(yagombucks.revenue) 입니다.")
