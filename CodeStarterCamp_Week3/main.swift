2adb431714fd88924250e8d5dd9579c9b6eb26a4
hemil0102 <hemil0102@gmail.com>
2022-11-11 21:06:01 +0900

    week3 step2 is modified.

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -24,11 +24,17 @@ class Person {
     }
     
     func orderCoffee(order coffee: Coffee, to shop: CoffeeShop) {
-        print("\(name)가 \(shop.name)에 \(coffee)를 주문합니다.")
-        shop.dropCoffee(coffee, from: name)
-        money = money - (shop.menu[coffee] ?? 0)
+        if money >= (shop.menu[coffee] ?? 0) {
+            print("\(name)가 \(shop.name)에 \(coffee)를 주문합니다.")
+            
+            money = money - (shop.menu[coffee] ?? 0)
+            print("\(name)이(가) 커피 주문 이후 남은 돈은 \(money) 입니다.")
+            
+            shop.dropCoffee(coffee, from: name)
+        } else {
+            print("잔액이 부족해서 주문을 할 수가 없어요. 지못미 ㅠ.ㅠ...")
+        }
     }
-    
 }
 
 /// CoffeeShop 타입을 정의합니다.
@@ -36,24 +42,30 @@ class Person {
 /// 2. 메서드: 주문받기, 커피제작
 class CoffeeShop {
 
-    var name: String = "yagombucks"
-    var revenue: Int = 0
+    var name: String
+    var revenue: Int
     var menu: [Coffee : Int]
-    var pickUpTabel: [Coffee] = []
+    var orderNumber = 0
+    var pickUpTable: [Coffee] {
+        willSet {
+            print("\(name)이 주문하신 \(newValue[orderNumber])가 준비되었습니다. 픽업대에서 가져가주세요.")
+        }
+    }
     var baristar = Person(name: "Harry", age: 20, money: 10000)
     
-    init(name: String, revenue: Int, menu: [Coffee : Int], pickUpTabel: [Coffee], baristar: Person = Person(name: "Harry", age: 20, money: 10000)) {
+    init(name: String, revenue: Int, menu: [Coffee : Int], pickUpTable: [Coffee]) {
         self.name = name
         self.revenue = revenue
         self.menu = menu
-        self.pickUpTabel = pickUpTabel
-        self.baristar = baristar
+        self.pickUpTable = pickUpTable
     }
     
     func dropCoffee(_ coffee: Coffee, from name: String) {
-        pickUpTabel.append(coffee)
-        print("\(name)이 주문하신 \(coffee)가 준비되었습니다. 픽업대에서 가져가주세요.")
+        pickUpTable.append(coffee)
+        orderNumber += 1
+       
         revenue = revenue + (menu[coffee] ?? 0)
+        print("\(name)의 현재 매출은 \(revenue) 입니다.")
     }
     
 }
@@ -62,10 +74,13 @@ enum Coffee {
     case americano, cafelatte, greentea
 }
 
-var missKim = Person(name: "Jane", age: 30, money: 15000)
-var yagombucks = CoffeeShop(name: "yagombucks", revenue: 3550, menu:  [ .americano : 1000, .cafelatte : 1800, .greentea : 1500], pickUpTabel: [])
+var missKim = Person(name: "Jane", age: 30, money: 3000)
+var yagombucks = CoffeeShop(name: "yagombucks", revenue: 3550, menu:  [ .americano : 1000, .cafelatte : 1800, .greentea : 1500], pickUpTable: [])
 
-print(missKim.orderCoffee(order: .americano, to: yagombucks))
+missKim.orderCoffee(order: .americano, to: yagombucks)
+missKim.orderCoffee(order: .greentea, to: yagombucks)
+missKim.orderCoffee(order: .cafelatte, to: yagombucks)
 
-print("\(missKim.name)이(가) 커피 주문 이후 남은 돈은 \(missKim.money) 입니다.")
-print("\(yagombucks.name)이(가) 현재 매출은 \(yagombucks.revenue) 입니다.")
+
+
+
