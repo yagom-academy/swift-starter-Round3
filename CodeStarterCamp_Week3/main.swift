9b583c5ea9936b128da2bed4662000ae96413556
Jae Ho Yoon <mr.jaeho@gmail.com>
2022-11-11 14:31:27 +0900

    Step1 Feedback Updated

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -9,10 +9,11 @@
 import Foundation
 
 class Person {
-    var name: String
-    var gender: String
+    let name: String
+    let gender: String
     var age: Int
     var money: Int
+    
     init(name: String, gender: String, age: Int, money: Int) {
         self.name = name
         self.gender = gender
@@ -20,8 +21,15 @@ class Person {
         self.money = money
     }
     
-    func order(coffee: Coffee) {
+    func order(coffee: Coffee, shopName: CoffeeShop) {
+        if let price = shopName.menu[coffee] {
+            money -= price
+        } else {
+            print("메뉴를 찾을 수 없습니다.")
+        }
         print("\(name)가 \(coffee.rawValue)를 구매합니다.")
+        print("남은 돈 \(money)입니다.")
+        shopName.takeOrder(coffee: coffee)
     }
     
     func leave(shopName: CoffeeShop) {
@@ -31,11 +39,12 @@ class Person {
 }
 
 class CoffeeShop {
-    var shopName: String
+    let shopName: String
     var sales: Int
     var barista: Person
     var pickUpTable = [Coffee]()
     var menu: [Coffee: Int] = [Coffee.americano: 4100, Coffee.cafeLatte: 4600, Coffee.caramelMacchiato: 5600]
+    
     init(shopName: String, sales: Int, barista: Person) {
         self.shopName = shopName
         self.sales = sales
@@ -44,7 +53,13 @@ class CoffeeShop {
     
     func takeOrder(coffee: Coffee) {
         print("\(coffee.rawValue) 주문 받았습니다.")
-    }
+        if let price = menu[coffee] {
+            sales += price
+        } else {
+            print("메뉴를 찾을 수 없습니다.")
+        }
+        make(coffee)
+        print("현재 매출액 \(sales)원입니다.")    }
     
     func make(_ coffee: Coffee) {
         print("\(coffee.rawValue) 준비되었습니다.")
@@ -55,6 +70,8 @@ enum Coffee: String {
     case americano = "아메리카노", cafeLatte = "카페 라떼", caramelMacchiato = "카라멜 마끼아또"
 }
 
-var misterLee: Person = Person(name: "Mr. Lee", gender: "male", age: 40, money: 20000)
-var missKim: Person = Person(name: "Ms. Kim", gender: "female", age: 25, money: 50000)
-var yagombucks: CoffeeShop = CoffeeShop(shopName: "YAGOMBUCKS", sales: 100000, barista: misterLee)
+let misterLee: Person = Person(name: "Mr. Lee", gender: "male", age: 40, money: 20000)
+let missKim: Person = Person(name: "Ms. Kim", gender: "female", age: 25, money: 50000)
+let yagombucks: CoffeeShop = CoffeeShop(shopName: "YAGOMBUCKS", sales: 0, barista: misterLee)
+
+missKim.order(coffee: Coffee.cafeLatte, shopName: yagombucks)
