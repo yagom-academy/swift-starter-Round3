524e0abed2f2b7252a7a517b27c80054c823ec57
Jae Ho Yoon <mr.jaeho@gmail.com>
2022-11-12 21:09:56 +0900

    STEP 2 제출

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -21,48 +21,58 @@ class Person {
         self.money = money
     }
     
-    func order(coffee: Coffee, shopName: CoffeeShop) {
-        if let price = shopName.menu[coffee] {
+    func order(_ coffee: Coffee, _ shop: CoffeeShop) {
+        print("\(name)님이 \(coffee.rawValue)(을/를) 구매합니다.")
+        
+        if let price = shop.menu[coffee] {
             money -= price
         } else {
             print("메뉴를 찾을 수 없습니다.")
         }
-        print("\(name)가 \(coffee.rawValue)를 구매합니다.")
-        print("남은 돈 \(money)입니다.")
-        shopName.takeOrder(coffee: coffee)
+        
+        guard money > 0 else {
+            return print("잔액이 \(-money)원만큼 부족합니다.")
+        }
+
+        print("\(name)님의 남은 돈은 \(money)원입니다.")
+        shop.make(coffee, from: name)
     }
     
-    func leave(shopName: CoffeeShop) {
-        print("\(name)가 \(shopName.shopName) 떠납니다.")
+    func leave(_ shop: CoffeeShop) {
+        print("\(name)님이 \(shop.shop)(을/를) 떠납니다.")
     }
     
 }
 
 class CoffeeShop {
-    let shopName: String
+    let shop: String
     var sales: Int
     var barista: Person
-    var pickUpTable = [Coffee]()
+    var pickUpTable = [String]()
     var menu: [Coffee: Int] = [Coffee.americano: 4100, Coffee.cafeLatte: 4600, Coffee.caramelMacchiato: 5600]
     
-    init(shopName: String, sales: Int, barista: Person) {
-        self.shopName = shopName
+    init(shop: String, sales: Int, barista: Person) {
+        self.shop = shop
         self.sales = sales
         self.barista = barista
     }
     
-    func takeOrder(coffee: Coffee) {
+    func make(_ coffee: Coffee, from name: String) {
         print("\(coffee.rawValue) 주문 받았습니다.")
+        
         if let price = menu[coffee] {
             sales += price
         } else {
             print("메뉴를 찾을 수 없습니다.")
         }
-        make(coffee)
-        print("현재 매출액 \(sales)원입니다.")    }
+        
+        print("현재 \(shop)의 매출액은 \(sales)원입니다.")
+        pickUp(coffee, from: name)
+    }
     
-    func make(_ coffee: Coffee) {
-        print("\(coffee.rawValue) 준비되었습니다.")
+    func pickUp(_ coffee: Coffee, from name: String) {
+        pickUpTable.append("\(coffee.rawValue)")
+        print("\(name) 님이 주문하신 \(coffee.rawValue)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
     }
 }
 
@@ -70,8 +80,10 @@ enum Coffee: String {
     case americano = "아메리카노", cafeLatte = "카페 라떼", caramelMacchiato = "카라멜 마끼아또"
 }
 
-let misterLee: Person = Person(name: "Mr. Lee", gender: "male", age: 40, money: 20000)
-let missKim: Person = Person(name: "Ms. Kim", gender: "female", age: 25, money: 50000)
-let yagombucks: CoffeeShop = CoffeeShop(shopName: "YAGOMBUCKS", sales: 0, barista: misterLee)
+let misterLee: Person = Person(name: "Mr. Lee", gender: "male", age: 40, money: 2900)
+let missKim: Person = Person(name: "Ms. Kim", gender: "female", age: 25, money: 10000)
+let yagombucks: CoffeeShop = CoffeeShop(shop: "YAGOMBUCKS", sales: 0, barista: misterLee)
 
-missKim.order(coffee: Coffee.cafeLatte, shopName: yagombucks)
+missKim.order(Coffee.cafeLatte, yagombucks)
+misterLee.order(Coffee.americano, yagombucks)
+misterLee.leave(yagombucks)
