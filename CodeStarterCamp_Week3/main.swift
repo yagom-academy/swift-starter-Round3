cf458d895ed372408526151be09789fc23063b0e
hemg2 <hemg2@naver.com>
2023-03-07 23:37:13 +0900

    step1 2차 피드백

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -17,22 +17,14 @@ enum Coffee: CustomStringConvertible {
     var description: String {
         switch self {
         case .americano:
-            return "americano"
+            return "아메리카노"
         case .latte:
-            return "latte"
+            return "라떼"
         case .javachipFrappuccino:
-            return "javachipFrappuccino"
+            return "자바칩프라푸치노"
         case .caramelMacchiato:
-            return "caramelMacchiato"
+            return "카라멜마끼아또"
         }
-    }
-}
-
-struct Price {
-    var price: Int
-    
-    init(price: Int) {
-        self.price = price
     }
 }
 
@@ -45,44 +37,46 @@ struct Person {
         self.money = money
     }
     
-    func buyCoffee(coffeeShop: CoffeeShop) {
-        print("\(name)은 가지고 있는 \(money)원으로 \(coffeeShop.coffeeShopName)에서 \(coffeeShop.barista)가 만들어주는 커피를 \(coffeeShop.menu)원을 주고 커피를 구매했다")
+    func buyCoffee(coffeeShop: CoffeeShop, coffee: Coffee) {
+        print("\(name)은 가지고 있는 \(money)원으로 \(coffeeShop.coffeeShopName)에서 \(coffeeShop.barista.name)가 만들어주는 \(coffeeShop.menu.keys)를 \(coffeeShop.menu.values)원을 주고 커피를 구매했다")
     }
 }
 
 struct CoffeeShop {
     var coffeeShopName: String
-    var sales: Int?
+    var sales: Int
     var barista: Person
     var menu: [Coffee: Int]
     var pickUpTable: [Coffee]
     
-    init(coffeeShopName: String, sales: Int? = nil, barista: Person, menu: [Coffee : Int], pickUpTable: [Coffee]) {
-        self.init(coffeeShopName: coffeeShopName, barista: barista, menu: menu, pickUpTable: pickUpTable)
-        self.sales = sales
-    }
-    init(coffeeShopName: String, barista: Person, menu: [Coffee: Int], pickUpTable: [Coffee]) {
+    init(coffeeShopName: String, sales: Int, barista: Person, menu: [Coffee : Int], pickUpTable: [Coffee]) {
         self.coffeeShopName = coffeeShopName
+        self.sales = sales
         self.barista = barista
         self.menu = menu
         self.pickUpTable = pickUpTable
     }
     
-    mutating func orderingCoffee(menus:Coffee, menu: [Coffee: Int]) {
-        print("\(menus)주문 받았습니다.  \(menu.values)원 입니다.")
-        print("\(menus)을 만들고 있습니다.")
+    mutating func orderingCoffee(menus:Coffee) {
+        print("\(menus)주문 받았습니다.")
+        if let price = menu[menus] {
+            print("주문하신 \(menus)의 가격은\(price)입니다.")
+            sales += price
+        }
         pickUpTable.append(menus)
     }
     
-    func makingCoffee() {
-        print("주문하신 \(pickUpTable)나왔습니다 테이블에서 가져가주세요")
+    func coffeePickupTable() {
+        print("테이블에는 \(pickUpTable)의 커피가 있습니다.")
     }
 }
 var missKim = Person(name: "missKim", money: 9700)
 var misterLee = Person(name: "misterLee", money: 9700)
-missKim.buyCoffee(coffeeShop: CoffeeShop(coffeeShopName: "오뚜기", barista: misterLee, menu: [.americano: 1500], pickUpTable: []))
+missKim.buyCoffee(coffeeShop: CoffeeShop(coffeeShopName: "오뚜기", sales: 200, barista: misterLee, menu: [.americano: 1500, ], pickUpTable: []), coffee: .americano)
 
-var yagombucks = CoffeeShop(coffeeShopName: "yagombucks", barista: misterLee, menu: [.caramelMacchiato: 5000], pickUpTable: [])
-yagombucks.orderingCoffee(menus: .caramelMacchiato, menu: [.caramelMacchiato: 2100])
-yagombucks.makingCoffee()
-print(" 픽업 테이블에는\(yagombucks.pickUpTable)")
+var yagombucks = CoffeeShop(coffeeShopName: "yagombucks", sales: 0, barista: misterLee,
+                            menu: [.americano: 2000, .latte: 2900, .javachipFrappuccino: 4500, .caramelMacchiato: 3500],
+                            pickUpTable: [])
+yagombucks.orderingCoffee(menus: .americano)
+yagombucks.orderingCoffee(menus: .javachipFrappuccino)
+yagombucks.coffeePickupTable()
