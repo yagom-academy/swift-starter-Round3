34330e6191db68946f9d3f8b79082dd3717ac64f
MaryJo-github <jotjdal0215@gmail.com>
2023-03-07 20:41:46 +0900

    Week3 [STEP 1] Mary 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -10,6 +10,6 @@ import Foundation
 
 var misterLee = Person(name: "misterLee", money: 3000)
 var missKim = Person(name: "missKim", money: 5000)
-var yagombucks = CoffeeShop(menu: [Coffee.americano: 3500, Coffee.decafAmericano: 4000, Coffee.vanillaLatte: 3800, Coffee.hazelnutLatte: 3800, Coffee.caramelmacchiato: 3900])
-yagombucks.barista = "misterLee"
+var yagombucks = CoffeeShop(menu: [Coffee.americano: 3500, Coffee.decafAmericano: 4000, Coffee.vanillaLatte: 3800, Coffee.hazelnutLatte: 3800, Coffee.caramelmacchiato: 3900], barista: misterLee)
+
 missKim.order(Coffee.americano, at: yagombucks)
--- CodeStarterCamp_Week3/step1.swift
+++ CodeStarterCamp_Week3/step1.swift
@@ -10,13 +10,10 @@ import Foundation
 struct Person {
     let name: String
     var money: Int
+    
     func order(_ coffee: Coffee, at coffeshop: CoffeeShop) {
         print("주문: \(coffee) (\(name))")
         coffeshop.make(coffee)
-    }
-    init(name: String, money: Int) {
-        self.name = name
-        self.money = money
     }
 }
 
@@ -24,14 +21,17 @@ class CoffeeShop {
     var salesRevenue: Int = 0
     var menu: [Coffee: Int]
     var pickUpTable: [Coffee] = []
-    var barista: String = "owner"
+    var barista: Person
+    
     func make(_ coffee: Coffee) {
-        print("바리스타 \(barista)가 음료 제조 중...")
+        print("바리스타 \(barista.name)가 음료 제조 중...")
         pickUpTable.append(coffee)
         print("\(coffee)가 pick-up 테이블에 준비되었습니다.")
     }
-    init(menu: [Coffee : Int]) {
+    
+    init(menu: [Coffee : Int], barista: Person) {
         self.menu = menu
+        self.barista = barista
     }
 }
 
