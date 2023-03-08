7681b30d3b64faa063d08f36609039d2e9096f8a
eve <rlawldms1385@gmail.com>
2023-03-08 14:42:21 +0900

    Add Step1

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,12 @@
 
 import Foundation
 
-print("Hello, World!")
-
+/* Step 1: 타입과 인스턴스의 관계 */
+var misterLee = Person(name: "misterLee")
+var missKim = Person(name: "missKim", money: 5000)
+let yagombucks = CoffeeShop(menu: [
+    Coffee.americano: 4500,
+    Coffee.latte: 5000,
+    Coffee.cappuccino: 5000,
+    Coffee.macchiato: 5900,
+], barista: misterLee)
