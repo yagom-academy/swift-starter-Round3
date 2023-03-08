c9f7edeab264ad63dd6ecec98457f9b1f6dbac45
hemg2 <hemg2@naver.com>
2023-03-08 23:46:01 +0900

    style:bindedCoffees

--- CodeStarterCamp_Week3/CoffeeShop.swift
+++ CodeStarterCamp_Week3/CoffeeShop.swift
@@ -12,9 +12,9 @@ struct CoffeeShop {
     var sales: Int
     var barista: Person
     var menu: [Coffee: Int]
-    var pickUpTable: [Coffee.RawValue]
+    var pickUpTable: [Coffee]
     
-    init(name: String, sales: Int, barista: Person, menu: [Coffee : Int], pickUpTable: [Coffee.RawValue]) {
+    init(name: String, sales: Int, barista: Person, menu: [Coffee : Int], pickUpTable: [Coffee]) {
         self.name = name
         self.sales = sales
         self.barista = barista
@@ -27,14 +27,14 @@ struct CoffeeShop {
             print("\(coffeeMenu)주문 받았습니다.")
             print("주문하신 \(coffeeMenu)의 가격은\(price)입니다.")
             sales += price
-            pickUpTable.append(coffeeMenu.rawValue)
+            pickUpTable.append(coffeeMenu)
         } else {
             print("주문 하신 \(coffeeMenu)은 저희 매장에 없는 메뉴 입니다.")
         }
     }
     
     func coffeeOnPickUpTable() {
-        let bindedCoffees = pickUpTable.joined(separator: ",")
+        let bindedCoffees = pickUpTable.map {$0.rawValue}.joined(separator: ",")
         print("테이블에는 \(bindedCoffees)의 커피가 있습니다.")
     }
 }
--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -15,11 +15,10 @@ enum Coffee: String {
     case caramelMacchiato
 }
 
-var missKim = Person(name: "missKim", money: 9700)
-var misterLee = Person(name: "misterLee", money: 9700)
-missKim.buyCoffee(coffeeShop: CoffeeShop(name: "오뚜기", sales: 200, barista: misterLee, menu: [.americano: 1500], pickUpTable: []), coffee: .americano)
+var missKim = Person(name: "missKim", money: 35000)
+missKim.buyCoffee(coffeeShop: CoffeeShop(name: "오뚜기", sales: 200, barista: missKim, menu: [.americano: 1500], pickUpTable: []), coffee: .americano)
 
-var yagombucks = CoffeeShop(name: "yagombucks", sales: 0, barista: misterLee,
+var yagombucks = CoffeeShop(name: "yagombucks", sales: 0, barista: missKim,
                             menu: [.americano: 2000, .latte: 2900, .javachipFrappuccino: 4500],
                             pickUpTable: [])
 yagombucks.takeOrderCoffee(coffeeMenu: .americano)
