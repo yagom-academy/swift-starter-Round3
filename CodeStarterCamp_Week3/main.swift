62de2e9448f6d6d213776dd779a5b83774e8f106
Charles <kch1759@naver.com>
2023-03-09 20:49:08 +0900

    Step1

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,52 @@
 
 import Foundation
 
-print("Hello, World!")
+
+
+struct Person {
+    var money: Int
+    
+    mutating func buyCoffee(coffeePrice: CoffeShop, coffee: Coffee) {
+        
+        if let price = coffeePrice.menu[coffee] {
+            self.money -= price
+        }
+
+    }
+}
+
+
+struct CoffeShop {
+    var barista: Person!
+    var sales: Int
+    var menu: Dictionary<Coffee, Int>
+    var pickUpTable: [Coffee]
+    
+    mutating func takeOrder(for coffee: Coffee) -> Coffee {
+        if coffee == Coffee.latte {
+            self.pickUpTable.append(coffee)
+        } else if coffee == .americano {
+            self.pickUpTable.append(coffee)
+        } else {
+            self.pickUpTable.append(coffee)
+        }
+        
+        return pickUpTable[pickUpTable.endIndex - 1]
+    }
+    
+    
+}
+
+enum Coffee {
+    case latte
+    case americano
+    case lemonade
+}
+
+var misterLee: Person = Person(money: 1000)
+var missKim: Person = Person(money: 2000)
+
+var yagombucks: CoffeShop = CoffeShop(sales: 0, menu: [:], pickUpTable: [])
+yagombucks.barista = misterLee
+
 
