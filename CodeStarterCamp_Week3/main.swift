efad3183e6a9a2711dfa77be3582bcc198facae4
dduddupark <develop.dduddu@gmail.com>
2023-03-10 09:26:50 +0900

    step1 피드백 03/10 09:26

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -13,22 +13,29 @@ import Foundation
  */
 
 var misterLee = Person(totalMoney: 10000)
-var missKim = Person(totalMoney: 1000)
+var missKim = Person(totalMoney: 100)
+var missPark = Person(totalMoney: 10000)
 
-var yagombucks = CoffeShop(
-    menus: [
-        CoffeShop.Menu(coffeeKind: .Americano, price: 400),
-        CoffeShop.Menu(coffeeKind: .CaffeeLatte, price: 500),
-        CoffeShop.Menu(coffeeKind: .CaffeeMocha, price: 1000)
-    ])
+var yagombucks = CoffeShop(menus: [CoffeShop.Menu(coffee: .Americano, price: 400),
+                                   CoffeShop.Menu(coffee: .CaffeeLatte, price: 500)])
 
 
-yagombucks.takeOrder(person: missKim, coffees: [.Americano])
+yagombucks.takeOrder(person: missKim, coffees: .CaffeeMocha)
 
 yagombucks.barista = misterLee
 
-yagombucks.takeOrder(person: missKim, coffees: [.Americano, .CaffeeMocha])
+yagombucks.takeOrder(person: missKim)
+
+yagombucks.takeOrder(person: missKim, coffees: .Americano)
 
 missKim.totalMoney = 10000
 
-yagombucks.takeOrder(person: missKim, coffees: [.Americano, .CaffeeMocha])
+yagombucks.takeOrder(person: missKim, coffees: .CaffeeMocha, .Americano)
+
+yagombucks.takeOrder(person: missKim, coffees: .Americano, .CaffeeLatte)
+
+yagombucks.barista = missPark
+
+yagombucks.takeOrder(person: missKim, coffees: .Americano, .Americano, .Americano)
+
+print("영업 종료 : 총 매출액 \(yagombucks.salesRevenue)원")
--- CodeStarterCamp_Week3/step1.swift
+++ CodeStarterCamp_Week3/step1.swift
@@ -11,13 +11,17 @@ class Person {
     var totalMoney: Int
     
     func buyCoffee(spendMoney: Int) -> Bool {
-        if  totalMoney >= spendMoney {
-            totalMoney = totalMoney - spendMoney
-            print("커피가 계산 되었습니다. 총 잔액은 \(totalMoney)원 입니다.")
+        if totalMoney >= spendMoney {
+            calculateMoney(spendMoney: spendMoney)
             return true
         } else {
             return false
         }
+    }
+    
+    private func calculateMoney(spendMoney: Int) {
+        totalMoney = totalMoney - spendMoney
+        print("커피가 계산 되었습니다. 총 잔액은 \(totalMoney)원 입니다.")
     }
     
     init (totalMoney: Int) {
@@ -26,24 +30,30 @@ class Person {
 }
 
 class CoffeShop {
-    var salesRevenue: Int = 0
+    var salesRevenue: Int
     var menus: [Menu]
-    var pickUpTable: [Coffee] = []
+    var pickUpTable: [Coffee] {
+        willSet {
+            print("모든 음료의 제조가 완료되었습니다. 픽업 부탁드립니다:)")
+        }
+    }
     var barista: Person? = nil {
         willSet (newValue) {
             if newValue != nil {
-                print("바리스타가 생겼습니다!")
+                print("새로운 바리스타가 등장하였습니다!")
             }
         }
     }
     
     init(menus: [Menu]) {
         print("카페가 생겼어요!!!")
+        self.salesRevenue = 0
         self.menus = menus
+        self.pickUpTable = []
     }
     
     struct Menu {
-        var coffeeKind: Coffee
+        var coffee: Coffee
         var price: Int
     }
     
@@ -53,41 +63,57 @@ class CoffeShop {
         case CaffeeMocha
     }
     
-    func takeOrder(person: Person, coffees: [Coffee]) {
-        
-        if barista != nil {
-            
-            var price = 0
-            
-            coffees.forEach { coffee in
-                let menu = menus.filter({ $0.coffeeKind == coffee }).first
-                
-                if let menu = menu {
-                    print("\(menu.coffeeKind) 주문 받았습니다.")
-                    price = price + menu.price
-                }
-            }
-            
-            print("총 \(price)원 입니다.")
-            
-            if missKim.buyCoffee(spendMoney: price) {
-                makeCoffee(coffees: coffees)
-                salesRevenue = price
-            } else {
-                print("잔액이 부족합니다.")
-            }
-        } else {
+    func takeOrder(person: Person, coffees: Coffee...) {
+        guard barista != nil else {
             print("현재 주문을 받을 수 있는 바리스타가 없어요ㅠㅠ")
+            return
+        }
+        
+        if coffees.count == 0 {
+            print("받은 주문이 없어요ㅠㅠ")
+            return
+        }
+        
+        let totalPrice = getTotalPrice(coffees: coffees)
+        if totalPrice > 0 {
+            print("총 \(totalPrice)원 입니다.")
+            checkCanMakeCoffee(person: person, price: totalPrice, coffees: coffees)
         }
     }
     
     private func makeCoffee(coffees: [Coffee]) {
-        print("커피 만드는 중입니다...")
-        
         coffees.forEach { coffee in
-            print("주문하신 \(coffee)가 나왔습니다.")
+            print("\(coffee) 제조가 완료되었습니다.")
         }
         
-        pickUpTable = coffees
+        pickUpTable = pickUpTable + coffees
+    }
+    
+    private func getTotalPrice(coffees: [Coffee]) -> Int {
+        var price = 0
+        
+        for coffee in coffees {
+            let menuOrNil = menus.filter({ $0.coffee == coffee }).first
+            
+            guard let menu = menuOrNil else {
+                print("#주문을 받는중 오류가 발생하였습니다#")
+                price = 0
+                break
+            }
+            
+            print("\(menu.coffee) 주문 받았습니다.")
+            price = price + menu.price
+        }
+        
+        return price
+    }
+    
+    private func checkCanMakeCoffee(person: Person, price: Int, coffees: [Coffee]) {
+        if person.buyCoffee(spendMoney: price) {
+            makeCoffee(coffees: coffees)
+            salesRevenue = salesRevenue + price
+        } else {
+            print("잔액이 부족합니다.")
+        }
     }
 }
