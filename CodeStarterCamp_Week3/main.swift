311732d14b3a9d4d48d561c7ff4e98b65f99d8b3
bubblecocoa <bubblecocoa@gmail.com>
2023-03-11 23:44:31 +0900

    Feat: Step 2: 인스턴스는 서로 소통한다.
    Person구조체와 CoffeeShop구조체로 선언된 인스턴스간의 소통

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -22,13 +22,24 @@ struct Person {
     let name: String
     var money: Int
     
-    mutating func order(_ coffee: Coffee, to coffeeShop: CoffeeShop) {
+    mutating func order(_ coffee: Coffee, of coffeeShop: inout CoffeeShop) {
+        guard coffeeShop.hasCoffee(coffee) else { return }
+        
         guard let price = coffeeShop.menu[coffee] else {
-            print("\(coffeeShop.name)에는 \(coffee.rawValue)가 없습니다.")
+            print("\(coffee.rawValue)의 가격 정보가 없어 주문할 수 없습니다.\n")
             return
         }
         
-        money -= price
+        if money > price {
+            money -= price
+            coffeeShop.takeOrder(coffee, price: price, by: self)
+        } else {
+            print("""
+            잔액이 \(price - money)원 부족합니다.
+            \(coffee.rawValue)의 가격: \(price)원
+            \(name)님의 잔액: \(money)원\n
+            """)
+        }
     }
 }
 
@@ -39,17 +50,31 @@ struct CoffeeShop {
     var pickUpTable: [Coffee] = []
     var barista: Person
     
-    func takeOrder(from person: Person, _ coffee: Coffee) {
-        // TODO: 주문확인 코드 후 make 함수 호출
+    func hasCoffee(_ coffee: Coffee) -> Bool {
+        if menu.keys.contains(coffee) {
+            return true
+        } else {
+            print("\(name)에는 \(coffee.rawValue)가 없습니다.\n")
+            
+            return false
+        }
     }
     
-    mutating func make(_ coffee: Coffee) {
+    mutating func takeOrder(_ coffee: Coffee, price:Int, by person: Person) {
+        totalSalesPrice += price
+        
+        make(coffee, for: person)
+    }
+    
+    mutating func make(_ coffee: Coffee, for person: Person) {
         pickUpTable.append(coffee)
+        
+        print("\(person.name)님이 주문하신 \(coffee.rawValue)가 준비되었습니다. 픽업대에서 가져가주세요.\n")
     }
 }
 
 var misterLee = Person(name: "이곰돌", money: 10000)
-var missKim = Person(name: "김곰순", money: 20000)
+var missKim = Person(name: "김곰순", money: 3300)
 var yagombucksMenu: [Coffee: Int] = [
     .americano: 4000,
     .caffeLatte: 4500,
@@ -61,3 +86,9 @@ var yagombucks = CoffeeShop(
     menu: yagombucksMenu,
     barista: misterLee
 )
+
+missKim.order(.caffeLatte, of: &yagombucks)
+misterLee.order(.espresso, of: &yagombucks)
+misterLee.order(.vanilaLatte, of: &yagombucks)
+misterLee.order(.americano, of: &yagombucks)
+misterLee.order(.caffeMocaha, of: &yagombucks)
