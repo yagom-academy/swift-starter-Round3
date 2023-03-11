6931baf4bf0e485b5e91c30f4ad2c2dfe02840e5
Hyeok <huk000808@gmail.com>
2023-03-11 12:14:26 +0900

    main.swift 수정4

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -1,15 +1,20 @@
-class Person {
+import Foundation
+
+//
+//  main.swift
+//  CodeStarterCamp_Week2
+//
+//  Created by yagom.
+//  Copyright :copyright: yagom academy. All rights reserved.
+//
+
+struct Person {
     var name: String
     var age: Int
     var cardBalance: Int
     
-    init(name: String, age: Int, cardBalance: Int) {
-        self.name = name
-        self.age = age
-        self.cardBalance = cardBalance
-    }
     
-    func orderMenu(coffeePrice: Int) {
+    mutating func orderMenu(coffeePrice: Int) {
         if cardBalance >= coffeePrice {
             cardBalance -= coffeePrice
             print("주문이 완료되었습니다.")
@@ -21,34 +26,34 @@ class Person {
 }
 
 enum Coffee {
-    case americano, capuccino, latte, espresso, mocha
+    case americano, cappuccino, latte, espresso, mocha
 }
 
 struct CoffeeShop {
     var coffeeShopName: String
     var totalRevenue: Int = 0
-    var coffeeMenu: [Coffee: Int] = [.americano: 4500, .capuccino: 5000, .espresso: 4000, .latte: 5000, .mocha: 5500]
+    var coffeeMenu: [Coffee: Int] = [.americano: 4500, .cappuccino: 5000, .espresso: 4000, .latte: 5000, .mocha: 5500]
     var pickUpTable: [Coffee] = []
     var barista: Person
-        
-    mutating func takeOrder(orderCoffee: Coffee, customer: Person) {
-        print("손님, 원하시는 음료 \(orderCoffee)가 맞으시나요?")
+    
+    mutating func takeOrder(likeCoffee: Coffee, customer: inout Person) {
+        print("손님, 원하시는 음료 \(likeCoffee)가 맞으시나요?")
         print("결제 도와드리겠습니다.")
-        if let orderCoffeePrice = coffeeMenu[orderCoffee] {
-            print("주문하신 \(orderCoffee)의 가격은 \(orderCoffeePrice)원 입니다.")
+        if let orderCoffeePrice = coffeeMenu[likeCoffee] {
+            print("주문하신 \(likeCoffee)의 가격은 \(orderCoffeePrice)원 입니다.")
             customer.cardBalance -= orderCoffeePrice
             totalRevenue += orderCoffeePrice
             print("결제가 완료되었습니다. 잠시만 기다려주세요.")
             for _ in 1...3 {
                 print("...............................")
             }
-            makeCoffee(cutomerCoffee: orderCoffee)
+            makeCoffee(cutomerOrder: likeCoffee)
         }
     }
     
-    mutating func makeCoffee(cutomerCoffee: Coffee) {
-        print("주문하신 \(cutomerCoffee)가 나왔습니다.")
-        pickUpTable.append(cutomerCoffee)
+    mutating func makeCoffee(cutomerOrder: Coffee) {
+        print("주문하신 \(cutomerOrder)가 나왔습니다.")
+        pickUpTable.append(cutomerOrder)
     }
 }
 
@@ -56,5 +61,4 @@ struct CoffeeShop {
 var misterLee: Person = Person(name: "misterLee", age: 28, cardBalance: 400000)
 var missKim: Person = Person(name: "missKim", age: 27, cardBalance: 600000)
 
-var yagombucks: CoffeeShop = CoffeeShop(coffeeShopName: "yagombucks", barista: missKim)
-
+var yagombucks: CoffeeShop = CoffeeShop(coffeeShopName: "yagombucks", barista: misterLee)
