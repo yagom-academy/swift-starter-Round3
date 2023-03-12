51ca1495c92dbd4395f88afe654c551988ac67c1
Min Hyun <minnimida@gmail.com>
2023-03-12 13:50:23 +0900

    Week3 STEP 2 struct로 교체

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,14 +8,10 @@
 
 import Foundation
 
-class Person {
-    var money: Int
+struct Person {
+    var money: Int = 0
     
-    init(money: Int = 0) {
-        self.money = money
-    }
-    
-    func hasEnoughMoney(for productPrice: Int) -> Bool {
+    func isPaymentEnable(for productPrice: Int) -> Bool {
         if productPrice > self.money {
             print("잔액이 \(productPrice - self.money)원만큼 부족합니다.")
             return false
@@ -24,12 +20,11 @@ class Person {
         }
     }
     
-    func order(_ coffee: Coffee, of coffeeShop: CoffeeShop, by name: String) {
-        if let coffeePrice = coffeeShop.checkPrice(coffee) {
-            if self.hasEnoughMoney(for: coffeePrice) {
-                self.money -= coffeePrice
-                coffeeShop.processPurchase(for: coffee, with: coffeePrice, from: name)
-            }
+    mutating func order(_ coffee: Coffee, of coffeeShop: inout CoffeeShop, by name: String) {
+        guard let coffeePrice = coffeeShop.checkPrice(coffee) else { return }
+        if self.isPaymentEnable(for: coffeePrice) {
+            self.money -= coffeePrice
+            coffeeShop.processPurchase(for: coffee, with: coffeePrice, from: name)
         }
     }
 }
@@ -42,32 +37,31 @@ enum Coffee: String {
     case cappuccino = "카푸치노"
 }
 
-class CoffeeShop {
+struct CoffeeShop {
     var menu: Dictionary<Coffee, Int>
     var barista: Person
     var pickUpTable: Array<Coffee> = Array<Coffee>()
     var revenue: Int = 0
     
-    init(menu: Dictionary<Coffee, Int>, barista: Person) {
-        self.barista = barista
-        self.menu = menu
-    }
-    
     func checkPrice(_ coffee: Coffee) -> Int? {
-        if let coffeePrice = self.menu[coffee] {
-            return coffeePrice
-        } else {
+        guard let coffeePrice = self.menu[coffee] else {
             print("주문하신 메뉴가 존재하지 않습니다.")
             return nil
         }
+        return coffeePrice
     }
     
-    func make(_ coffee: Coffee, for name: String) {
+    mutating func addCoffeeToPickUpTable(_ coffee: Coffee, for name: String) {
         self.pickUpTable.append(coffee)
         print("\(name) 님이 주문하신 \(coffee.rawValue)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
     }
     
-    func processPurchase(for coffee: Coffee, with price: Int, from name: String) {
+    mutating func make(_ coffee: Coffee, for name: String) {
+        print("커피 추출중...")
+        self.addCoffeeToPickUpTable(coffee, for: name)
+    }
+    
+    mutating func processPurchase(for coffee: Coffee, with price: Int, from name: String) {
         self.revenue += price
         self.make(coffee, for: name)
     }
@@ -83,8 +77,8 @@ var yagombucks = CoffeeShop(menu: [Coffee.espresso: 3000,
                                    Coffee.cappuccino: 4500],
                             barista: misterLee)
 
-missKim.order(Coffee.cappuccino, of: yagombucks, by: "missKim")
-zzanggoo.order(Coffee.americano, of: yagombucks, by: "커피를못마시는짱구")
-minhyunm.order(Coffee.americano, of: yagombucks, by: "minhyunm")
+missKim.order(Coffee.cappuccino, of: &yagombucks, by: "missKim")
+zzanggoo.order(Coffee.americano, of: &yagombucks, by: "커피를못마시는짱구")
+minhyunm.order(Coffee.americano, of: &yagombucks, by: "minhyunm")
 
 
