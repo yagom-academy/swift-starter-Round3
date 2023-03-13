d855c31e117cd06294913e6a248a9f662af0b586
karen <karenrodanthe@icloud.com>
2023-03-13 11:16:29 +0900

    230313 week3-STEP01 피드백 수정 완료

---
+++ CodeStarterCamp_Week3/Step1.swift
@@ -0,0 +1,49 @@
+//
+//  Step1.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by karen on 2023/03/13.
+//
+
+let misterLee = Person(money: 1000)
+let missKim = Person(money: 1000)
+var yagomMenu: Dictionary<Coffee, Int> = [Coffee.moca: 5000, Coffee.iceMoca: 5500, Coffee.americano: 3000, Coffee.iceAmericano: 3500, Coffee.caramel: 4500, Coffee.iceCaramel: 5000, Coffee.espresso: 2500, Coffee.cappuccino: 4000, Coffee.hazelnut: 4000, Coffee.latte: 4000, Coffee.iceLatte: 4500]
+var yagombucks = CoffeeShop(totalSales:0, barista: misterLee, menu: yagomMenu)
+
+
+enum Coffee: String {
+    case moca = "카페 모카", iceMoca = "아이스 카페모카"
+    case americano = "아메리카노", iceAmericano = "아이스 아메리카노"
+    case caramel = "카라멜 마끼야또", iceCaramel = "아이스 카라멜 마끼야또"
+    case espresso = "에스프레소"
+    case cappuccino = "카푸치노"
+    case hazelnut = "헤이즐넛"
+    case latte = "카페 라떼", iceLatte = "아이스 카페 라떼"
+}
+
+struct Person {
+    var money: Int
+    func purchaseCoffee(_ coffee: Coffee) {
+        print("\(coffee)를 구매합니다.")
+    }
+}
+
+struct CoffeeShop {
+    var totalSales: Int
+    var barista: Person
+    var menu: Dictionary<Coffee, Int>
+    var pickUpTable: Array<Coffee> = Array<Coffee>()
+    
+    mutating func takeOnOrder(_ coffee: Coffee) {
+        print("주문받겠습니다.")
+        createCoffee(order: coffee)
+    }
+    
+    mutating func createCoffee(order: Coffee) {
+        pickUpTable.append(order)
+        print("주문하신 \(order.rawValue) 나왔습니다.")
+    }
+    
+}
+
+
