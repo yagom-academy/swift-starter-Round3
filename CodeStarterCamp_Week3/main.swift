523072a77ade7a628328eb9fa8df4acfbf78d1f8
dev-TOBY <yechan12356@gmail.com>
2023-03-14 20:57:59 +0900

    week3 step2 commit ver1.1

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -10,11 +10,10 @@ struct Person {
                 print("잔액이 \(price - money)원만큼 부족합니다.")
             } else {
                 money -= price
-                coffeeShop.salesAmount += price
                 coffeeShop.makeOrderedCoffee(coffee, for: name)
             }
         } else {
-            print("결제 오류")
+            print("존재하지 않는 메뉴입니다.")
         }
     }
 }
@@ -32,10 +31,11 @@ class CoffeeShop {
         self.beanType = beanType
         self.tableNumbers = tableNumbers
         self.menu = menu
-    }
+    }   
     
     func makeOrderedCoffee(_ coffee: Coffee, for name: String) {
         pickUpTable = [coffee]
+        salesAmount += menu[coffee] ?? 0
         print("\(name)님이 주문하신 \(coffee.rawValue)가(이) 준비되었습니다. 픽업대에서 가져가주세요.")
     }
 }
@@ -56,7 +56,3 @@ var yagombucks: CoffeeShop = CoffeeShop(baristaName: "misterLee", beanType: .ara
                                         menu: [.americano: 5000, .cafeLatte : 5500, .cappuccino: 5800, .einspanner: 6500])
 
 missKim.orderCoffee(.cappuccino, at: yagombucks)
-missKim.orderCoffee(.einspanner, at: yagombucks)
-missKim.orderCoffee(.cappuccino, at: yagombucks)
-print("yagombucks의 매출: \(yagombucks.salesAmount)원")
-print("missKim님의 남은 돈: \(missKim.money)원")
