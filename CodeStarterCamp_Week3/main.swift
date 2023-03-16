c4446e1aab901550161407c670a4bce5a17f1ac3
Tag <sea15510@gmail.com>
2023-03-16 20:40:19 +0900

    week3_step2 v1.0 작성

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -26,16 +26,15 @@ struct Person {
         self.money = money
     }
     
-    mutating func orderCoffee(coffeeShop: CoffeeShop, for coffees: Coffee...) {
-        for coffee in coffees {
-            if let price = coffeeShop.menu[coffee] {
-                if money >= price {
-                    print("\(coffee)를 주문합니다.")
-                    self.money = money - price
-                    print("금액이 \(price)만큼 차감됩니다.\n남은 금액은 \(self.money)입니다.")
-                } else {
-                    print("가지고 있는 돈으로는 구매할 수 없습니다.\n현재 가지고 계신 금액은 \(self.money)입니다.")
-                }
+    mutating func order(_ coffee: Coffee, of coffeeShop: CoffeeShop, by name: String) {
+        print("\(coffee)를 주문합니다.")
+        if let price = coffeeShop.menu[coffee] {
+            if money >= price {
+                self.money = money - price
+                print("금액이 \(price)만큼 차감됩니다.\n\(name)님의 남은 금액은 \(self.money)입니다.")
+                coffeeShop.make(coffee, for: name)
+            } else {
+                print("잔액이 \(price - self.money)원만큼 부족합니다.")
             }
         }
     }
@@ -60,20 +59,18 @@ class CoffeeShop {
         self.barista = barista
     }
     
-    func takeOrder(_ coffees: Coffee...) {
-        for coffee in coffees {
-            if let price = menu[coffee] {
-                self.sales += price
-                makeCoffee(coffee)
-            } else {
-                print("\(coffee)는 메뉴에 없습니다.")
-            }
+    func takeOrder(_ coffee: Coffee) {
+        if let price = menu[coffee] {
+            self.sales += price
+        } else {
+            print("손님 \(coffee)는 메뉴에 없습니다.")
         }
     }
     
-    func makeCoffee(_ coffees: Coffee) {
-        self.pickUpTable.append(coffees)
-        print("주문하신 \(coffees) 나왔습니다.")
+    func make(_ coffee: Coffee, for name: String) {
+        takeOrder(coffee)
+        self.pickUpTable.append(coffee)
+        print("\(name)님이 주문하신 \(coffee)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
     }
 }
 
@@ -86,12 +83,11 @@ enum Coffee: String {
     case vanillaLatte = "vanillaLatte"
 }
 
-var misterLee = Person(nationality: "Korea", birthYear: 1990, birthDate: 0509, money: 4000)
-var missKim = Person(nationality: "Korea", birthYear: 1997, birthDate: 1111, money: 10000)
+var missKim = Person(nationality: "Korea", birthYear: 1997, birthDate: 1111, money: 2000)
+var 커피를못마시는짱구 = Person(nationality: "Japan", birthYear: 1998, birthDate: 1212, money: 10000)
 var yagombucks = CoffeeShop(name: "yagombucks", menu: [Coffee.americano : 2000, Coffee.honeyAmericano : 3000, Coffee.cappuccino: 4000, Coffee.cafeLatte: 4000], sales: 0, pickUpTable: [])
 
 yagombucks.barista = "misterLee"
 
-misterLee.orderCoffee(coffeeShop: yagombucks, for: Coffee.americano)
-yagombucks.takeOrder(Coffee.americano)
-print(yagombucks.pickUpTable)
+missKim.order(Coffee.americano, of: yagombucks, by: "missKim")
+커피를못마시는짱구.order(Coffee.honeyAmericano, of: yagombucks, by: "커피를못하시는짱구")
