8f390f0b8a4c32f0145a926c4f328edade34ed0f
deanidh <deanidh1011@gmail.com>
2023-03-17 23:04:29 +0900

    feat: order 함수 기능 분리
    checkMoney 함수로 기능 일부 분리

--- CodeStarterCamp_Week3/Person.swift
+++ CodeStarterCamp_Week3/Person.swift
@@ -15,16 +15,25 @@ struct Person {
         money -= price
     }
     
-    mutating func order(_ coffee: Coffee, of coffeeShop: CoffeeShop, by name: String) {
+    func checkMoney(_ coffee: Coffee, of coffeeShop: CoffeeShop) -> Bool {
         if let coffeePrice = coffeeShop.menu[coffee] {
             if money < coffeePrice {
                 print("잔액이 \(coffeePrice - money)원만큼 부족합니다.")
+                return false
             } else {
-                pay(price: coffeePrice)
-                coffeeShop.make(coffee, for: name)
+                return true
             }
         } else {
             print("존재하지 않는 메뉴입니다.")
+            return false
+        }
+    }
+    
+    mutating func order(_ coffee: Coffee, of coffeeShop: CoffeeShop, by name: String) {
+        if checkMoney(coffee, of: coffeeShop),
+           let coffeePrice = coffeeShop.menu[coffee] {
+            pay(price: coffeePrice)
+            coffeeShop.make(coffee, for: name)
         }
     }
 }
