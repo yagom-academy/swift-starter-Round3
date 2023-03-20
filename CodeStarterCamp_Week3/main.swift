09a31840a92da62625848c2623a89fed3205dc59
제이드 <a_jb97@naver.com>
2023-03-20 21:58:41 +0900

    Step 1 코드 작성

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,40 @@
 
 import Foundation
 
-print("Hello, World!")
+class Person {
+    var money: Int
+    init(money: Int) {
+        self.money = money
+    }
+    
+    func orderCoffee(menu: Coffee, coffeeShop: CoffeeShop, by name: String) {
+        coffeeShop.makeCoffee(coffee: menu, for: name)
+    }
+}
 
+class CoffeeShop {
+    var barista: Person?
+    var sales: Int
+    let menu: Dictionary<Coffee, Int>
+    var pickUpTable: Array<Coffee> = []
+    init(sales: Int, menu: Dictionary<Coffee, Int>, pickUpTable: Array<Coffee>) {
+        self.sales = sales
+        self.menu = menu
+        self.pickUpTable = pickUpTable
+    }
+    
+    func makeCoffee(coffee: Coffee, for name: String) {
+        pickUpTable.append(coffee)
+    }
+}
+
+enum Coffee: String {
+    case espresso = "에스프레소", americano = "아메리카노", cafeLatte = "카페라떼", vanillaLatte = "바닐라 라떼", cappuccino = "카푸치노", einspanner = "아인슈페너"
+}
+
+let misterLee: Person = Person(money: 20000)
+let missKim: Person = Person(money: 30000)
+var yagombucks: CoffeeShop = CoffeeShop(sales: 0, menu: [.espresso: 2000, .americano: 3000, .cafeLatte: 4000, .einspanner: 5000], pickUpTable: [])
+yagombucks.barista = misterLee
+
+missKim.orderCoffee(menu: .americano, coffeeShop: yagombucks, by: "missKim")
