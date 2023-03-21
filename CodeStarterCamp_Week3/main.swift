2fb6712058914838ca47e13464ba00a361b7ad5e
Hot-ttu <ghkdwndud7@naver.com>
2023-03-22 01:29:47 +0900

    Week3 Step1 PR

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
+class Person {
+    var money: Int = 0
+    
+    func buyCoffee() {
+        print("buy coffee.")
+    }
+}
 
+class CoffeeShop {
+    var sales: Int = 0
+    var menuTable: [String: Int] = [:]
+    var pickUpTable: [Coffee] = []
+    var barista: Person?
+    
+    func takeOrder(order: Coffee) {
+        makeCoffee(coffee: order)
+    }
+    
+    func makeCoffee(coffee: Coffee) {
+        pickUpTable.append(coffee)
+    }
+}
+
+enum Coffee {
+    case americano, latte, espresso, iceTea
+}
+
+let misterLee: Person = Person()
+let missKim: Person = Person()
+let yagombucks: CoffeeShop = CoffeeShop()
+yagombucks.barista = misterLee
