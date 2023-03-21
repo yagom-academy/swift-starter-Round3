c37437cd89ac5add6c6360fe488f5e9a99c3a2e1
j1nt0 <sk2018univ@naver.com>
2023-03-21 20:28:20 +0900

    step 2.1

---
+++ Step2/Step2/main.swift
@@ -0,0 +1,64 @@
+//
+//  main.swift
+//  Step2
+//
+//  Created by 이진 on 2023/03/21.
+//
+
+import Foundation
+
+enum Coffee: String {
+    case americano = "아메리카노"
+    case caffeLatte = "라떼"
+    case greenTea = "녹차"
+}
+
+class CoffeeShop {
+    var sales: Int
+    var pickUpTable: Array<String> = Array<String>()
+    
+    init(sales: Int){
+        self.sales = sales
+    }
+    
+    func make(_ coffee: Coffee, for name: String) {
+        pickUpTable.append(coffee.rawValue)
+        if let coffeeOnPickUpTable = pickUpTable.last {
+            print("\(name)님이 주문하신 \(coffeeOnPickUpTable)(이/가) 준비되었습니다.")
+        }
+    }
+}
+
+class Person {
+    var name: String
+    var age: Int
+    var money: Int
+    let menu: Dictionary<Coffee, Int> = [.americano: 3000, .caffeLatte: 5000, .greenTea: 4000]
+    
+    init(name: String, age: Int, money: Int) {
+        self.name = name
+        self.age = age
+        self.money = money
+    }
+
+    func order(_ coffee: Coffee, of coffeeShop: CoffeeShop) {
+        if let coffeePrice = menu[coffee] {
+            if money - coffeePrice < 0 {
+                print("잔액이 \(coffeePrice - money)원만큼 모자랍니다")
+            } else {
+                coffeeShop.make(coffee, for: name)
+                money = money - coffeePrice
+                coffeeShop.sales = coffeeShop.sales + coffeePrice
+                print("\(name)의 잔액: \(money)원, 커피숍의 잔액: \(coffeeShop.sales)원")
+                print()
+            }
+        }
+    }
+}
+
+let misterLee = Person(name: "jin", age: 13, money: 10000)
+let starbucks = CoffeeShop(sales: 50000)
+
+misterLee.order(.americano, of: starbucks)
+misterLee.order(.caffeLatte, of: starbucks)
+misterLee.order(.greenTea, of: starbucks)
