ee06d63bb406d2f30da25d3c789140b581d0accb
ny <nayeon6868@gmail.com>
2023-03-22 20:44:38 +0900

    swift-starter-week3

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,11 +8,24 @@
 
 import Foundation
 
-enum Coffee: String {
-    case americano = "아메리카노"
-    case latte = "라떼"
-    case mocha = "카페모카"
-    case espresso = "에스프레소"
+enum Coffee: CustomStringConvertible {
+    case americano
+    case latte
+    case mocha
+    case espresso
+    
+    var description: String {
+        switch self {
+        case .americano:
+            return "아메리카노"
+        case .latte:
+            return "라떼"
+        case .mocha:
+            return "카페모카"
+        case .espresso:
+            return "에스프레소"
+        }
+    }
 }
 
 class Person {
@@ -24,8 +37,10 @@ class Person {
         self.money = money
     }
     
-    func order(_ coffee: Coffee, of coffeeShop: CoffeeShop, by name: String) {
-        let coffeePrice = coffeeShop.menu[coffee.rawValue] ?? 0
+    func order(_ coffee: Coffee, of coffeeShop: CoffeeShop) {
+        guard let coffeePrice = coffeeShop.menu[coffee] else {
+            return
+        }
         if money < coffeePrice {
             print("잔액이 \(coffeePrice - money)원만큼 부족합니다.")
         } else {
@@ -38,28 +53,31 @@ class Person {
 class CoffeeShop {
     var shopName: String
     var sales: Int = 0
-    var barista: String
-    var menu: [Coffee.RawValue: Int] = ["아메리카노": 2000, "라떼": 2500, "카페모카": 3000, "에스프레소": 1500]
-    var pickUpTable: [Coffee] = [Coffee]()
+    var barista: Person
+    var menu: [Coffee: Int] = [.americano: 2000, .latte: 2500, .mocha: 3000, .espresso: 1500]
+    var pickUpTable: [Coffee] = []
     
-    init(shopName: String, barista: String) {
+    init(shopName: String, barista: Person) {
         self.shopName = shopName
         self.barista = barista
     }
     
     func make(_ coffee: Coffee, for name: String) {
-        let coffeePrice = menu[coffee.rawValue] ?? 0
+        guard let coffeePrice = menu[coffee] else {
+            return
+        }
         sales += coffeePrice
         pickUpTable.append(coffee)
-        print("\(name)님이 주문하신 \(coffee.rawValue)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
+        print("\(name)님이 주문하신 \(coffee)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
     }
 }
 
 let missKim: Person = Person(name: "김민지", money: 3000)
-let yagombucks: CoffeeShop = CoffeeShop(shopName: "야곰벅스", barista: "바리스타")
+let baristakim: Person = Person(name: "바리스타", money: 0)
+let yagombucks: CoffeeShop = CoffeeShop(shopName: "야곰벅스", barista: baristakim)
 
 print(yagombucks.menu)
 print("\(missKim.name)님의 현재 잔액은 \(missKim.money)원 입니다.")
-missKim.order(.americano, of: yagombucks, by: missKim.name)
+missKim.order(.americano, of: yagombucks)
 print("\(missKim.name)님의 현재 잔액은 \(missKim.money)원 입니다.")
 print("\(yagombucks.shopName)의 현재 총 매출액은 \(yagombucks.sales)원 입니다.")
