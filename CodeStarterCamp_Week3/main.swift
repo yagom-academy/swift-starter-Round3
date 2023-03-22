0120bc1590c4cb79551b17f2e4a8e03fde5bc428
제이드 <a_jb97@naver.com>
2023-03-22 16:09:27 +0900

    Step 2 1차 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,21 +8,21 @@
 
 import Foundation
 
-class Person {
+struct Person {
     var money: Int
     
     init(money: Int) {
         self.money = money
     }
     
-    func order(coffee: Coffee, of coffeeShop: CoffeeShop, by name: String) {
-        if let buyCoffee = coffeeShop.menu[coffee] {
-            if money < buyCoffee {
-                print("잔액이 \(buyCoffee - money)원만큼 부족합니다.")
+    mutating func order(coffee: Coffee, of coffeeShop: CoffeeShop, by name: String) {
+        if let coffeePrice = coffeeShop.menu[coffee] {
+            if money < coffeePrice {
+                print("잔액이 \(coffeePrice - money)원만큼 부족합니다.")
             }
             else {
                 coffeeShop.make(coffee: coffee, for: name)
-                money -= buyCoffee
+                money -= coffeePrice
             }
         }
     }
@@ -41,12 +41,11 @@ class CoffeeShop {
     }
     
     func make(coffee: Coffee, for name: String) {
-        if let earnMoney = menu[coffee] {
-            sales += earnMoney
+        if let coffeePrice = menu[coffee] {
+            sales += coffeePrice
+            pickUpTable.append(coffee)
+            print("\(name) 님이 주문하신 \(coffee.rawValue)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
         }
-        
-        pickUpTable.append(coffee)
-        print("\(name) 님이 주문하신 \(coffee.rawValue)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
     }
 }
 
@@ -59,7 +58,7 @@ enum Coffee: String {
     case einspanner = "아인슈페너"
 }
 
-let missKim: Person = Person(money: 2000)
+var missKim: Person = Person(money: 2000)
 var yagombucks: CoffeeShop = CoffeeShop(sales: 0, menu: [.espresso : 2000, .americano : 3000, .cafeLatte : 4000, .vanillaLatte : 4500, .cappuccino : 4000], pickUpTable: [])
 missKim.order(coffee: .americano, of: yagombucks, by: "missKim")
 missKim.money += 20000
