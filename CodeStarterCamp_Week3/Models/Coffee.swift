f6fa96ef8ceda96721a0e05d54711fdc81c64381
JIWON1923 <zest1923@gmail.com>
2023-06-04 20:51:05 +0900

    week3

---
+++ CodeStarterCamp_Week3/Models/Coffee.swift
@@ -0,0 +1,8 @@
+//
+//  Coffee.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 이지원 on 2023/06/03.
+//
+
+import Foundation
---
+++ CodeStarterCamp_Week3/Models/Person.swift
@@ -0,0 +1,8 @@
+//
+//  Person.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 이지원 on 2023/06/03.
+//
+
+import Foundation
