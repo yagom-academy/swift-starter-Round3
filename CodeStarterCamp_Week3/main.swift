efdb4c54485a6a5877bb5a46a699bfba2520a49f
Kim, Yerim <yerim343@naver.com>
2023-06-07 00:28:48 +0900

    :hammer: 접근지정자 재설정 및 order 함수 수정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,19 +8,11 @@
 
 import Foundation
 
-
-func order(_ coffee: Coffee, of coffeeShop: inout CoffeeShop, by name: String, money: Int) {
-    var customer = Person(name: name, money: money)
-    coffeeShop.orderCoffee(from: &customer, menu: coffee, barista: coffeeShop.barista)
-}
-
-
-var missKim = Person(name: "missKim", money: 3000)
-var yagombucks = CoffeeShop(sales: 0, menu: ["아메리카노": 5000,
-                                             "카페라떼": 5500,
-                                             "에스프레소": 4500],
-                            pickUpTable: [], barista: "")
+var missKim = Person(name: "missKim", money: 6000)
+var yagombucks = CoffeeShop(menu: [Coffee.americano: Coffee.americano.price,
+                                             Coffee.caffeLatte: Coffee.caffeLatte.price,
+                                   Coffee.espresso: Coffee.espresso.price], barista: "")
 
 
-order(.americano, of: &yagombucks, by: missKim.name, money: missKim.money)
 
+missKim.order(.americano, of: &yagombucks)
