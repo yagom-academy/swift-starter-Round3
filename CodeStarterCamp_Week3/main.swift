1510b48db48c086430b9f97d4e3c60826c8c9d1f
mirue <leeminyeol@Lees-MacBook-Pro.local>
2023-06-07 12:22:21 +0900

    STEP1

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,58 @@
 
 import Foundation
 
-print("Hello, World!")
+class Person {
+    var name : String
+    var money : Int
+    
+    func buyCoffee (coffee: Coffee) {
+                print("\(coffee.rawValue) 구매할게요")
+    }
+    
+    func payMoney(coffee: Coffee, coffeeshop: CoffeeShop) {
+        if let coffeePrice = coffeeshop.menuboard[coffee.rawValue] {
+            if money < coffeePrice {
+                print("돈이 부족합니다.")
+            }
+            return print("결제 완료됐습니다.")
+        }
+    }
+    
+    init(name: String, money: Int) {
+        self.name = name
+        self.money = money
+    }
+}
+
+var misterLee : Person = Person(name: "misterLee", money: 5000)
+var missKim : Person = Person(name: "missKim", money: 6000)
+
+missKim.buyCoffee(coffee: .amricano)
+
+
+class CoffeeShop {
+    var brista : Person
+    var profit : Int
+    var menuboard : [String : Int] = ["americano" : 2000, "latte" : 4000]
+    var pickUpTable : [String] = []
+    
+    func orderCoffee(coffee: Coffee) {
+            print("주문한 \(coffee.rawValue)가 나왔습니다.")
+    }
+        
+        init(brista: Person, profit: Int, pickUpTable: [String]) {
+            self.brista = brista
+            self.profit = profit
+            self.pickUpTable = pickUpTable
+    }
+}
+
+var yagombucks : CoffeeShop = CoffeeShop(brista: misterLee, profit: 0, pickUpTable: [])
+
+yagombucks.orderCoffee(coffee: .amricano)
+
+enum Coffee : String {
+    case amricano = "아메리카노"
+    case latte = "라떼"
+}
 
