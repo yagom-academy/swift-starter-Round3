9f709a2615895425eec3f71a11e8000b3857f8bf
ltw <witlim1225@gmail.com>
2023-06-09 23:59:19 +0900

    Fix ( main ) : 객체를 생성하고, 주문하는 프로세스만 놔두도록 수정.

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -7,3 +7,24 @@
 //
 
 import Foundation
+
+let misterLee = Person(name: "misterLee", money: 10_000)
+let missKim = Person(name: "missKim", money: 1_000)
+let mrKim = Person(name: "msKim", money: 100_000)
+
+let menu: [Coffee: Int] = [
+    .Americano: 1500,
+    .Latte: 3000,
+    .CafeMoca: 4500
+]
+
+let yagomBucks = CoffeeShop(name: "yagomBucks", menu: menu, barista: misterLee)
+
+yagomBucks.showMenu()
+print("")
+
+missKim.buy(coffee: [.Americano, .CafeMoca], to: yagomBucks)
+print("")
+
+mrKim.buy(coffee: [.Americano, .CafeMoca], to: yagomBucks)
+
