6b35a6c9b6237941f5e3e83c4835f3b5329779dd
norang0 <kyuheehong0@gmail.com>
2023-06-10 00:43:14 +0900

    Week3 Step1

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -6,7 +6,38 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
+//import Foundation
 
-print("Hello, World!")
+struct Person {
+    var money: Int = 0
+    
+    func buyCoffee() {
+        print("커피를 구매합니다.")
+    }
+}
 
+struct CoffeeShop {
+    var revenue: Double = 0
+    var menu: Dictionary<Coffee,Double> = [:]
+    var pickUpTable: Array<Coffee> = []
+    var baristar: Person = Person()
+
+    func takeOrder() {
+        print("주문을 받습니다.")
+    }
+    
+    mutating func makeCoffee(orderedCoffee: Coffee) {
+        print("커피를 만듭니다.")
+        pickUpTable.append(orderedCoffee)
+    }
+        
+}
+
+enum Coffee {
+    case Espresso, Americano, Latte
+}
+
+var misterLee: Person = Person()
+var missKim: Person = Person()
+
+var yagombucks: CoffeeShop = CoffeeShop(baristar: misterLee)
