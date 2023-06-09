a9ac6e9c870e0cf201351656d794b059f9a70f85
ltw <witlim1225@gmail.com>
2023-06-09 23:57:45 +0900

    Fix ( CoffeeShop) : Menu를 private(set) 으로 두어 외부에서 접근할 수 있도록 처리. barista 할당 추가. 주문시 생성한 객체/타입을 사용할 수 있도록 수정.

--- CodeStarterCamp_Week3/CoffeeShop.swift
+++ CodeStarterCamp_Week3/CoffeeShop.swift
@@ -10,25 +10,46 @@ import Foundation
 class CoffeeShop {
     private var name: String
     private var sales: Int
-    private var menu: [Coffee: Int]
+    private(set) var menu: [Coffee: Int]
+    private var barista: [Person]
     
-    init(name: String, sales: Int, menu: [Coffee : Int]) {
+    init(name: String, sales: Int = 0, menu: [Coffee : Int], barista: Person...) {
         self.name = name
         self.sales = sales
         self.menu = menu
+        
+        self.barista = [Person]()
+        for person in barista {
+            self.barista.append(person)
+        }
     }
     
     func showMenu() {
         print("- - - - Menu - - - -")
-        for (coffee, price) in menu.enumerated() {
-            print("\(coffee) : \(price)원")
+        for (coffee, price) in menu {
+            print("\(coffee.name) : \(price)원")
         }
         print("- - - - - - - - - - -")
     }
     
-    func order(by person: String, for menu: [Coffee]) {
-        let orderedMenu = make(menu)
+    func order(by person: Person, for menu: [Coffee]) {
+        let sales = calculateMoney(menu)
+        getMoney(sales)
         
+        let orderedMenu = make(menu)
+        pickUpTable(to: person, orderedMenu)
+    }
+    
+    func calculateMoney(_ order: [Coffee]) -> Int {
+        var pay = 0
+        for item in order {
+            pay += menu[item] ?? 0
+        }
+        return pay
+    }
+    
+    func getMoney(_ money: Int) {
+        self.sales += money
     }
     
     func make(_ menu: [Coffee]) -> [Coffee] {
@@ -36,26 +57,25 @@ class CoffeeShop {
         
         for order in menu {
             madeMenu.append(order)
+            print("\(order.name) 제품을 만드는 중입니다.")
             sleep(1)
         }
         
         return madeMenu
     }
     
-    func pickUpTable(for order: String, _ menu: [Coffee: Int]) -> [Coffee] {
+    func pickUpTable(to person: Person, _ menu: [Coffee]) {
         var menuString = ""
-        var madeMenu = [Coffee]()
         
-        for (order, each) in menu {
-            menuString += "\(order.menuName) \(each)잔"
-            menuString += ","
+        for item in menu {
+            menuString += "\(item.name), "
         }
-        menuString.removeLast() // 마지막 , 를 지우기 위함.
+        menuString.removeLast(2) // 마지막 , 를 지우기 위함.
         
-        let sendingMessage = "\(order) 고객님. 주문하신 \(menuString) 나왔습니다."
+        let sendingMessage = "\(person.name) 고객님. 주문하신 \(menuString) 나왔습니다."
         print(sendingMessage)
         
-        return madeMenu
+        person.take(menu)
     }
     
     
