69a31e246b2d9658df9be7c358e67377bc63a9a4
mirue <leeminyeol@Lees-MacBook-Pro.local>
2023-06-12 20:25:25 +0900

    STEP1-1

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,26 +8,21 @@
 
 import Foundation
 
-class Person {
+struct Person {
     var name : String
     var money : Int
     
-    func buyCoffee (coffee: Coffee) {
-                print("\(coffee.rawValue) 구매할게요")
+    mutating func buyCoffee (coffee: Coffee) {
+        print("\(coffee.rawValue) 구매할게요")
     }
     
-    func payMoney(coffee: Coffee, coffeeshop: CoffeeShop) {
+    mutating  func payMoney(coffee: Coffee, coffeeshop: CoffeeShop) {
         if let coffeePrice = coffeeshop.menuboard[coffee.rawValue] {
             if money < coffeePrice {
                 print("돈이 부족합니다.")
             }
             return print("결제 완료됐습니다.")
         }
-    }
-    
-    init(name: String, money: Int) {
-        self.name = name
-        self.money = money
     }
 }
 
@@ -37,20 +32,14 @@ var missKim : Person = Person(name: "missKim", money: 6000)
 missKim.buyCoffee(coffee: .amricano)
 
 
-class CoffeeShop {
+struct CoffeeShop {
     var brista : Person
     var profit : Int
     var menuboard : [String : Int] = ["americano" : 2000, "latte" : 4000]
     var pickUpTable : [String] = []
     
-    func orderCoffee(coffee: Coffee) {
-            print("주문한 \(coffee.rawValue)가 나왔습니다.")
-    }
-        
-        init(brista: Person, profit: Int, pickUpTable: [String]) {
-            self.brista = brista
-            self.profit = profit
-            self.pickUpTable = pickUpTable
+    mutating  func orderCoffee(coffee: Coffee) {
+        print("주문한 \(coffee.rawValue)가 나왔습니다.")
     }
 }
 
