1efdc445683f3d5f4cef6cbb4cdf1625c71e3863
Ipjak <sciencerhgkr@gmail.com>
2023-06-16 01:43:14 +0900

    first

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,43 @@
 
 import Foundation
 
-print("Hello, World!")
+class Person {
+    var money: Int
+    func buyCoffee (){
+        var buyingCoffee: String
+    }
+    
+    init(money: Int) {
+        self.money = money
+    }
+}
 
+
+class CoffeeShop {
+    var revenue: Int
+    var menu: [String: Int]
+    var pickUpTable: [Int]
+    func orderAndMakeCoffee() {
+
+    }
+    func onPickUpTable() {
+        
+    }
+    
+    init(revenue: Int, menu: [String: Int], pickUpTable: [Int]) {
+        self.revenue = revenue
+        self.menu = menu
+        self.pickUpTable = pickUpTable
+    }
+}
+
+enum Coffee {
+    case superCoffe
+    case megaCoffe
+    case umCoffee
+    case realCoffee
+}
+
+var misterLee = Person(money: 1000)
+var missKim = Person(money: 2000)
+var yagombucks = CoffeeShop(revenue: 10000, menu: [SuperCoffe: 1000], pickUpTable: 1, barista: misterLee)
