860226d6ac875248e2af18fb86116ef1301be641
Hisop <hisop@Hisopui-MacBookPro.local>
2023-08-01 23:13:26 +0900

    Feat: Step1 요구사항 구현 class와 struct중 고민. 상속을 사용하지 않아서 Struct 선택 구매와 주문받기 커피만들기 메서드 생성 정확한 지시가 없는 항목은 임의대로 설정

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,58 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Person {
+    let name: String
+    var money = 0
+    
+    mutating func buyCoffee(_ coffee: Coffee, from coffeeShop: inout CoffeeShop) {
+        if coffeeShop.takeOrder(coffee, from: self) {
+            self.money = self.money - coffee.rawValue
+        }
+    }
+}
 
+enum Coffee:Int {
+    case americano = 4000
+    case latte = 4500
+    case cappuccino = 5000
+    case espresso = 3500
+}
+
+struct CoffeeShop {
+    var sales = 0
+    var barista: Person?
+    var menu: [Coffee] = []
+    var pickUpTable: [Coffee] = []
+    
+    mutating func takeOrder(_ coffee: Coffee, from: Person) -> Bool {
+        guard menu.contains(coffee) else {
+            print("해당 메뉴는 존재하지 않습니다.")
+            return false
+        }
+        if from.money < coffee.rawValue {
+            print("잔액이 \(from.money - coffee.rawValue)모자랍니다.")
+            return false
+        }
+        sales = sales + coffee.rawValue
+        self.makeCoffee(coffee, from: from.name)
+        return true
+    }
+    
+    mutating func makeCoffee(_ coffee: Coffee, from name: String) {
+        pickUpTable.append(coffee)
+        print("\(name) 님이 주문하신 \(coffee)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
+    }
+    
+    init() {
+        menu.append(Coffee.americano)
+        menu.append(Coffee.latte)
+        menu.append(Coffee.cappuccino)
+    }
+}
+
+var misterLee = Person(name: "misterLee")
+var missKim = Person(name: "missKim")
+
+var yagombucks = CoffeeShop()
+yagombucks.barista = misterLee
