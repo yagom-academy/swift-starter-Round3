ea53a579b1cb5da322e06a6d4313668fcc728fec
yawoong2 <adamzoong@naver.com>
2023-08-08 02:40:11 +0900

    Feat: 커피숍 타입과 인스턴스 생성

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,37 @@
 
 import Foundation
 
-print("Hello, World!")
+class Person {
+    var money: Int = 0
+    
+    func buyCoffee() {
+        
+    }
+}
+
+class CoffeeShop {
+    var sales: Int = 0
+    var menu: [String: Int] = [:]
+    var pickUpTable: [String] = []
+    var barista: Person?
+    
+    enum Coffee: String {
+        case espresso, americano, cafeLatte, vanilaLatte, cafeMoca
+    }
+    
+    func getOrder(orderedCoffee: String) {
+        print("\(orderedCoffee)를 주문 받았다.")
+        let brewedCoffee: String = brewCoffee(to: orderedCoffee)
+        pickUpTable.append(brewedCoffee)
+    }
+    
+    func brewCoffee(to orderedCoffee: String) -> String {
+        let brewedCoffee: String = "완성된 \(orderedCoffee)"
+        return brewedCoffee
+    }
+}
+
+var misterLee: Person = Person()
+var yagombucks: CoffeeShop = CoffeeShop()
+yagombucks.barista = misterLee
 
