5dc702d2556194aa729ec729e5ed722a2a4e4797
LeeHyunsoo <mm083434@naver.com>
2023-08-09 17:49:10 +0900

    🎉first commit Week3 Step1

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -1,12 +1,34 @@
-//
-//  main.swift
-//  CodeStarterCamp_Week3
-//
-//  Created by yagom.
-//  Copyright © yagom academy. All rights reserved.
-//
-
 import Foundation
 
-print("Hello, World!")
+struct Person {
+    var money : Int = 10000
+    func buyCoffee(coffee : Coffee) {
+        print("\(coffee)를 구매하였습니다.")
+    }
+}
+struct CoffeeShop {
+    var salesRevenue : Int = 0
+    var menu: [Coffee: Int] = [:]
+    var pickUpTable: Array<Coffee> = []
+    var barista: Person
+    func takeOrder() {
+
+    }
+    mutating func makeCoffee(coffee : Coffee) {
+        print("\(coffee) 만들기 완료")
+        pickUpTable.append(coffee)
+    }
+
+}
+enum Coffee {
+    case americano, latte, vanillaLatte, espresso
+}
+
+var misterLee = Person()
+var missKim = Person()
+var yagombucks = CoffeeShop(barista: misterLee)
+
+yagombucks.makeCoffee(coffee: .americano)
+print(yagombucks.pickUpTable)
+
 
