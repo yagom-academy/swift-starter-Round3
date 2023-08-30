cc3224a4f5231551bd737280c68dd1534ea6efb1
chaloopy <sorrowsi93@naver.com>
2023-08-30 18:07:06 +0900

    Step 1 commit

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,34 @@
 
 import Foundation
 
-print("Hello, World!")
+class Person {
+    var money: Int = 0
+    
+    func buyCoffee() {
+        print("커피를 구매 하였습니다.")
+    }
+}
 
+class CoffeeShop {
+    var sales: Int = 0
+    var menu: [String: Int] = [:]
+    var pickUpTable: [Coffee] = []
+    var barista: Person?
+    
+    func takeOrder(order: Coffee) {
+        makeCoffee(coffee: order)
+    }
+    
+    func makeCoffee(coffee: Coffee) {
+        pickUpTable.append(coffee)
+    }
+}
+
+enum Coffee {
+    case americano, espresso, latte, mocha, greenTea, iceTea
+}
+
+let misterLee: Person = Person()
+let missKim: Person = Person()
+let yagombucks: CoffeeShop = CoffeeShop()
+yagombucks.barista = misterLee
