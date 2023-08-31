e8bdaca4c659260a7ffe37b3f52910f3c8fb9685
chaloopy <sorrowsi93@naver.com>
2023-08-31 15:19:41 +0900

    code refector & rename methods

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -9,33 +9,49 @@
 import Foundation
 
 class Person {
-    var money: Int = 0
+    var money: Int
     
     func buyCoffee() {
-        print("커피를 구매 하였습니다.")
+        print("커피를 구매하였습니다.")
+    }
+    
+    init(money: Int) {
+        self.money = money
     }
 }
 
 class CoffeeShop {
-    var sales: Int = 0
-    var menu: [String: Int] = [:]
-    var pickUpTable: [Coffee] = []
-    var barista: Person?
+    var sales: Int
+    var menu: [Coffee: Int]
+    var pickUpTable: [Coffee]
+    var barista: Person
     
-    func takeOrder(order: Coffee) {
-        makeCoffee(coffee: order)
+    func order(_ coffee: Coffee) {
+        make(coffee)
     }
     
-    func makeCoffee(coffee: Coffee) {
+    func make(_ coffee: Coffee) {
         pickUpTable.append(coffee)
+    }
+    
+    init(sales: Int, menu: [Coffee : Int], pickUpTable: [Coffee], barista: Person) {
+        self.sales = sales
+        self.menu = menu
+        self.pickUpTable = pickUpTable
+        self.barista = barista
     }
 }
 
 enum Coffee {
-    case americano, espresso, latte, mocha, greenTea, iceTea
+    case americano, cappuccino, espresso, latte, mocha, greenTea, iceTea
 }
 
-let misterLee: Person = Person()
-let missKim: Person = Person()
-let yagombucks: CoffeeShop = CoffeeShop()
-yagombucks.barista = misterLee
+let misterLee: Person = Person(money: 0)
+let missKim: Person = Person(money: 12000)
+let yagombucksMenu: [Coffee: Int] = [.americano: 4500, .cappuccino: 5500, .espresso: 4000, .mocha: 5500, .latte: 5000, .greenTea: 6500, .iceTea: 5000]
+let yagombucks: CoffeeShop = CoffeeShop(sales: 0,
+                                        menu: yagombucksMenu,
+                                        pickUpTable: [],
+                                        barista: misterLee)
+
+
