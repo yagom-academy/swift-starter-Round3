bfcef90fa1e0b4dc8d1e54a489a3e8798df24134
sueunal <doms@kakao.com>
2023-09-07 18:13:19 +0900

    Week 3 [STEP 2] 수하
    @gzzjk159
    ---
    
    ## 고민했던 점
    - 사람이 커피숍에서 커피를 주문해야하는데 제 코드는 사람이 주문하는 것 처럼 보이지 않습니다..
    ## 해결이 되지 않은 점
    - 없는 것 같습니다
    ## 조언을 얻고 싶은 부분
    - 인스턴스가 커피숍에서 주문을 하는 것 처럼 하려면 어떻게 해야하는지 궁굼합니다
    
    감사합니다.

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,67 @@
 
 import Foundation
 
-print("Hello, World!")
+enum Coffe{
+    case asspresso, americano, latte
+    var name : String{
+        switch self{
+        case .asspresso:
+            return "asspresso"
+        case .americano:
+            return "americano"
+        case .latte:
+            return "latte"
+        }
+    }
+    var values : Int{
+        switch self{
+        case .americano:
+            return 4000
+        case .latte:
+            return 5000
+        case .asspresso:
+            return 3500
+        }
+    }
+}
+struct Person{
+    var name : String
+    var money : Int = 10000
+    mutating func order(drink : Coffe){
+        if money < drink.values{
+            print("잔액이 부족합니다")
+        }else{
+            money -= drink.values
+            print("\(drink.name)를 주문합니다")
+            print("\(money)원 남았습니다.")
+        }
+    }
+}
+struct CoffeShop{
+    var barista : Person
+    var sales : Int = 0
+    var menuBoard : Coffe = .americano
+    var pickUpTable : [Coffe] = []
+    
+    mutating func salesCoffe(){
+        print("\(self.sales)원 벌었습니다.")
+    }
+    mutating func takeOrder(menu : Coffe){
+        print("\(menu)의 가격은 \(menu.values)원 입니다.")
+        sales += menu.values
+        makeCoffe(menu: menu.name)
+        pickUpTable.append(menu)
+    }
+    func makeCoffe(menu : String){
+        print("\(menu)를 만듭니다.")
+    }
+    func showPickUpTable(){
+        for  drink in pickUpTable{
+            print("\(drink)가 완성되었습니다.")
+        }
+    }
+}
+var minsterLee = Person(name: "minsterLee")
+var missKim = Person(name: "missKim")
 
+var yagombucks = CoffeShop(barista: minsterLee)
