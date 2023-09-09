8a1760486b34680da294b38afeee21a5d5abb622
jjwon2149 <45623603+jjwon2149@users.noreply.github.com>
2023-09-09 16:09:25 +0900

    AddPropertyObserver

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -31,7 +31,7 @@ enum Coffee {
 
 //MARK: - Person 클래스 정의
 class Person {
-    var name: String?
+    var name: String
     var age: Int
     var money: Int
     
@@ -42,10 +42,10 @@ class Person {
     }
     
     func order(_ coffee: Coffee, of coffeeShop: CoffeeShop, by name: String) {
-        if self.money >= coffee.price() {
+        if money >= coffee.price() {
             coffeeShop.make(coffee, from: name)
             money -= coffee.price()
-        }else {
+        } else {
             print("\(name)의 잔액이 \(coffee.price() - money)원만큼 부족합니다.")
         }
     }
@@ -56,25 +56,37 @@ class CoffeeShop {
     var revenue: Int
     var menu: [Coffee]
     var barista: Person?
-    var pickUpTable: [Coffee]
+    var orderCoffee: Coffee
+    var orderName: String
+    var pickUpTable: [Coffee] {
+        willSet {
+            print("\(orderName) 님이 주문하신 \(orderCoffee)(이/가) 준비중입니다.")
+        }
+        didSet {
+            print("\(orderName) 님이 주문하신 \(orderCoffee)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
+        }
+    }
     
-    init(revenue: Int = 0, menu: [Coffee] = [.americano, .cappuccino, .latte, .chocofrappuccino], pickUpTable: [Coffee] = []) {
+    init(revenue: Int = 0, menu: [Coffee] = [.americano, .cappuccino, .latte, .chocofrappuccino], pickUpTable: [Coffee] = [], orderCoffee: Coffee = .americano, orderName: String = "") {
         self.revenue = revenue
         self.menu = menu
         self.pickUpTable = pickUpTable
+        self.orderCoffee = orderCoffee
+        self.orderName = orderName
     }
     
     func make(_ coffee: Coffee, from name: String) {
         revenue += coffee.price()
+        orderCoffee = coffee
+        orderName = name
         pickUpTable.append(coffee)
-        print("\(name) 님이 주문하신 \(coffee)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
     }
 }
 
 
 //Person 타입의 인스턴스로 misterLee, missKim, coda를 생성.
 let misterLee = Person(name: "misterLee", age: 23, money: 10000)
-let missKim = Person(name: "missKim", age: 33, money: 500)
+let missKim = Person(name: "missKim", age: 33, money: 5000)
 let coda = Person(name: "Coda", age: 50, money: 50000)
 
 //CoffeeShop 타입의 인스턴스로 yagombucks을 생성.
@@ -84,26 +96,19 @@ let yagombucks = CoffeeShop()
 yagombucks.barista = misterLee
 
 //missKim이 카페라떼 주문.
-missKim.order(.latte, of: yagombucks, by: missKim.name ?? "missKim")
+missKim.order(.latte, of: yagombucks, by: missKim.name)
 
 //coda가 초코프라푸치노 주문.
-coda.order(.chocofrappuccino, of: yagombucks, by: coda.name ?? "coda")
+coda.order(.chocofrappuccino, of: yagombucks, by: coda.name)
 
 //매출액 확인.
 print("yagombucks의 매출액은 \(yagombucks.revenue)원 입니다.")
 
 //pickUpTable 확인.
-print("yagombucks의 pickUpTable은 \(yagombucks.pickUpTable.map { String(describing: $0) }.joined(separator: ", "))입니다.")
+let pickUpTableMessage = yagombucks.pickUpTable.isEmpty ? "주문이 없습니다." : "yagombucks의 pickUpTable은 \(yagombucks.pickUpTable.map { String(describing: $0) }.joined(separator: ", "))입니다."
+print(pickUpTableMessage)
+
 
 //커피주문후 가진 돈 확인
-if let name = missKim.name {
-    print("\(name)의 돈은 \(missKim.money)입니다.")
-} else {
-    print("사용자 이름이 없습니다.")
-}
-
-if let name = coda.name {
-    print("\(name)의 돈은 \(coda.money)입니다.")
-} else {
-    print("사용자 이름이 없습니다.")
-}
+print("\(missKim.name)의 돈은 \(missKim.money)입니다.")
+print("\(coda.name)의 돈은 \(coda.money)입니다.")
