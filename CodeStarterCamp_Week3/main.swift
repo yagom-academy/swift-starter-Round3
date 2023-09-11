9bf7b1b3300de5e4c52fd757207aaf18ba6fcd33
aldalddl <aldalddl2007@gmail.com>
2023-09-11 17:25:55 +0900

    feat: Person 클래스에 메서드 추가

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -40,12 +40,29 @@ class Person {
     var age: Int
     var money: Int
     var nickname: String!
-    
+    var order: Coffee!
+
     init(name: String, gender: Gender, age: Int, money: Int) {
         self.name = name
         self.gender = gender
         self.age = age
         self.money = money
+    }
+    
+    func buyCoffee(to: CoffeeShop, order: Coffee, nickname: String) {
+        print("카페 \(to.name)에 \(nickname) 고객님이 \(order)(을)를 주문했습니다.\n")
+        
+        self.money -= order.price
+        self.order = order
+        self.nickname = nickname        
+    }
+    
+    func eatCoffee(coffee: Coffee) {
+        if coffee == self.order {
+            print("주문한 \(coffee)(을)를 마셨습니다.\n")
+        } else {
+            print("\(coffee)(을)를 주문하지 않았습니다. 주문 먼저 해주세요.\n")
+        }
     }
 }
 
