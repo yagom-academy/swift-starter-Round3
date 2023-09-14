854a0b5157625058d723b7605a895c2a9c510ccb
SAY-Hong <bobseahee@gmail.com>
2023-09-14 17:52:19 +0900

    step2 ver3

--- CodeStarterCamp_Week3/step2.swift
+++ CodeStarterCamp_Week3/step2.swift
@@ -14,16 +14,11 @@ enum Coffee: String {
     case banillaLatte = "바닐라라떼"
 }
 
-class Person {
+struct Person {
     var money: Int
     var name: String
     
-    init(money: Int, name: String) {
-        self.money = money
-        self.name = name
-    }
-
-    func order(_ coffee: Coffee, of coffeeShop: CoffeeShop) {
+    mutating func order(_ coffee: Coffee, of coffeeShop: CoffeeShop) {
         if let price = menu[coffee] {
             if money < price {
                 print("잔액이 \(price - money)원 만큼 부족합니다. ")
@@ -39,6 +34,12 @@ class CoffeeShop {
     var sales: Int
     var barista: Person
     var menu: [Coffee: Int]
+    var pickUpTable: [(menu: Coffee, name: String)] = [] {
+        didSet {
+            let order = pickUpTable.removeFirst()
+            print("\(order.name)님이 주문하신 \(order.menu.rawValue)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
+        }
+    }
     
     init(sales: Int, barista: Person, menu: [Coffee: Int]) {
         self.sales = sales
@@ -50,19 +51,9 @@ class CoffeeShop {
         if let price = menu[coffee] {
             sales += price
         }
-        
-        var pickUpTable: [Coffee] = [] {
-            didSet {
-                if let coffeeName = pickUpTable.last {
-                    print("\(name)님이 주문하신 \(coffeeName.rawValue)(이/가) 준비되었습니다. 픽업대에서 가져가주세요.")
-                }
-            }
-        }
-        pickUpTable.append(coffee)
-        
+        pickUpTable.append(_: (menu: coffee, name: name))
     }
-    
 }
-
+    
 var menu: [Coffee: Int] = [.americano: 1000, .cafeLatte: 3000, .cappuccino: 1500]
 
