094e946413c3387253bc4d28d5f3d5ae9af693a8
jjudy_kim <jjudykim0@gmail.com>
2023-09-15 00:23:52 +0900

    STEP 2 - 인스턴스는 서로 소통한다

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -9,9 +9,10 @@
 import Foundation
 
 var misterLee: Person = Person(name: "이철수", age: 30, gender: Person.Gender.male, money: 100000)
-var missKim: Person = Person(name: "김영희", age: 18, gender: Person.Gender.female, money: 30000)
+var missKim: Person = Person(name: "김영희", age: 18, gender: Person.Gender.female, money: 13000)
 var yagombucks: CoffeeShop = CoffeeShop(brandName: "yagombucks", branchName: "코엑스별마당점")
 
 yagombucks.hireNewBarista(newBarista: misterLee)
 yagombucks.takeOrder()
-yagombucks.makeCoffee(orderedMenu: Coffee.frappuccino)
+missKim.orderCoffee(Coffee.flatWhite, of: yagombucks, by: missKim.nickName)
+yagombucks.checkSalesRevenue()
--- CodeStarterCamp_Week3/week3_step1.swift
+++ CodeStarterCamp_Week3/week3_step1.swift
@@ -8,11 +8,11 @@
 import Foundation
 
 enum Coffee: String {
-    case americano = "americano"
-    case latte = "latte"
-    case macchiato = "macchiato"
-    case flatWhite = "flatWhite"
-    case frappuccino = "frappucino"
+    case americano = "아메리카노"
+    case latte = "라떼"
+    case macchiato = "마끼아또"
+    case flatWhite = "플랫화이트"
+    case frappuccino = "프라푸치노"
 }
 
 class Person {
@@ -20,12 +20,13 @@ class Person {
         case male
         case female
     }
-    
+
     var name: String
     var age: Int
     var gender: Gender
     var money: Int
     var nickName: String
+    var face: String
  
     // 이니셜라이저
     init(name: String, age: Int, gender: Gender, money: Int) {
@@ -34,6 +35,7 @@ class Person {
         self.gender = gender
         self.money = money
         self.nickName = String(name.dropFirst(1))
+        if gender == Gender.male { face = "🧑🏻" } else { face = "👩🏻"}
     }
     
     func walk() {
@@ -44,8 +46,29 @@ class Person {
         print("\(nickName), 출근합니다...")
     }
     
-    func buyCoffee() {
-        print("\(nickName), 도핑을 위한 커피 구입! 🥳")
+    func orderCoffee(_ coffee: Coffee, of coffeeShop: CoffeeShop, by name: String) {
+        print("\(self.face) : 안녕하세요! \(coffee.rawValue) 하나 주세요. 닉네임은 \(name)예요!")
+        
+        let price = coffeeShop.checkPossibleGetOrder(coffee)
+        
+        if price != 0 {
+            if isPossiblePay(price: price) {
+                self.money -= price
+                coffeeShop.salesRevenue += price
+                print("===결제 완료!===")
+                coffeeShop.makeCoffee(coffee, from: name)
+            } else {
+                print("[이런! 잔액이 \(abs(self.money - price))만큼 부족합니다.\n")
+            }
+        }
+    }
+    
+    func isPossiblePay(price priceOfCoffee: Int) -> Bool{
+        if (self.money - priceOfCoffee) >= 0 {
+            return true
+        } else {
+            return false
+        }
     }
     
     func sleep() {
@@ -69,24 +92,48 @@ class CoffeeShop {
         self.baristas = [Person]()
         self.menu = [Coffee: Int]()
         self.pickUpTable = [Coffee]()
+        setMenu()
+    }
+    
+    func setMenu() {
+        menu[Coffee.americano] = 4500
+        menu[Coffee.latte] = 5000
+        menu[Coffee.flatWhite] = 5300
+        menu[Coffee.macchiato] = 5400
     }
     
     func takeOrder() {
         print("===주문을 받습니다!===")
-        print("안녕하세요! \(brandName)입니다. 주문 도와드릴까요?")
+        print("☕️ : 안녕하세요! \(brandName)입니다. 주문 도와드릴까요?")
     }
     
-    func makeCoffee(orderedMenu: Coffee) {
-        print("===음료를 제조합니다!===")
-        print("지금 제조하는 메뉴는 \(orderedMenu)입니다")
-        pickUpTable.append(orderedMenu)
-        print("제조가 모두 완료되었습니다. 픽업대를 확인해주세요 :3")
+    func checkPossibleGetOrder(_ coffee: Coffee) -> Int{
+        if let priceOfCoffee = menu[coffee] {
+            print("☕️ : 네, \(priceOfCoffee)원 입니다! 결제 부탁드려요!")
+            return priceOfCoffee
+        } else {
+            print("☕️ : 죄송합니다 고객님, 저희는 \(coffee.rawValue) 메뉴가 없답니다")
+            return 0
+        }
+    }
+    
+    func makeCoffee(_ coffee: Coffee, from name: String) {
+        print("\n===음료를 제조합니다!===")
+        print("지금 제조하는 메뉴는 \(coffee.rawValue)입니다")
+        pickUpTable.append(coffee)
+        print("☕️ : \(name)님, 주문하신 \(coffee.rawValue) 제조가 모두 완료되었습니다. 픽업대를 확인해주세요 :3\n")
     }
     
     func hireNewBarista(newBarista: Person) {
-        print("===직원을 고용합니다!===")
-        print("오늘부터 \(brandName) \(branchName)에서 같이 근무하게 될 \(newBarista.name)입니다! \n다들 환영해주세요 :3")
+        print("\n===직원을 고용합니다!===")
+        print("☕️ : 오늘부터 \(brandName) \(branchName)에서 같이 근무하게 될 \(newBarista.name)입니다! 다들 환영해주세요 :3 \n")
         baristas.append(newBarista)
     }
+    
+    func checkSalesRevenue() {
+        print("☕️ : 오늘의 매출은 \(salesRevenue)원! 모두 고생하셨습니다 \n")
+    }
+    
+    // missKim은 커피를 구매하는 order(_ coffee: Coffee, of coffeeShop: CoffeeShop, by name: String) 메서드를 실행해 yagomBucks의 make(_ coffee: Coffee, from name: String) 메서드를 호출합니다.
     
 }
