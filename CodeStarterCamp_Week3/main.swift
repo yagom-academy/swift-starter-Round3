412752dca10521a3dd2b939b906f3eb3e11ea534
lucie <146562581+lucidly-lucie@users.noreply.github.com>
2023-10-26 13:47:06 +0900

    Week3 [STEP 1] lucie (#276)
    * Person 타입 정의 추가
    
    * CoffeeShop 타입 정의 추가
    
    * Coffee 타입 정의 추가
    
    * 인스턴스 생성
    
    * CoffeeShop 내 barista 변수 추가 및 바리스타 할당
    
    * Person 타입의 money 변수 init 설정
    
    * Coffee를 enum으로 변경
    
    * BuyCoffee, menu에 Coffee 타입 사용

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,45 @@
 
 import Foundation
 
-print("Hello, World!")
+
+struct Person {
+    var money: Int
+    init(money: Int) {
+        self.money = money
+    }
+    func buyCoffee(order: [Coffee]) {
+        print("\(order)을(를) 구매합니다.")
+    }
+}
+
+struct CoffeeShop {
+    var salesVolume: Int = 0
+    let menu: [Coffee: Int] = [Coffee.아메리카노: 2500, Coffee.카페라떼: 4000]
+    var pickupTable: [String] = []
+    var barista: Person?
+    
+    mutating func receiveOrder(order: String) {
+        print("\(order) 메뉴의 주문이 들어왔습니다.")
+    }
+    mutating func makeCoffee(order: String) {
+        pickupTable = [order]
+        print("\(order) 메뉴의 제작이 완료되었습니다.")
+    }
+    
+}
+
+enum Coffee {
+    case 아메리카노
+    case 카페라떼
+    case 돌체라떼
+    case 아포가토
+    }
+
+
+var misterLee: Person = Person(money: 10000)
+var missKim: Person = Person(money: 50000)
+
+var yagombucks: CoffeeShop = CoffeeShop()
+yagombucks.barista = misterLee
+
 
