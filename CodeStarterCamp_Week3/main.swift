9c3a8d63a99d86a485661a2105804881bf125977
이대호 <idaeho@idaehoui-MacBookAir.local>
2023-11-28 10:54:16 +0900

    Round3[Step1]- Daeho

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -6,7 +6,55 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
+
+// 2023-11-28 by ss_12_daeho8078
+// Round3-Step1: 타입과 인스턴스 관계
+
 import Foundation
 
-print("Hello, World!")
+enum Coffee {
+    case Americano
+    case Latte
+    case Choco
+    case Caramel
+    case Mint
+    
+}
+
+//Person 타입을 구조체로 설정
+struct Person {
+    var money: Int = 0
+    
+    func buyCoffee(){
+        print("커피를 구매하였습니다.")
+    }
+}
+
+
+//CoffeShop 타입을 구조체로 설정
+struct CoffeeShop {
+    var barista: Person
+    var totalSales: Int = 0
+    var coffeeMenu: String = ""
+    var coffeePrice: Int = 0
+    var pickUpTable: [String] = []
+    
+    
+    mutating func orderCoffee(coffeMenu: String){
+        self.coffeeMenu = coffeMenu
+       
+    }
+    
+    mutating func makeCoffee(){
+        self.pickUpTable.append(self.coffeeMenu)
+    }
+}
+
+
+var misterLee: Person = Person()
+var missKim: Person = Person()
+
+var yagombucks: CoffeeShop = CoffeeShop(barista: misterLee)
+
+
 
