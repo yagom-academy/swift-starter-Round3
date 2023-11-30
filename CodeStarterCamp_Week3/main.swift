7d748dafab1d1cb3b831dd461d312519cb6f0e16
forseaest <u3z@naver.com>
2023-12-01 06:17:19 +0900

    [step1] 타입과 인스턴스 생성

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,60 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Person {
+    var money: Int
+    
+    init() {
+        self.money = 0
+    }
+    
+    init(money: Int) {
+        self.money = money
+    }
+    
+    func buyCoffee() {
+        print("커피를 구매합니다.")
+    }
+}
 
+struct CoffeeShop {
+    var sales: Int
+    var menu: [String: Int]
+    var pickUpTable: [Coffee]
+    var barista: Person
+    
+    init(barista: Person) {
+        self.sales = 0
+        self.menu = ["americano": 2900, "latte": 3900, "cappuccino": 3900, "mocha": 4200, "macchiato": 4900]
+        self.pickUpTable = [Coffee.americano, Coffee.latte, Coffee.cappuccino, Coffee.mocha, Coffee.macchiato]
+        self.barista = barista
+    }
+    
+    func takeOrder() {
+        print("주문을 받습니다.")
+    }
+    
+    func makeCoffee() {
+        print("커피를 만듭니다.")
+        
+        putCoffee()
+    }
+    
+    func putCoffee() {
+        print("커피를 할당합니다.")
+//        pickUpTable.append(<#T##newElement: Coffee##Coffee#>)
+    }
+}
+
+enum Coffee {
+    case americano
+    case latte
+    case cappuccino
+    case mocha
+    case macchiato
+}
+
+var misterLee = Person(money: 30000)
+var missKim = Person(money: 50000)
+
+var yagombucks = CoffeeShop(barista: misterLee)
