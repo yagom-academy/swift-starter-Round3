0d639d0543b537a696220451623c7e41e9487b33
jmormalik <malikworkarm@gmail.com>
2023-12-15 14:44:46 +0900

    Round3 step1

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -11,25 +11,33 @@ import Foundation
 struct Person {
     var money: Int = 0
     
-    func buyCoffe(){
-        print("카피를 주문하였습니다.")
+    mutating func buyCoffee(price: Int) {
+        if money >= price {
+            money -= price
+            print("커피를 주문하였습니다.")
+        } else {
+            let shortage = price - money
+            print("잔액이 \(shortage)원만큼 부족합니다.")
+        }
     }
 }
 
 class CoffeeShop{
-        var sales: Int = 0
-        var menu: [Coffee: Int]
-        var pickUpTable = [Coffee]()
-        var barista: Person
-
+    var sales: Int = 0
+    var menu: [Coffee: Int]
+    var pickUpTable = [Coffee]()
+    var barista: Person
     
     init(barista: Person, menu:[Coffee: Int]){
         self.barista = barista
         self.menu = menu
     }
     func takeCoffee(type: Coffee){
-        print("\(type) 주문이 들어왔습니다. 제조를 시작합니다.")
-        pickUpTable.append(type)
+        if let price = menu[type] {
+            barista.buyCoffee(price: price)
+            pickUpTable.append(type)
+            print("\(type) 주문이 들어왔습니다. 제조를 시작합니다.")
+        }
     }
 }
 
@@ -44,9 +52,9 @@ let coffeeMenu: [Coffee: Int] = [
     .latte: 3500,
     .ade: 5000,
     .water: 500
-]//메뉴 가격 설정
+]
 
 var misterLee: Person = Person()
 var missKim: Person = Person()
 
-var yagombucks: CoffeeShop = CoffeeShop(barista: misterLee, menu: coffeeMenu) // 바리스타와 메뉴 가격 할당
+var yagombucks: CoffeeShop = CoffeeShop(barista: misterLee, menu: coffeeMenu)
---
+++ CodeStarterCamp_Week3/야곰벅스.swift
@@ -0,0 +1,8 @@
+//
+//  야곰벅스.swift
+//  CodeStarterCamp_Week3
+//
+//  Created by 유제민 on 12/14/23.
+//
+
+import Foundation
