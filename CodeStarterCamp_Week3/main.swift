502f69af85a8fcce302794472654c30be23912f0
kwonseokki <seokki0706@gmail.com>
2024-01-18 19:52:15 +0900

    Round3[Step1] - seokki

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,55 @@
 
 import Foundation
 
-print("Hello, World!")
+class Person {
+    var money: Int
+    
+    init(money: Int = 0) {
+        self.money = money
+    }
+    
+    func buyCoffee() {
+        
+    }
+}
+
+class CoffeeShop {
+    
+    enum Coffee {
+        case americano, latte, hazelnuts
+    }
+    
+    var sales: Double = 0.0
+    var menus: [Coffee: Double] = [.americano: 3000, .latte: 3500, .hazelnuts: 4000]
+    var pickUpTable = [Coffee]()
+    var barista: Person?
+    
+    func takeOrder(coffee: Coffee) {
+        self.makeCoffee(name: coffee)
+    }
+    
+    private func makeCoffee(name coffee: Coffee) {
+        self.pickUpTable.append(coffee)
+    }
+    
+    func employment(person: Person) {
+        self.barista = person
+    }
+}
+
+
+var misterLee: Person = Person()
+var missKim: Person = Person()
+
+var yagombucks = CoffeeShop()
+
+yagombucks.employment(person: misterLee)
+
+//yagombucks.takeOrder(coffee: .americano)
+//yagombucks.takeOrder(coffee: .hazelnuts)
+//yagombucks.takeOrder(coffee: .hazelnuts)
+//
+//print(yagombucks.pickUpTable)
+
+
 
