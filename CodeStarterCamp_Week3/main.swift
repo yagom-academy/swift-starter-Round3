b93b183dfa4ba43793512841fdb81fbc81447a4f
jimin387 <tig05015@naver.com>
2024-01-26 01:53:26 +0900

    Round3[Step1] - 고래안고래

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -6,7 +6,41 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
+class Person {
+    var money: Double = 0.0
+    
+    func buyCoffee(from coffeeShop: CoffeeShop) {
+        coffeeShop.takeOrder(customer: self)
+    }
+}
 
-print("Hello, World!")
+class CoffeeShop {
+    var revenue: Double = 0.0
+    var menu: [(coffeeType: Coffee, price: Double)] = []
+    var pickUpTable: [Coffee] = []
+    var barista: Person?
+    
+    func takeOrder(customer: Person) {
+        makeCoffee()
+        customer.money -= menu[0].price
+        revenue += menu[0].price
+    }
+    
+    private func makeCoffee() {
+        if let coffeeType = menu.first?.coffeeType {
+            pickUpTable.append(coffeeType)
+        }
+    }
+}
 
+enum Coffee {
+    case americano
+    case latte
+    case cappuccino
+}
+
+let misterLee = Person()
+let missKim = Person()
+
+let yagombucks = CoffeeShop()
+yagombucks.barista = misterLee
