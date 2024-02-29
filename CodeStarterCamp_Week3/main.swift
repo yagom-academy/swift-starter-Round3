bf94eff077d1f1d8bad34b172a885219ec6f05ae
Jay <shinju1412@gmail.com>
2024-02-29 16:19:14 +0900

    Step1 - 타입과 인스턴스의 관계

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -6,7 +6,39 @@
 //  Copyright © yagom academy. All rights reserved.
 //
 
-import Foundation
+struct Person {
+    let name: String
+    var money: Int = 0
+    
+    func buyCoffee(coffee: typesOfCoffee) {
+        print("\(name)이(가) \(coffee)를 주문합니다.")
+    }
+}
 
-print("Hello, World!")
+struct CoffeeShop {
+    var totalSales: Int = 0
+    var menu: [typesOfCoffee: Int] = [:]
+    var pickUpTable: [String] = []
+    var barista: Person
+    
+    func takeOrders() {
+        print("\(barista.name)이(가) 주문을 받습니다.")
+    }
+    
+    mutating func makeCoffee(coffee: typesOfCoffee) {
+        pickUpTable.append(coffee.rawValue)
+        print("주문받은 \(coffee)를 만들었습니다.")
+    }
+}
 
+enum typesOfCoffee: String {
+    case espresso, americano, iceAmericano, cafeLatte, cafeMocha, vanillaLatte
+}
+
+let misterLee = Person(name: "Mr.Lee", money: 5000)
+let missKim = Person(name: "Ms.Kim", money: 10000)
+var yagombucks = CoffeeShop(barista: misterLee)
+
+missKim.buyCoffee(coffee: .espresso)
+yagombucks.takeOrders()
+yagombucks.makeCoffee(coffee: .espresso)
